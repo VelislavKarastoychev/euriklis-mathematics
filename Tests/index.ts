a55301f8eddb1bcc097426dc4576d9e6cb641d09
VelislavKarastoychev <exel_mmm@abv.bg>
2023-12-27 10:41:06 +0200

    Commented all other tests instead of the Frobenius norm test.

--- Tests/index.ts
+++ Tests/index.ts
@@ -1,24 +1,24 @@
 "use strict";
-import "./matrix-constructor.spec.ts";
-import "./is-matrix.spec.ts";
-import "./random-matrix.spec.ts";
-import "./zeros.spec.ts";
-import "./identity-like.spec.ts";
-import "./get-block.spec.ts";
-import "./set-block.spec.ts";
-import "./get-row.spec.ts";
-import "./set-row.spec.ts";
-import "./exchange-rows.spec.ts";
-import "./exchange-columns.spec.ts";
-import "./get-diagonal.spec.ts";
-import "./to-diagonal-matrix.spec.ts";
-import "./append-block-right.spec.ts";
-import "./append-block-bottom.spec.ts";
-import "./transpose.spec.ts";
-import "./is-equal-to.spec.ts";
-import "./is-less-than.spec.ts"
-import "./is-less-than-or-equal.spec.ts";
-import "./is-greater-than.spec.ts";
-import "./is-greater-than-or-equal.spec.ts";
-import "./replicate.spec.ts"
+// import "./matrix-constructor.spec.ts";
+// import "./is-matrix.spec.ts";
+// import "./random-matrix.spec.ts";
+// import "./zeros.spec.ts";
+// import "./identity-like.spec.ts";
+// import "./get-block.spec.ts";
+// import "./set-block.spec.ts";
+// import "./get-row.spec.ts";
+// import "./set-row.spec.ts";
+// import "./exchange-rows.spec.ts";
+// import "./exchange-columns.spec.ts";
+// import "./get-diagonal.spec.ts";
+// import "./to-diagonal-matrix.spec.ts";
+// import "./append-block-right.spec.ts";
+// import "./append-block-bottom.spec.ts";
+// import "./transpose.spec.ts";
+// import "./is-equal-to.spec.ts";
+// import "./is-less-than.spec.ts"
+// import "./is-less-than-or-equal.spec.ts";
+// import "./is-greater-than.spec.ts";
+// import "./is-greater-than-or-equal.spec.ts";
+// import "./replicate.spec.ts"
 import "./frobenius-norm.spec.ts";
