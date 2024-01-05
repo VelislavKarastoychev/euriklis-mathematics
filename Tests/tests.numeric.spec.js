0347abfd2462a8156b7f31e6258bc2e389161a43
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-05 17:37:11 +0200

    Deleted

--- Tests/tests.numeric.spec.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-import numeric from "numericjs";
-import validator from "@euriklis/validator";
-const m = numeric.random([5000, 5000]);
-const benchmark = new validator(m).benchmark((m) => numeric.norm2(m), 1000);
-console.table(benchmark);
