7f1b0f62b8ec217d6d99ac59dc18ed7a854c8ccf
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-05 17:31:12 +0200

    added tests for error handling when the method params are not correct.

--- Tests/identity-like.spec.ts
+++ Tests/identity-like.spec.ts
@@ -1,9 +1,15 @@
 "use strict";
 import validator from "@euriklis/validator";
 import { Matrix } from "../src/index.ts";
-
+import { Integer } from "../src/Matrix/types.ts";
+const generateIdentityLikeMatrix = (rows: Integer, columns: Integer) =>
+  Matrix.identityLike(rows, columns);
 new validator(Matrix.identityLike(15, 51).M)
-  .describe("The identityLike and identity static methods:").test({title: true, success: "green", error: "red"})
+  .describe("The identityLike and identity static methods:").test({
+    title: true,
+    success: "green",
+    error: "red",
+  })
   .describe(
     "1. Have to generates matrix with zero elements except for the diagonal elements which are equal to onces",
   )
@@ -13,11 +19,26 @@ new validator(Matrix.identityLike(15, 51).M)
         new validator(i).not.isSame(j),
       ).or.isSame(1);
     });
-  }).test()
-  .and.isArray
-  .describe("2. Time performance of the identity(Like) method with parameters rows = 5000 and columns = 5000")
+  })
+  .and.isArrayOfIntegerArraysWithEqualSize
+  .test()
+  .describe(
+    "2. Throws error if the rows or columns parameters are inappropriately declared.",
+  )
+  .and.bind(
+    new validator(generateIdentityLikeMatrix)
+      .throwsErrorWith(-10, 7)
+      .and.throwsErrorWith(Math.E, 4)
+      .and.throwsErrorWith(-2, 2)
+      .and.not.throwsErrorWith(4, 3),
+  ).test()
+  .describe(
+    "3. Time performance of the identity(Like) method with parameters rows = 5000 and columns = 5000",
+  )
   .test()
   .on(true, () => {
-    const benchmark = new validator(Matrix).benchmark(m => m.identityLike(5000, 5000))
+    const benchmark = new validator(Matrix).benchmark((m) =>
+      m.identityLike(5000, 5000)
+    );
     console.table(benchmark);
   });
