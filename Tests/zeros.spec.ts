a1e69d75fbccb9596d504c3d584d432adb2a231c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-05 17:33:00 +0200

    Added tests for error handling ability.

--- Tests/zeros.spec.ts
+++ Tests/zeros.spec.ts
@@ -1,17 +1,39 @@
 "use strict";
 import validator from "@euriklis/validator";
 import { Matrix } from "../src/index.ts";
-
+import { Integer } from "../src/Matrix/types.ts";
+const zeros = Array.from({ length: 5000 }).map((_) =>
+  Array.from({ length: 5000 }).map((_) => 0)
+);
+const generateZeroMatrix = (rows: Integer, columns: Integer) =>
+  Matrix.zeros(rows, columns);
 new validator(Matrix.zeros(5000, 5000).M)
-  .describe("The zeros static method:").test({title: true, success: "green", error: "red"})
+  .describe("The zeros static method:").test({
+    title: true,
+    success: "green",
+    error: "red",
+  })
   .isArrayOfArraysWithEqualSize
-  .and.forEvery((row) => row.forEvery((el) => el.isSame(0)))
+  .and.isSame(zeros)
   .describe("1. Has to produce matrix with zero elements")
   .test()
   .and.isInstanceof(Array)
-  .describe("2. Performance of the zeros method with rows = 5000, columns = 5000, type = 'float64'")
+  .and.bind(
+    new validator(generateZeroMatrix).throwsErrorWith(-1, 20)
+      .and.throwsErrorWith(-10, -20.4)
+      .and.throwsErrorWith(10, -2)
+      .and.throwsErrorWith(5, Math.PI)
+      .and.not.throwsErrorWith(3, 4),
+  ).describe(
+    "2. Throws error when the rows or columns are incorrectly (inappropriately) defined.",
+  ).test()
+  .describe(
+    "3. Performance of the zeros method with rows = 5000, columns = 5000, type = 'float64'",
+  )
   .test()
   .on(true, () => {
-    const benchmark = new validator(Matrix).benchmark((m) => m.zeros(5000, 5000));
-    console.table(benchmark)
-  })
+    const benchmark = new validator(Matrix).benchmark((m) =>
+      m.zeros(5000, 5000)
+    );
+    console.table(benchmark);
+  });
