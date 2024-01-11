d1447f91e177f825bc21b994bfef59321e716e37
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-11 21:19:00 +0200

    Added benchmark for the reshape method.

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -5,6 +5,9 @@
 // import "./exchange-rows.spec.ts";
 // import "./frobenius-norm.spec.ts";
 // import "./get-block.spec.ts";
-import "./get-diagonal.spec.ts"
+import "./get-diagonal.spec.ts";
 import "./get-row.spec.ts";
-import "./identity-like.spec.ts";
+// import "./identity-like.spec.ts";
+// import "./replicate.spec.ts";
+// import "./random.spec.ts";
+import "./reshape.spec.ts";
