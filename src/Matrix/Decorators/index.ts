09f1cc208bb2606e6de3c33bdcbc70f84a40037e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-13 23:42:31 +0200

    Renamed file

--- src/Matrix/Decorators/index.ts
+++ src/Matrix/Decorators/index.ts
@@ -9,4 +9,4 @@ export {
 } from "./IfIsNotArrayOfArraysWithEqualSizeThrow.ts";
 export { resetMatrix } from "./ResetMatrix.ts";
 export { ifFromOrToParametersAreIncorrectlyDefinedThrow } from "./IfFromOrToParametersAreIncorrectlyDefinedThrow.ts";
-export { ifRowsAndColumnsAreInappropriatelyDefinedThrow } from "./ifRowsAndColumnsAreInappropriatelyDefinedThrow.ts";
+export { ifRowsAndColumnsAreInappropriatelyDefinedThrow } from "./IfRowsAndColumnsAreInappropriatelyDefinedThrow.ts";
