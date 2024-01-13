8de68f8723b2596d26e3dbb0db90a5af102a35f9
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-13 23:42:38 +0200

    Renamed file

---
+++ src/Matrix/Decorators/IfRowsAndColumnsAreInappropriatelyDefinedThrow.ts
@@ -0,0 +1,28 @@
+"use strict";
+import { Matrix } from "..";
+import { Integer } from "../types";
+/**
+ * Validates that the provided rows and columns are appropriately defined,
+ * throwing an error if conditions are not met.
+ * This function is a decorator for the reshape method.
+ *
+ * @param {Function} error - The error function to be thrown if validation fails.
+ * @returns {Function} Decorator function.
+ */
+export function ifRowsAndColumnsAreInappropriatelyDefinedThrow(
+  error: Function,
+): Function {
+  return function (_: any, __: string, descriptor: PropertyDescriptor) {
+    const method: Function = descriptor.value;
+    descriptor.value = function (rows: Integer, columns: Integer) {
+      if (
+        rows < 0 || columns < 0 ||
+        (this as Matrix).rows * (this as Matrix).columns !== rows * columns ||
+        !Number.isInteger(rows) || !Number.isInteger(columns)
+      ) {
+        error();
+      }
+      return method.call(this, rows, columns);
+    };
+  };
+}
