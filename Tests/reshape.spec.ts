3e18115445914a3ac81a021c12f9de5dbfa888d5
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-13 09:18:33 +0200

    Modified test for reshape method to catch even columns.

--- Tests/reshape.spec.ts
+++ Tests/reshape.spec.ts
@@ -7,6 +7,7 @@ const m = Matrix.random(3, 4);
 const reshaped = m.reshape(2, 6);
 const reshapeMatrix = (rows: Integer, columns: Integer) =>
   Matrix.random(12, 5).reshape(rows, columns);
+const r1 = Matrix.random(3, 11);
 new validator(m)
   .isInstanceof(Matrix)
   .describe("The reshape method has to:")
@@ -27,10 +28,16 @@ new validator(reshaped.M).isArrayOfNumberArraysWithEqualSize
     "1. Regroups the elements of the matrix according to the rows and the columns of the method.",
   )
   .test();
+new validator(
+  r1.reshape(11, 3).reshape(1, 33).reshape(33, 1).reshape(3, 11).isEqualTo(r1)
+)
+  .isSame(true).describe(
+    "2. Returns correct result when the columns are even number.",
+  ).test();
 new validator(reshapeMatrix).throwsErrorWith(2, 4)
   .and.throwsErrorWith(-1, 60)
   .and.throwsErrorWith(Math.PI, 60 / Math.PI)
   .and.not.throwsErrorWith(1, 60)
   .describe(
-    "2. Throws error when the rows and the columns of the matrix are not positive integers",
+    "3. Throws error when the rows and the columns of the matrix are not positive integers",
   ).test();
