788501dc1be01eafb7f3d83eaf3cbef5c4ff317e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-13 09:32:05 +0200

    Restart the implementation

--- Tests/reshape.spec.ts
+++ Tests/reshape.spec.ts
@@ -7,7 +7,6 @@ const m = Matrix.random(3, 4);
 const reshaped = m.reshape(2, 6);
 const reshapeMatrix = (rows: Integer, columns: Integer) =>
   Matrix.random(12, 5).reshape(rows, columns);
-const r1 = Matrix.random(3, 11);
 new validator(m)
   .isInstanceof(Matrix)
   .describe("The reshape method has to:")
@@ -28,16 +27,10 @@ new validator(reshaped.M).isArrayOfNumberArraysWithEqualSize
     "1. Regroups the elements of the matrix according to the rows and the columns of the method.",
   )
   .test();
-new validator(
-  r1.reshape(11, 3).reshape(1, 33).reshape(33, 1).reshape(3, 11).isEqualTo(r1)
-)
-  .isSame(true).describe(
-    "2. Returns correct result when the columns are even number.",
-  ).test();
 new validator(reshapeMatrix).throwsErrorWith(2, 4)
   .and.throwsErrorWith(-1, 60)
   .and.throwsErrorWith(Math.PI, 60 / Math.PI)
   .and.not.throwsErrorWith(1, 60)
   .describe(
-    "3. Throws error when the rows and the columns of the matrix are not positive integers",
+    "2. Throws error when the rows and the columns of the matrix are not positive integers",
   ).test();
