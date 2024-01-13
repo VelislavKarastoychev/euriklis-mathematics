435793495156ed005bab81357e3e81f5f5c51544
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-14 00:27:16 +0200

    Added isEqualsTo method benchmark

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -3,11 +3,12 @@
 // import "./append-block-right.spec.ts";
 // import "./exchange-columns.spec.ts";
 // import "./exchange-rows.spec.ts";
-// import "./frobenius-norm.spec.ts";
+import "./frobenius-norm.spec.ts";
 // import "./get-block.spec.ts";
 import "./get-diagonal.spec.ts";
 import "./get-row.spec.ts";
 // import "./identity-like.spec.ts";
 // import "./replicate.spec.ts";
 // import "./random.spec.ts";
-import "./reshape.spec.ts";
+// import "./reshape.spec.ts";
+// import "./is-equal-to.spec.ts";
