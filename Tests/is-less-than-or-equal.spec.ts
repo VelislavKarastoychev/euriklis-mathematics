520b5c5ac2d5ccbadd5370b278e2b48c01c4de04
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-14 08:53:11 +0200

    Changed test for isLessThanOrEqual method.

--- Tests/is-less-than-or-equal.spec.ts
+++ Tests/is-less-than-or-equal.spec.ts
@@ -12,28 +12,24 @@ new validator(a1.isLessThanOrEqual(a2)).describe(
   "The method isLessThanOrEqual has to:",
 ).test({ title: true, success: "green", error: "red" })
   .isSame(true)
-  .describe("1. return true if the matrices are equals.").test()
-  .and.bind(
-    new validator(a1.isLessThanOrEqual(a3)).isSame(true),
-  ).describe(
-    "2. return true when all elements are less than the method parameter matrix elements.",
-  ).test()
-  .and.bind(
-    new validator(a1.isLessThanOrEqual(a4)).isSame(false),
-  ).describe(
-    "3. return false when some element is not less than or equal to the corresponding element from the method parameter matrix.",
-  ).test()
-  .and.bind(
-    new validator(a1.isLessThanOrEqual(a5)).isSame(false),
-  ).describe(
-    "4. return false when the current Matrix instance and the method parameter have distinct dimension.",
-  ).test()
+  .describe("1. return true if the matrices are equals.").test();
+
+new validator(a1.isLessThanOrEqual(a3)).isSame(true).describe(
+  "2. return true when all elements are less than the method parameter matrix elements.",
+).test();
+
+new validator(a1.isLessThanOrEqual(a4)).isSame(false)
   .describe(
-    "5. Time performance of the isLessThanOrEqual method for random matrices with dimension 5000 x 5000",
-  ).test()
-  .on(true, () => {
-    const m1 = Matrix.random(5000, 5000);
-    const m2 = Matrix.random(5000, 5000, 1, 2, "float32");
-    const t = new validator(m1).benchmark((m) => m.isLessThanOrEqual(m2));
-    console.table(t);
-  });
+    "3. return false when some element is not less than or equal to the corresponding element from the method parameter matrix.",
+  ).test();
+
+new validator(a1.isLessThanOrEqual(a5)).isSame(false).describe(
+  "4. return false when the current Matrix instance and the method parameter have distinct dimension.",
+).test();
+
+new validator(a1.isLessThanOrEqual(a2.M)).isSame(true)
+  .and.bind(
+    new validator(a1.isLessThanOrEqual(a2.data)).isSame(true),
+  ).describe(
+    "5. returns the correct answer when the matrices are from different types",
+  ).test();
