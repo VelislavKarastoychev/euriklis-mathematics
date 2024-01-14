93cd775ebc1c0fc5f3ba2c28313288cd282c2c7b
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-14 09:39:36 +0200

    Specified maxNorm test.

--- Tests/max-norm.spec.ts
+++ Tests/max-norm.spec.ts
@@ -1,6 +1,6 @@
 "use strict";
 import validator from "@euriklis/validator";
-import { Matrix } from '../src/index.ts'
+import { Matrix } from "../src/index.ts";
 
 const matrix = new Matrix([[1, 2, 3], [-4, 5, 6], [7, 8, -9]]);
 const m = Matrix.random(4, 4);
@@ -12,18 +12,13 @@ new validator(matrix.maxNorm)
   .test({
     title: true,
     success: "green",
-    error: "red"
+    error: "red",
   })
   .isSame(9)
   .and.bind(
-    new validator(m.maxNorm).isSame(Math.PI)
+    new validator(m.maxNorm).isSame(Math.PI),
   )
-  .describe("1. Returns the element with the greates absolute value of the matrix")
-  .test()
-  .describe("2. Time performance of the maxNorm method for random matrix with dimension 5000 x 5000")
-  .test()
-  .on(true, () => {
-    const rand = Matrix.random(5000, 5000);
-    const t = new validator(rand).benchmark((m) => m.maxNorm);
-    console.table(t);
-  });
+  .describe(
+    "1. Returns the element with the greates absolute value of the matrix",
+  )
+  .test();
