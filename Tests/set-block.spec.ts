783cc25ad6a765b8231a119bf9784f34d3a9a73a
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-14 10:16:46 +0200

    Specified tests for the setBlock method.

--- Tests/set-block.spec.ts
+++ Tests/set-block.spec.ts
@@ -34,42 +34,30 @@ new validator(
   })
   .describe("1. Has to produce the correct result")
   .isSame(result)
-  .test()
-  .and.bind(
-    new validator(() =>
-      new Matrix(mat48).setBlock({
-        from: [3, 4],
-        to: [2, 3],
-        block: mat23,
-      })
-    ).throwsErrorWith(),
-  )
+  .test();
+
+new validator(() =>
+  new Matrix(mat48).setBlock({
+    from: [3, 4],
+    to: [2, 3],
+    block: mat23,
+  })
+).throwsErrorWith()
   .describe(
     "2. Has to throw error when the from parameters are greater than the to",
   )
-  .test()
-  .describe(
-    "3. Has to throw error when the block is incorrectly defined (with dimensions that don't correspond to the 'from' and 'to' parameters)",
-  ).and.bind(
-    new validator(() =>
-      new Matrix(mat48).setBlock({
-        from: [0, 0],
-        to: [3, 3],
-        block: Array.from({ length: 20 }).map((_) =>
-          Array.from({ length: 10 }).map(Math.random)
-        ),
-      })
-    ).throwsErrorWith()
-  )
-  .test()
-  .isArray
-  .describe("4. Time performance of setBlock for matrix with dimensions 6000 x 6000, from = [100, 99], to: [5099, 5098], block --> random matrix with dimensions 5000 x 5000")
-.test()
-.on(true, () => {
-    const m6 = Matrix.random(6000, 6000);
-    const block = Matrix.random(5000, 5000);
-    const from = [100, 99];
-    const to = [5099, 5098];
-    const benchmark = new validator(m6).benchmark(m => m.setBlock({block, from, to}));
-    console.table(benchmark);
+  .test();
+
+new validator(() =>
+  new Matrix(mat48).setBlock({
+    from: [0, 0],
+    to: [3, 3],
+    block: Array.from({ length: 20 }).map((_) =>
+      Array.from({ length: 10 }).map(Math.random)
+    ),
   })
+).throwsErrorWith()
+  .describe(
+    "3. Has to throw error when the block is incorrectly defined (with dimensions that don't correspond to the 'from' and 'to' parameters)"
+  )
+  .test();
