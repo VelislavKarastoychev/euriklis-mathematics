9d349c2b162817dc8f028e7c6e03c9608af724b8
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-14 18:02:25 +0200

    Specified tests for zeros method.

--- Tests/zeros.spec.ts
+++ Tests/zeros.spec.ts
@@ -27,13 +27,3 @@ new validator(Matrix.zeros(5000, 5000).M)
   ).describe(
     "2. Throws error when the rows or columns are incorrectly (inappropriately) defined.",
   ).test()
-  .describe(
-    "3. Performance of the zeros method with rows = 5000, columns = 5000, type = 'float64'",
-  )
-  .test()
-  .on(true, () => {
-    const benchmark = new validator(Matrix).benchmark((m) =>
-      m.zeros(5000, 5000)
-    );
-    console.table(benchmark);
-  });
