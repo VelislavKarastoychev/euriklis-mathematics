1f6bb2f406ec5affadad0897234c9ae2c5112be6
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-16 17:41:04 +0200

    Implementation of the sum of cubes benchmark

---
+++ Benchmark/sum-of-cubes-of-all-elements.spec.ts
@@ -0,0 +1,29 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+import numeric from "numericjs";
+
+(async () => {
+  const m = Matrix.random(5000, 5000);
+  const md = m.M;
+  new validator(
+    m.sumOfCubesOfAllElements -
+      numeric.mapreduce("accum += xi * xi * xi", "0")(md),
+  ).isInRange(-1e-6, 1e-6)
+    .on(true, (v) => {
+      v.describe("Time performance of sumOfCubesOfAllElements getter method:")
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+      const t1 = new validator(m).benchmark((m) => m.sumOfCubesOfAllElements);
+      const t2 = new validator(md).benchmark((m) =>
+        numeric.mapreduce("accum += xi * xi * xi", "0")(m)
+      );
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2,
+      });
+    });
+})();
