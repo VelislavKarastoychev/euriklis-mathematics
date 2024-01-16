57dbcfa2b7cabcaae4a7499c403a1ca002bc91bf
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-16 17:41:21 +0200

    Implementation of sum of squares benchmark.

---
+++ Benchmark/sum-of-squares-of-all-elements.spec.ts
@@ -0,0 +1,34 @@
+"use strict";
+import validator from "@euriklis/validator";
+import numeric from "numericjs";
+import { Matrix } from "../src/index.ts";
+
+(async () => {
+  const m = Matrix.random(5000, 5000);
+  const md = m.M;
+  new validator(m.sumOfSquaresOfAllElements - numeric.mapreduce("accum += xi * xi", "0")(md))
+    .isInRange(-1e-6, 1e-6)
+    .on(true, (v) => {
+      v.describe("Time performance of the sumOfSquaresOfAllElements method:")
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+      const t1 = new validator(m).benchmark((m) => m.sumOfSquaresOfAllElements);
+      const t2 = new validator(md).benchmark((m) =>
+        numeric.mapreduce("accum += xi * xi", "0")(m)
+      );
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2,
+      });
+    }).on(false, (v) => {
+      v.describe("Internal error in sumOfSquaresOfAllElements.")
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+    });
+})();
