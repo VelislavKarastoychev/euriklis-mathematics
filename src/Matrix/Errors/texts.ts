658778b1ebe8fe1133ff0749e7f18d0add941f7c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-17 22:00:13 +0200

    Error messasge for the pointwise family methods.

--- src/Matrix/Errors/texts.ts
+++ src/Matrix/Errors/texts.ts
@@ -57,3 +57,5 @@ export const InternalErrorInSquaresTxt =
   "Internal error in sumOfSquaresOfAllElements getter method. Probably some of the elements of the current matrix instance is NaN or the result of the calculation is negative.";
 export const InternalErrorInCubesTxt =
   "Internal error in the sumOfCubesOfAllElements getter method. Probably some of the elements of the matrix is NaN.";
+export const IncorrectMatrixParameterInPointwiseTxt = (method: string) =>
+  `Incorrect Matrix parameter in the ${method} method. Note that this parameter has to be either a number, a Matrix or Matrix - like object.`;
