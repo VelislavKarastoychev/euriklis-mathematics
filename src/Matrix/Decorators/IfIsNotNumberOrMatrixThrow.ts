892966caf18cd5129d33b8a8935c9dc9a269e16e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-18 18:52:56 +0200

    Implemented decorator for the pointwise family methods.

--- src/Matrix/Decorators/IfIsNotNumberOrMatrixThrow.ts
+++ src/Matrix/Decorators/IfIsNotNumberOrMatrixThrow.ts
@@ -1,9 +1,16 @@
 "use strict";
 
 import { Matrix } from "../index.ts";
-import { IsArrayOfArraysWithEqualSize, IsNumber } from "../Conditions/index.ts";
+import { IsArrayOfArraysWithEqualSize } from "../Conditions/index.ts";
 import { MatrixType, NumericMatrix } from "../types";
 
+/**
+ * Decorator function to check if the parameter is a number or a matrix-like structure,
+ * and throws an error if not.
+ *
+ * @param {Function} error - The error function to throw if the check fails.
+ * @returns {Function} Decorator function.
+ */
 export function ifIsNotNumberOrMatrixThrow(
   error: Function,
 ): (_: any, __: string, descriptor: PropertyDescriptor) => any {
@@ -13,15 +20,16 @@ export function ifIsNotNumberOrMatrixThrow(
       m: number | Matrix | MatrixType | NumericMatrix,
     ) {
       if (
-        Matrix.isMatrix(m) ||
-        IsArrayOfArraysWithEqualSize(m as MatrixType | NumericMatrix) ||
-        IsNumber(m)
-      ) {
+        !Matrix.isMatrix(m) &&
+        !IsArrayOfArraysWithEqualSize(m as MatrixType | NumericMatrix) &&
+        typeof m !== "number"
+      ) error();
+      else {
         return method.call(
           this,
           m as number | Matrix | MatrixType | NumericMatrix,
         );
-      } else error();
+      }
     };
   };
 }
