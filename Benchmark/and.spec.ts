7673abb8af743749b55b08fc57f8bc99340ec1c8
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-20 11:57:54 +0200

    Implemented and benchmark test.

---
+++ Benchmark/and.spec.ts
@@ -0,0 +1,37 @@
+"use strict";
+import validator from "@euriklis/validator";
+import numeric from "numericjs";
+import { Matrix } from "../src/index.ts";
+import { NumericMatrix } from "../src/Matrix/types.ts";
+
+(async () => {
+  const r = Matrix.random(5000, 5000, 1, 100);
+  const rm = r.M;
+  const  halfs = Matrix.replicate(5, 5000, 5000);
+  const halfsm = halfs.M;
+  new validator(r.and(halfs).isEqualTo(
+    ((numeric.and(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
+  )
+    .isSame(true)
+    .on(true, (v) => {
+      v.describe("Time performance of and method:")
+       .test({
+          title: true,
+          success: "green",
+          error: "red"
+        });
+      const t1 = new validator(r).benchmark((m) => m.and(halfs));
+      const t2 = new validator(rm).benchmark((m) => numeric.and(m, halfsm));
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2
+      });
+    }).on(false, (v) => {
+      v.describe("Internal error in and benchmark test.")
+       .test({
+          title: true,
+          success: "green",
+          error: "red"
+        });
+    });
+})();
