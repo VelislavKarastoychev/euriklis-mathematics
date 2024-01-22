cf2790d94fae0f50316217121ef16b2460b04cbb
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-22 15:49:22 +0200

    Implemented benchmark test for the negate method.

---
+++ Benchmark/negate.spec.ts
@@ -0,0 +1,39 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+import numeric from "numericjs";
+
+(async () => {
+  const r = Matrix.random(5000, 5000);
+  const m = r.M;
+
+  new validator(r.negate.isEqualTo(numeric.neg(m)))
+    .isSame(true)
+    .on(true, (v) => {
+      v.describe(
+        "Time performance of the negate getter method for matrix with dimension 5000 x 5000:",
+      )
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+
+      const t1 = new validator(r).benchmark((m) => m.negate);
+      const t2 = new validator(m).benchmark((m) => numeric.neg(m));
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2,
+      });
+    })
+    .on(false, (v) => {
+      v.describe(
+        "Internal error in the negate benchmark test. Probably the unary pointwise utiltiy function has an error.",
+      )
+        .test({
+          title: true,
+          success: "red",
+          error: "green",
+        });
+    });
+})();
