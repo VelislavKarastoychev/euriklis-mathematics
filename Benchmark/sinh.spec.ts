f919189bd3b8872348d42fbf5c0f15d8ec1b2b95
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 18:01:12 +0200

    Implemented benchmark test for the sinh method.

---
+++ Benchmark/sinh.spec.ts
@@ -0,0 +1,35 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+import numeric from "numericjs";
+
+(async () => {
+  const r = Matrix.random(5000, 5000);
+  const m = r.M;
+  new validator(r.sinh().isEqualTo(numeric.pointwise(['x[i]'],'ret[i] = Math.sinh(x[i])')(m)))
+    .isSame(true)
+    .on(true, (v) => {
+      v.describe(
+        "Time performance of the sinh method with matrix with dimension 5000 x 5000.",
+      )
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+      const t1 = new validator(r).benchmark((m) => m.sinh());
+      const t2 = new validator(m).benchmark((m) => numeric.pointwise(['x[i]'], 'ret[i] = Math.sinh(x[i])')(m));
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2,
+      });
+    })
+    .on(false, (v) => {
+      v.describe("Internal error in sinh benchmark test method.")
+        .test({
+          title: true,
+          success: "red",
+          error: "green",
+        });
+    });
+})();
