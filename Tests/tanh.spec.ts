6d2cb9dd960d3da3ad3d4b6c8d9e8bbadccf7e7f
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 19:07:28 +0200

    Implemented test for the tanh method.

---
+++ Tests/tanh.spec.ts
@@ -0,0 +1,24 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+
+const r = Matrix.random(3, 4);
+const tanh = r.M.map((r: number[]) => r.map((c: number) => Math.tanh(c)));
+const tanhWeighted = r.M.map((r: number[]) =>
+  r.map((c: number) => Math.tanh(2 * c + 3))
+);
+
+new validator(r.tanh().isEqualTo(tanh))
+  .describe("The tanh method has to:")
+  .test({
+    title: true,
+    success: "green",
+    error: "red",
+  }).isSame(true)
+  .describe("1. return the correct result without method parameters")
+  .test();
+
+new validator(r.tanh(2, 3).isEqualTo(tanhWeighted))
+  .isSame(true)
+  .describe("2. return the correct result when the method has parameters.")
+  .test();
