c693ad8b035009529acc453617ac52b71919a875
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 03:18:58 +0200

    Fixed getter to method.

--- Tests/negate.spec.ts
+++ Tests/negate.spec.ts
@@ -4,12 +4,19 @@ import { Matrix } from "../src/index.ts";
 
 const r1 = Matrix.random(3, 4);
 const zeros = Matrix.zeros(3, 4);
-new validator(r1.plus(r1.negate).isEqualTo(zeros))
+new validator(r1.plus(r1.negate()).isEqualTo(zeros))
   .describe("The negate getter method has to:")
   .test({
     title: true,
     success: "green",
-    error: "red"
+    error: "red",
   }).isSame(true)
-  .describe("1. return the correct result (the negative value of each element).")
+  .describe(
+    "1. return the correct result (the negative value of each element).",
+  )
+  .test();
+
+new validator(r1.Hadamard(2).plus(r1.negate(2)).isEqualTo(zeros))
+  .isSame(true)
+  .describe("2. return the correct result when weight and bias are defined.")
   .test();
