5879988a24742422fec56c20e313cb6742aadd2f
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 21:13:04 +0200

    Implemented benchmark sigmoid test.

---
+++ Benchmark/sigmoid.spec.ts
@@ -0,0 +1,41 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+import numeric from "numericjs";
+
+(async () => {
+  const r = Matrix.random(5000, 5000);
+  const m = r.M;
+  new validator(
+    r.sigmoid().isEqualTo(
+      numeric.pointwise(["x[i]"], "ret[i] = 1 / (1 + Math.exp(-x[i]))")(m),
+    ),
+  )
+    .isSame(true)
+    .on(true, (v) => {
+      v.describe(
+        "Time performance of the sigmoid method with matrix with dimension 5000 x 5000.",
+      )
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+      const t1 = new validator(r).benchmark((m) => m.sigmoid());
+      const t2 = new validator(m).benchmark((m) =>
+        numeric.pointwise(["x[i]"], "ret[i] = 1 / (1 + Math.exp(-x[i]))")(m)
+      );
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2,
+      });
+    })
+    .on(false, (v) => {
+      v.describe("Internal error in sigmoid benchmark test method.")
+        .test({
+          title: true,
+          success: "red",
+          error: "green",
+        });
+    });
+})();
