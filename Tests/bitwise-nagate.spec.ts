f6bb0c8ab825d16f3147646ff2931efe7da185b8
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 03:18:37 +0200

    Fixed getter to method.

--- Tests/bitwise-nagate.spec.ts
+++ Tests/bitwise-nagate.spec.ts
@@ -4,7 +4,8 @@ import { Matrix } from "../src/index.ts";
 
 const r = Matrix.random(3, 4);
 const bneg = r.M.map((r: number[]) => r.map((c: number) => ~c));
-new validator(r.bitwiseNegate.isEqualTo(bneg))
+const bneg2 = r.M.map((r: number[]) => r.map((c: number)=> ~(2 * c)));
+new validator(r.bitwiseNegate().isEqualTo(bneg))
   .describe("The bitwiseNegate method has to:")
   .test({
     title: true,
@@ -14,4 +15,9 @@ new validator(r.bitwiseNegate.isEqualTo(bneg))
   .describe(
     "1. return a matrix with the bitwise negated elements of the elements.",
   )
+  .test();
+
+new validator(r.bitwiseNegate(2).isEqualTo(bneg2))
+  .describe("2. return the correct result when a weight and bias are defined.")
+  .isSame(true)
   .test();
