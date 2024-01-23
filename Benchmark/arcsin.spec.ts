79ea5ff4ca58e30ed97656515c8172bc90af15c4
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 19:06:24 +0200

    Implemented benchmark test for the arcsin method.

---
+++ Benchmark/arcsin.spec.ts
@@ -0,0 +1,35 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+import numeric from "numericjs";
+
+(async () => {
+  const r = Matrix.random(5000, 5000);
+  const m = r.M;
+  new validator(r.arcsin().isEqualTo(numeric.asin(m)))
+    .isSame(true)
+    .on(true, (v) => {
+      v.describe(
+        "Time performance of the arcsin method with matrix with dimension 5000 x 5000.",
+      )
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+      const t1 = new validator(r).benchmark((m) => m.arcsin());
+      const t2 = new validator(m).benchmark((m) => numeric.asin(m));
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2,
+      });
+    })
+    .on(false, (v) => {
+      v.describe("Internal error in arcsin benchmark test method.")
+        .test({
+          title: true,
+          success: "red",
+          error: "green",
+        });
+    });
+})();
