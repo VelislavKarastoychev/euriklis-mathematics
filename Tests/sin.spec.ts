ae4ce2a0e42f08e788ec28bbdb5a482f7dd108e6
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 03:20:29 +0200

    Implemented test for the sin method.

---
+++ Tests/sin.spec.ts
@@ -0,0 +1,24 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+
+const r = Matrix.random(3, 4);
+const sin = r.M.map((r: number[]) => r.map((c: number) => Math.sin(c)));
+const sinWeighted = r.M.map((r: number[]) =>
+  r.map((c: number) => Math.sin(2 * c + 3))
+);
+
+new validator(r.sin().isEqualTo(sin))
+  .describe("The sin method has to:")
+  .test({
+    title: true,
+    success: "green",
+    error: "red",
+  }).isSame(true)
+  .describe("1. return the correct result without method parameters")
+  .test();
+
+new validator(r.sin(2, 3).isEqualTo(sinWeighted))
+  .isSame(true)
+  .describe("2. return the correct result when the method has parameters.")
+  .test();
