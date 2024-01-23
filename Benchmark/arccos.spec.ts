30e377eaeb839434ebb0e9625fe64f1525d35412
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-23 19:27:42 +0200

    Implemented benchmark test for the arccos method.

---
+++ Benchmark/arccos.spec.ts
@@ -0,0 +1,35 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+import numeric from "numericjs";
+
+(async () => {
+  const r = Matrix.random(5000, 5000);
+  const m = r.M;
+  new validator(r.arccos().isEqualTo(numeric.acos(m)))
+    .isSame(true)
+    .on(true, (v) => {
+      v.describe(
+        "Time performance of the arccos method with matrix with dimension 5000 x 5000.",
+      )
+        .test({
+          title: true,
+          success: "green",
+          error: "red",
+        });
+      const t1 = new validator(r).benchmark((m) => m.arccos());
+      const t2 = new validator(m).benchmark((m) => numeric.acos(m));
+      console.table({
+        "@euriklis/mathematics": t1,
+        numericjs: t2,
+      });
+    })
+    .on(false, (v) => {
+      v.describe("Internal error in arccos benchmark test method.")
+        .test({
+          title: true,
+          success: "red",
+          error: "green",
+        });
+    });
+})();
