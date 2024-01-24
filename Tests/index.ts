cbcd578db5d838148897a9f140173e2ad67ac7cd
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-24 13:38:43 +0200

    Added tests for the methods round,ceil and sqrt methods.

--- Tests/index.ts
+++ Tests/index.ts
@@ -67,3 +67,6 @@ import "./arctan.spec.ts";
 import "./arccotan.spec.ts";
 import "./abs.spec.ts";
 import "./sigmoid.spec.ts";
+import "./round.spec.ts";
+import "./ceil.spec.ts";
+import "./sqrt.spec.ts";
