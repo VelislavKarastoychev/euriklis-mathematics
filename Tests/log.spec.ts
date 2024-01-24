dc767ac063b96de3f55d9f4b7f6db6126ceed4ab
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-24 14:18:40 +0200

    Implemented test for the log method.

---
+++ Tests/log.spec.ts
@@ -0,0 +1,24 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+
+const r = Matrix.random(3, 4);
+const log = r.M.map((r: number[]) => r.map((c: number) => Math.log(c)));
+const logWeighted = r.M.map((r: number[]) =>
+  r.map((c: number) => Math.log(2 * c + 3))
+);
+
+new validator(r.log().isEqualTo(log))
+  .describe("The log method has to:")
+  .test({
+    title: true,
+    success: "green",
+    error: "red",
+  }).isSame(true)
+  .describe("1. return the correct result without method parameters")
+  .test();
+
+new validator(r.log(2, 3).isEqualTo(logWeighted))
+  .isSame(true)
+  .describe("2. return the correct result when the method has parameters.")
+  .test();
