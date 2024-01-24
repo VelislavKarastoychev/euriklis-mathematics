32b946d4d3555cb75610a34851c8776f902bfe1e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-01-24 13:45:35 +0200

    Implemented test for the ceil method.

---
+++ Tests/ceil.spec.ts
@@ -0,0 +1,26 @@
+"use strict";
+import validator from "@euriklis/validator";
+import { Matrix } from "../src/index.ts";
+
+const r = Matrix.random(3, 4, -1, 1);
+const m = r.M.map((r: number[]) => r.map((c: number) => Math.ceil(c)));
+const m1 = r.M.map((r: number[]) =>
+  r.map((c: number) => Math.ceil(2 * c + 3))
+);
+
+new validator(r.ceil().isEqualTo(m))
+  .describe("The ceil method has to:")
+  .test({
+    title: true,
+    success: "green",
+    error: "red",
+  }).isSame(true)
+  .describe("1. return the correct result without parameters.")
+  .test();
+
+new validator(r.ceil(2, 3).isEqualTo(m1))
+  .isSame(true)
+  .describe(
+    "2. return the correct result when the method's parameters are declared.",
+  )
+  .test();
