b8754d978885efaaff8341d485e6a26b14d3cdf2
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-14 22:10:09 +0200

    Implemented test for the random method.

--- Benchmark/random.spec.ts
+++ Benchmark/random.spec.ts
@@ -1,27 +1,27 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
-import { Matrix } from "../src/index.ts";
+
 import numeric from "numericjs";
+import { Matrix } from "../src/index.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
+(async () => {
+  const condition = true;
+  const conditionGeneric = true;
+  const euriklisTest = (m: any) => m.random(5000, 5000, "float64");
+  const euriklisTestGeneric = (m: any) => m.random(5000, 5000);
+  const numericTest = (m: any) => m.random([5000, 5000]);
+  startPerformanceTest(
+    "random()",
+    [{ param: "matrix", dimensions: [5000, 5000], type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 
-const r1 = Matrix.random(5, 11);
-const r2 = numeric.random([5, 11]);
-const onces = Matrix.replicate(1, 5, 11);
-
-(async () =>
-  new validator(r1.isLessThan(onces))
-    .isSame(true)
-    .and.bind(
-      new validator(new Matrix(r2).isLessThan(onces)).isSame(true),
-    ).describe("Time performance of random method for matrix with dimension 5000 x 5000:")
-    .test({
-      title: true,
-      success: "green",
-      error: "red",
-    })
-    .on(true, () => {
-      const t1 = new validator(Matrix).benchmark((m) => m.random(5000, 5000));
-      const t2 = new validator(numeric).benchmark((num) =>
-        num.random([5000, 5000])
-      );
-      console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-    }))();
+  // startPerformanceTest(
+  //   "random",
+  //   [{ param: "matrix", dimensions: [5000, 5000], type: "generic" }],
+  //   conditionGeneric,
+  //   euriklisTestGeneric,
+  //   numericTest,
+  // );
+})();
