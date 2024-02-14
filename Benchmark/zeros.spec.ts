a3c1d582b05223ea0f4e44a145c4f9b4ec4e08a4
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-14 22:10:21 +0200

    Revidsed test for the zeros method.

--- Benchmark/zeros.spec.ts
+++ Benchmark/zeros.spec.ts
@@ -1,16 +1,13 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
 import { dimensions, startPerformanceTest } from "./utils.ts";
 (async () => {
-  const z = Matrix.zeros(dimensions[0], dimensions[1]);
-  const zm = numeric.rep(dimensions, 0) as number[][];
-  const condition = Matrix.isEqualTo(z, zm);
-  const conditionGeneric = Matrix.isEqualTo(Matrix.zeros(501, 501), numeric.rep([501, 501], 0));
-  const euriklisTest = (m) => m.zeros(dimensions[0], dimensions[1]);
-  const euriklisTestGeneric = (m) => m.zeros(dimensions[0], dimensions[1], "generic");
-  const numericTest = (m) => m.rep(dimensions, 0);
+  const condition = Matrix.isEqualTo(Matrix.zeros(503, 503), numeric.rep([503, 503], 0));
+  const conditionGeneric = Matrix.isEqualTo(Matrix.zeros(501, 501, "generic"), numeric.rep([501, 501], 0));
+  const euriklisTest = (m: any) => m.zeros(dimensions[0], dimensions[1]);
+  const euriklisTestGeneric = (m: any) => m.zeros(dimensions[0], dimensions[1], "generic");
+  const numericTest = (m: any) => m.rep(dimensions, 0);
   startPerformanceTest(
     "zeros and zero",
     [{ param: "matrix", dimensions, type: "float64" }],
