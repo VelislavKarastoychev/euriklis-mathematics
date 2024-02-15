360b2aabdbdd12f476810cb49b2d3fc20ad39ae3
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-15 16:01:15 +0200

    Implemented test for the copy message.

--- Tests/copy.spec.ts
+++ Tests/copy.spec.ts
@@ -1,10 +1,10 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
-import { Matrix } from "../src/index.ts";
+import { Matrix } from "../src";
 
 const r1 = Matrix.random(3, 4);
-const r2 = r1.copy();
-new validator(r1.isEqualTo(r2))
+const r2 = Matrix.copy(r1);
+new validator(Matrix.isEqualTo(r1, r2))
   .isSame(true)
   .describe("The copy method has to:")
   .test({
@@ -14,7 +14,17 @@ new validator(r1.isEqualTo(r2))
   }).describe("1. copies the initial matrix.")
   .test();
 
-new validator(r1.cos().isEqualTo(r2))
+new validator(Matrix.isEqualTo(r2, Matrix.copy(r1, "float64", 2, 1)))
   .isSame(false)
   .describe("2. be immutable with respect to the initial matrix.")
   .test();
+
+
+new validator(
+  Matrix.isEqualTo(
+    Matrix.copy(Matrix.replicate(2, 3, 4), undefined, 2, 1),
+    Matrix.replicate(5, 3, 4),
+  ),
+).isSame(true)
+.describe("3. transform linearly the elements of the matrix if weight and bias parameters are specified.")
+.test();
