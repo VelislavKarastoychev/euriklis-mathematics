3989e70523ec081c84788825777caebbd154fb39
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-15 16:00:47 +0200

    Implemented test for the copy method.

--- Benchmark/copy.spec.ts
+++ Benchmark/copy.spec.ts
@@ -1,34 +1,36 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
-import { Matrix } from "../src/index.ts";
-import numeric from "numericjs";
-
+import { Matrix } from "../src";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+import { dimensions, startPerformanceTest } from "./utils";
 (async () => {
-  const r1 = Matrix.random(5000, 5000);
-  const r2 = r1.copy();
-  const m1 = r1.M;
-
-  new validator(r1.isEqualTo(r2))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of the copy method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r1).benchmark((m) => m.copy());
-      const t2 = new validator(m1).benchmark((m) => numeric.clone(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in copy method.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const r = Matrix.random(2, 3);
+  const r1 = Matrix.copy(r);
+  r1[1][2] = Math.PI;
+  const c1 = !Matrix.isEqualTo(r, r1);
+  const n = Matrix.copy(r, "generic");
+  const n1 = Matrix.copy(n, "generic");
+  n1[1][2] = Math.PI;
+  const c2 = !Matrix.isEqualTo(n, n1);
+  let m: MatrixType | NumericMatrix, m1: MatrixType | NumericMatrix;
+  m = Matrix.random(...dimensions);
+  m1 = Matrix.random(dimensions[0], dimensions[1], 0, 1, "generic");
+  const euriklisTest = (matrix: any) => matrix.copy(m);
+  const numericTest = (matrix: any) => matrix.clone(m1);
+  startPerformanceTest(
+    "copy",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    c1 && c2,
+    euriklisTest,
+    numericTest,
+  );
+  Matrix.setType("generic");
+  m = Matrix.random(...dimensions);
+  m1 = Matrix.random(dimensions[0], dimensions[1], 0, 1, "generic");
+  startPerformanceTest(
+    "copy",
+    [{ param: "matrix", dimensions, type: "generic" }],
+    c1 && c2,
+    euriklisTest,
+    numericTest,
+  );
 })();
