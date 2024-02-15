7039643a3de60bf04ebcc582ef9bb3dbb0d27abd
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-15 10:35:53 +0200

    Implemented tests for random matrix methods

--- Benchmark/random.spec.ts
+++ Benchmark/random.spec.ts
@@ -6,22 +6,42 @@ import { dimensions, startPerformanceTest } from "./utils.ts";
 (async () => {
   const condition = true;
   const conditionGeneric = true;
-  const euriklisTest = (m: any) => m.random(5000, 5000, "float64");
-  const euriklisTestGeneric = (m: any) => m.random(5000, 5000);
-  const numericTest = (m: any) => m.random([5000, 5000]);
+  const euriklisTest = (m: any) => m.random(dimensions[0], dimensions[1]);
+  const numericTest = (m: any) => m.random(dimensions);
+  const euriklisRandomFast = (m: any) => m.uniqueRandom(dimensions[0], dimensions[1]);
   startPerformanceTest(
     "random()",
-    [{ param: "matrix", dimensions: [5000, 5000], type: "float64" }],
+    [{ param: "matrix", dimensions, type: "float64" }],
     condition,
     euriklisTest,
     numericTest,
   );
 
-  // startPerformanceTest(
-  //   "random",
-  //   [{ param: "matrix", dimensions: [5000, 5000], type: "generic" }],
-  //   conditionGeneric,
-  //   euriklisTestGeneric,
-  //   numericTest,
-  // );
+  Matrix.setType("generic");
+  startPerformanceTest(
+    "random",
+    [{ param: "matrix", dimensions, type: "generic" }],
+    conditionGeneric,
+    euriklisTest,
+    numericTest,
+  );
+  Matrix.setType("float64");
+
+  startPerformanceTest(
+    "uniqueRandom()",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisRandomFast,
+    numericTest,
+  );
+
+  Matrix.setType("generic");
+  startPerformanceTest(
+    "uniqueRandom()",
+    [{ param: "matrix", dimensions, type: "generic" }],
+    condition,
+    euriklisRandomFast,
+    numericTest,
+  );
+  Matrix.setType("float64");
 })();
