7d8cfacf736bb23efd368a4e797d4e1c793b7716
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-16 19:55:42 +0200

    Implemented isEqualTo method test.

--- Tests/is-equal-to.spec.ts
+++ Tests/is-equal-to.spec.ts
@@ -1,43 +1,64 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const matrix1 = Matrix.random(10, 11);
 const matrix2 = Matrix.random(10, 11);
 
-new validator(matrix1.isEqualTo(matrix2))
+new validator(Matrix.isEqualTo(matrix1, matrix2))
   .describe("The isEqualsTo method has to:")
   .test({ title: true, success: "green", error: "red" })
   .isSame(true)
   .and.bind(
-    new validator(matrix1.isEqualTo(Matrix.identityLike(10, 11)))
+    new validator(Matrix.isEqualTo(matrix1, Matrix.identityLike(10, 11)))
       .isSame(false),
   )
   .describe(
     "1. return the correct output for matrices with dimension less than 20 x 20",
   ).test();
 new validator(
-  Matrix.random(201, 203, -1, 1).isEqualTo(Matrix.random(201, 203, -1, 1)),
+  Matrix.isEqualTo(
+    Matrix.random(201, 203, -1, 1),
+    Matrix.random(201, 203, -1, 1),
+  ),
 )
   .isSame(true).and.bind(
     new validator(
-      Matrix.random(203, 303).isEqualTo(
+      Matrix.isEqualTo(
+        Matrix.random(203, 303),
         Matrix.random(203, 303, 0, 1, "float32"),
       ),
     )
       .isSame(false),
   )
   .describe("2. returns the correct output for larger matrices.").test();
-new validator(Matrix.identity(200).isEqualTo(Matrix.identity(201)))
+new validator(Matrix.isEqualTo(Matrix.identity(200), Matrix.identity(201)))
   .isSame(false)
   .describe(
     "3. returns 'false' when the matrices have distinct dimension.",
   )
   .test();
-new validator(Matrix.random(2, 3).isEqualTo(Matrix.random(2, 3).M))
+new validator(Matrix.isEqualTo(
+  Matrix.random(2, 3),
+  Matrix.random(2, 3),
+))
   .isSame(true)
   .and.bind(
-    new validator(Matrix.random(40, 31).isEqualTo(Matrix.random(40, 31).data)).isSame(true)
+    new validator(Matrix.isEqualTo(
+      Matrix.random(40, 31),
+      Matrix.random(40, 31),
+    ))
+      .isSame(true),
   )
-  .describe("4. return the correct output when the matrices have different type.")
+  .describe(
+    "4. return the correct output when the matrices have different type.",
+  )
+  .test();
+new validator((m: MatrixType | NumericMatrix) => Matrix.isEqualTo(m, m))
+  .throwsErrorWith([
+    [1, 2, 3],
+    [1, 23],
+    [123],
+  ]).describe("5. throws error when the matrices are not tables.")
   .test();
