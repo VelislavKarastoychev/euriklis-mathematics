904c7da73dc8969eca00e2c897325f7a46f2745e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-16 21:40:04 +0200

    Implemented test for the isLessThan method.

--- Tests/is-less-than.spec.ts
+++ Tests/is-less-than.spec.ts
@@ -1,40 +1,49 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const a = new Matrix([
+const a = [
   [1, 2, 3],
   [4, 5, 6],
-]);
+];
 
-const b = new Matrix([
+const b = [
   [2, 3, 4],
   [5, 6, 7],
-]);
+];
 
-const c = new Matrix([
+const c = [
   [0, 1, 2],
   [3, 4, 15],
-]);
+];
 
-new validator(a.isLessThan(b))
+new validator(Matrix.isLessThan(a, b))
   .describe("The isLessThan method has to:").test({
     title: true,
     success: "green",
     error: "red",
   })
   .isSame(true);
-new validator(b.isLessThan(c)).isSame(false)
+new validator(Matrix.isLessThan(b, c)).isSame(false)
   .describe("1. return the correct answer").test();
 
-new validator(a.isLessThan(new Matrix([[10000000]]))).isSame(false)
+new validator(Matrix.isLessThan(b, [[10000000]])).isSame(false)
   .describe("2. return false when the matrices have distinct dimension.")
   .test();
 
-new validator(a.isLessThan(b.M)).isSame(true)
+new validator(Matrix.isLessThan(a, Matrix.copy(b, "float32"))).isSame(true)
   .and.bind(
-    new validator(a.isLessThan(b.data)).isSame(true),
+    new validator(Matrix.isLessThan(Matrix.copy(a, "float64"), b)).isSame(true),
   ).describe(
     "3. return the correct result when the matrices have different types.",
   )
+  .test();
+
+new validator((m: MatrixType | NumericMatrix) => Matrix.isLessThan(m, m))
+  .throwsErrorWith([
+    [1, 2, 3],
+    [1, 23],
+    [123],
+  ]).describe("4. throws error when some of the matrices is not table.")
   .test();
