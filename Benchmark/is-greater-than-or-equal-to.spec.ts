a21e6e9e00d827dfad6cc426afe74a0f91162470
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-16 21:08:59 +0200

    Implemented benchmark test for the isGreaterThanOrEqual method.

--- Benchmark/is-greater-than-or-equal-to.spec.ts
+++ Benchmark/is-greater-than-or-equal-to.spec.ts
@@ -2,45 +2,19 @@
 import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r1 = Matrix.random(3, 4);
-  const r2 = Matrix.random(3, 4);
-  const r3 = Matrix.random(3, 4, 1, 2);
-  const m1 = r1.M;
-  const m2 = r2.M;
-  const m3 = r3.M;
-  new validator(r1.isGreaterThanOrEqual(r2)).isSame(true)
-    .and.bind(
-      new validator(r3.isGreaterThanOrEqual(r1)).isSame(true),
-    ).and.bind(
-      new validator(numeric.geq(m1, m2)).isArrayOfArraysWithEqualSize
-        .and.forEvery((row) => row.forEvery((item) => item.isSame(true))),
-    ).and.bind(
-      new validator(numeric.geq(m3, m1)).isArrayOfArraysWithEqualSize
-        .and.forEvery((row) => row.forEvery((item) => item.isSame(true))),
-    ).on(true, (v) => {
-      v.describe("Time performance of isGreaterThanOrEqual method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const br1 = Matrix.random(5000, 5000);
-      const br2 = Matrix.random(5000, 5000);
-      const bm1 = br1.M;
-      const bm2 = br2.M;
-      const t1 = new validator(br1).benchmark((m) =>
-        m.isGreaterThanOrEqual(br2)
-      );
-      const t2 = new validator(bm1).benchmark((m) => numeric.geq(m, bm2));
-      console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-    }).on(false, (v) => {
-      v.describe("Internal error in isGreaterThanOrEqualTo method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const m1 = Matrix.random(dimensions[0], dimensions[1], 2, 3);
+  const m2 = Matrix.random(...dimensions);
+  const condition = numeric.geq(m1, m2) && Matrix.isGreaterThanOrEqual(m1, m2);
+  const euriklisTest = (m: any) => m.isGreaterThanOrEqual(m1, m2);
+  const numericTest = (m: any) => m.geq(m2, m1);
+  startPerformanceTest(
+    "isGreaterThanOrEqual",
+    [{ param: "matrices" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
