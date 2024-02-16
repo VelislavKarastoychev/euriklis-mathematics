445a3a0951a33a3652fe51238d4f227e72577020
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-16 17:57:51 +0200

    Implemented test for the isSquare method.

---
+++ Tests/is-square.spec.ts
@@ -0,0 +1,24 @@
+"use strict";
+import validator from "@euriklis/validator-ts";
+import { Matrix } from "../src";
+import { MatrixType } from "../src/Matrix/types";
+new validator(Matrix.isSquare(Matrix.random(2, 2)))
+  .isSame(true)
+  .and.bind(
+    new validator(Matrix.isSquare(Matrix.random(2, 3)))
+      .isSame(false),
+  )
+  .describe("The isSquare method has to:")
+  .test({ title: true, success: "green", error: "red" })
+  .describe(
+    "1. return true when the matrix has the same number of rows and columns and false otherwise.",
+  )
+  .test();
+
+new validator((m: MatrixType) => Matrix.isSquare(m))
+  .throwsErrorWith([
+    [2, 3, 4],
+    [2, 34],
+    [234]
+  ]).describe("2. throws error when the matrix is incorrectly defined (i.e. the matrix is not a table).")
+  .test();
