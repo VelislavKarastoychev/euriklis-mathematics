22b258aa6ac174365786a6e440fb5b5adc4ac997
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-16 21:12:20 +0200

    Implemented test for the method isGreaterThanOrEqual

--- Tests/is-greater-than-or-equal.spec.ts
+++ Tests/is-greater-than-or-equal.spec.ts
@@ -1,14 +1,14 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+const a1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const a2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const a3 = [[4, 5, 6], [7, 8, 9], [10, 11, 12]];
+const a4 = [[1, 2, 3], [4, 5, 6]];
+const a5 = [[1, 2], [3, 4], [5, 6]];
 
-const a1 = new Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-const a2 = new Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-const a3 = new Matrix([[4, 5, 6], [7, 8, 9], [10, 11, 12]]);
-const a4 = new Matrix([[1, 2, 3], [4, 5, 6]]);
-const a5 = new Matrix([[1, 2], [3, 4], [5, 6]]);
-
-new validator(a1.isGreaterThanOrEqual(a2))
+new validator(Matrix.isGreaterThanOrEqual(a1, a2))
   .describe("The isGreaterThanOrEqual method has to:").test({
     title: true,
     success: "green",
@@ -18,26 +18,34 @@ new validator(a1.isGreaterThanOrEqual(a2))
     "1. return true when the current Matrix instance and the method parameter are equal.",
   ).test();
 
-new validator(a3.isGreaterThanOrEqual(a2)).isSame(true).describe(
+new validator(Matrix.isGreaterThanOrEqual(a3, a2)).isSame(true).describe(
   "2. return true when the elements of the current Matrix instance are greater than the elements of the method parameter.",
 ).test();
 
-new validator(a4.isGreaterThanOrEqual(a5)).isSame(false)
+new validator(Matrix.isGreaterThanOrEqual(a4, a5)).isSame(false)
   .describe(
     "3. return false when the current Matrix instance and the method parameter have distinct dimension.",
   ).test();
 
 new validator(
-  Matrix.random(2, 3, 1, 2).isGreaterThanOrEqual(Matrix.random(2, 3).M),
+  Matrix.isGreaterThanOrEqual(Matrix.random(2, 3, 1, 2),Matrix.random(2, 3)),
 )
   .isSame(true)
   .and.bind(
     new validator(
-      Matrix.random(2, 4, 2, 3).isGreaterThanOrEqual(
-        Matrix.random(2, 4, 2, 3).data,
+      Matrix.isGreaterThanOrEqual(
+        Matrix.random(2, 4, 2, 3),
+        Matrix.random(2, 4, 2, 3, "generic"),
       ),
     ).isSame(true),
   ).describe(
     "4. return the correct value when the matrices are with the same dimension and different type.",
   )
+  .test();
+new validator((m: MatrixType | NumericMatrix) => Matrix.isGreaterThanOrEqual(m, m))
+  .throwsErrorWith([
+    [1, 2, 3],
+    [1, 23],
+    [123]
+  ]).describe("5. throws error when some of the matrices are not table.")
   .test();
