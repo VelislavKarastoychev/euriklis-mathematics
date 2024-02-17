ad2739485da2ff4fab9b28556232a9c660f49e19
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-17 17:24:52 +0200

    Implemented benchmark test for the geq method.

--- Benchmark/geq.spec.ts
+++ Benchmark/geq.spec.ts
@@ -1,37 +1,19 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
-import { Matrix } from "../src/index.ts";
-import { NumericMatrix } from "../src/Matrix/types.ts";
+import { Matrix } from "../src";
+import { dimensions, startPerformanceTest } from "./utils";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(0.5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.geq(halfs).isEqualTo(
-    ((numeric.geq(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of geq method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.geq(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.geq(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in geq benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    })
+  const m1 = Matrix.random(...dimensions);
+  const m2 = Matrix.copy(m1, undefined, 2, 1);
+  const condition = Matrix.isEqualTo(Matrix.copy(numeric.geq(m2, m1)), Matrix.geq(m2, m1));
+  const test = (m: any) => m.geq(m2, m1);
+  startPerformanceTest(
+    "geq",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
