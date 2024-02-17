844648e15c18ab2205eccf5bbfe0b5dee52b0af2
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-17 17:43:18 +0200

    Implemented bechmark test for the eq method.

--- Benchmark/eq.spec.ts
+++ Benchmark/eq.spec.ts
@@ -1,38 +1,21 @@
-
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
-import { NumericMatrix } from "../src/Matrix/types.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(0.5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.eq(halfs).isEqualTo(
-    ((numeric.eq(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of eq method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.eq(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.eq(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in eq benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    })
+  const m1 = Matrix.random(dimensions[0], dimensions[1], 3, Math.PI, "generic");
+  const m2 = Matrix.random(dimensions[0], dimensions[1], 3, Math.PI);
+  const condition = Matrix.isEqualTo(
+    Matrix.copy(numeric.eq(m1, m2)),
+    Matrix.eq(m1, m2),
+  );
+  const test = (m: any) => m.eq(m1, m2);
+  startPerformanceTest(
+    "eq",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
