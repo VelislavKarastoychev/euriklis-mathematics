0f602b3df90595449c29f1ec92109ad7739dd844
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-17 13:39:17 +0200

    Adapted decorator to compute the index of the parameter.

--- src/Matrix/Decorators/IfIsNotArrayOfArraysWithEqualSizeThrow.ts
+++ src/Matrix/Decorators/IfIsNotArrayOfArraysWithEqualSizeThrow.ts
@@ -1,13 +1,17 @@
 "use strict";
 import { IsArrayOfArraysWithEqualSize } from "../Conditions/index.ts";
-import { MatrixType, NumericMatrix } from "../types";
+import { Integer, MatrixType, NumericMatrix } from "../types";
 
-export function ifIsNotArrayOfArraysWithEqualSizeThrow(error: Function) {
+export function ifIsNotArrayOfArraysWithEqualSizeThrow(
+  error: Function,
+  paramIndex: Integer = 0,
+) {
   return function (_: any, __: string, propertyDescriptor: PropertyDescriptor) {
     const method = propertyDescriptor.value as Function;
-    propertyDescriptor.value = function (matrix: MatrixType | NumericMatrix, ...params: any[]) {
+    propertyDescriptor.value = function (...params: any[]) {
+      const matrix: MatrixType | NumericMatrix = params[paramIndex];
       if (!IsArrayOfArraysWithEqualSize(matrix)) error();
-      return method.call(this, matrix, ...params);
+      return method.call(this, ...params);
     };
   };
 }
