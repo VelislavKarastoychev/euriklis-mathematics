1e5e01d6fc46ebd2586d034dbe1a18309b462b2a
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-17 13:38:12 +0200

    Implemented benchmark for the gt method.

--- Benchmark/gt.spec.ts
+++ Benchmark/gt.spec.ts
@@ -3,35 +3,21 @@ import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
 import { NumericMatrix } from "../src/Matrix/types.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(0.5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.gt(halfs).isEqualTo(
-    ((numeric.gt(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of gt method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.gt(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.gt(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in gt benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    })
+  const m1 = Matrix.random(...dimensions);
+  const m2 = Matrix.random(dimensions[0], dimensions[1], 2, 3);
+  const condition = Matrix.isEqualTo(
+    Matrix.gt(m2, m1),
+    Matrix.copy(numeric.gt(m2, m1)),
+  );
+  const test = (m: any) => m.gt(m2, m1);
+  startPerformanceTest(
+    "gt",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
