e921d05ff4854cedc7dc01d220f9c6b42e85f425
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-18 22:04:07 +0200

    Implemented benchmark test for the or method.

--- Benchmark/or.spec.ts
+++ Benchmark/or.spec.ts
@@ -1,37 +1,18 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
-import { NumericMatrix } from "../src/Matrix/types.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000, 1, 100);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.or(halfs).isEqualTo(
-    ((numeric.or(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of or method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.or(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.or(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in or benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    });
+  const m1 = Matrix.uniqueRandom(dimensions[0], dimensions[1], 10, 30);
+  const m2 = Matrix.uniqueRandom(dimensions[0], dimensions[1], 20, 50);
+  const condition = Matrix.isEqualTo(Matrix.or(m1, m2), numeric.or(m1, m2));
+  const test = (m: any) => m.or(m1, m2);
+  startPerformanceTest(
+    "or",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
