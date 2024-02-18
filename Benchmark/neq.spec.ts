e60bc72069a76da5d7ab7165147f47b63a96eb8f
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-18 14:45:31 +0200

    Implemented benchmark test for the neq method.

--- Benchmark/neq.spec.ts
+++ Benchmark/neq.spec.ts
@@ -1,37 +1,21 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
-import { NumericMatrix } from "../src/Matrix/types.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(0.5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.neq(halfs).isEqualTo(
-    ((numeric.neq(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of neq method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.neq(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.neq(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in neq benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    })
+  const m1 = Matrix.random(...dimensions);
+  const m2 = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.neq(m1, m2),
+    Matrix.copy(numeric.neq(m1, m2)),
+  );
+  const test = (m: any) => m.neq(m1, m2);
+  startPerformanceTest(
+    "neq",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
