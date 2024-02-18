3eb8758d0fde30c3e53f667e0d026ecba4bec4b8
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-18 14:27:31 +0200

    Implemented benchmark test for the leq method.

--- Benchmark/leq.spec.ts
+++ Benchmark/leq.spec.ts
@@ -1,37 +1,21 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
-import { NumericMatrix } from "../src/Matrix/types.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(0.5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.leq(halfs).isEqualTo(
-    ((numeric.leq(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of leq method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.leq(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.leq(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in leq benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    })
+  const m1 = Matrix.random(...dimensions);
+  const m2 = Matrix.copy(m1, undefined, Math.PI, Math.E);
+  const condition = Matrix.isEqualTo(
+    Matrix.leq(m1, m2),
+    Matrix.copy(numeric.leq(m1, m2)),
+  );
+  const test = (m: any) => m.leq(m1, m2);
+  startPerformanceTest(
+    "leq",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
