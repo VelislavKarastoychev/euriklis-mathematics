aee4312abd1b49a54875513bea1e73931d19f7da
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-18 21:49:21 +0200

    Implemented benchmark test for the bitwiseAnd method.

--- Benchmark/bitwiseAnd.spec.ts
+++ Benchmark/bitwiseAnd.spec.ts
@@ -1,37 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
-import { NumericMatrix } from "../src/Matrix/types.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000, 1, 100);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.bitwiseAnd(halfs).isEqualTo(
-    ((numeric.band(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of bitwiseAnd method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.bitwiseAnd(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.band(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in bitwiseAnd benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    });
+  const m1 = Matrix.uniqueRandom(dimensions[0], dimensions[1], 10, 30);
+  const m2 = Matrix.uniqueRandom(dimensions[0], dimensions[1], 20, 50);
+  const condition = Matrix.isEqualTo(
+    Matrix.bitwiseAnd(m1, m2),
+    Matrix.copy(numeric.band(m1, m2)),
+  );
+  const euriklisTest = (m: any) => m.bitwiseAnd(m1, m2);
+  const numericTest = (m: any) => m.band(m1, m2);
+  startPerformanceTest(
+    "bitwiseAnd",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
