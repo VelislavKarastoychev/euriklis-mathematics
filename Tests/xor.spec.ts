aa9acef2e9aea24fe9dc6962f9a2f67e87ba50df
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-18 22:39:58 +0200

    Implemented test for the xor method.

--- Tests/xor.spec.ts
+++ Tests/xor.spec.ts
@@ -3,11 +3,8 @@ import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const r1 = Matrix.random(4, 5, 1, 2);
-const r2 = Matrix.random(4, 5, 5, 6);
-
-const m1 = r1.M;
-const m2 = r2.M;
+const m1 = Matrix.random(4, 5, 1, 2, "generic") as NumericMatrix;
+const m2 = Matrix.random(4, 5, 5, 6, "generic") as NumericMatrix;
 
 const m3 = m1.map((r: number[], i: number) =>
   r.map((c: number, j: number) => {
@@ -16,10 +13,10 @@ const m3 = m1.map((r: number[], i: number) =>
 );
 
 const m4 = m1.map((r: number[]) => r.map((c) => c ^ 4));
-const runXor = (matrix: number | Matrix | MatrixType | NumericMatrix) =>
-  Matrix.random(4, 5).xor(matrix);
+const runXor = (matrix: number | MatrixType | NumericMatrix) =>
+  Matrix.xor(Matrix.random(4, 5), matrix);
 
-new validator(r1.xor(r2).isEqualTo(m3))
+new validator(Matrix.isEqualTo(Matrix.xor(m1, m2), m3))
   .describe("The xor method has to:")
   .test({
     title: true,
@@ -30,16 +27,18 @@ new validator(r1.xor(r2).isEqualTo(m3))
   )
   .isSame(true)
   .test();
-new validator(r1.xor(r2.M).isEqualTo(m3))
+new validator(Matrix.isEqualTo(Matrix.xor(m1, Matrix.copy(m2, "float64")), m3))
   .and.bind(
-    new validator(r1.xor(r2.data).isEqualTo(m3)),
+    new validator(
+      Matrix.isEqualTo(Matrix.xor(Matrix.copy(m1, "float64"), m2), m3),
+    ),
   ).describe(
     "2. return the correct result when the method's parameter is a matrix - like structure.",
   )
   .isSame(true)
   .test();
 
-new validator(r1.xor(4).isEqualTo(m4))
+new validator(Matrix.isEqualTo(Matrix.xor(m1, 4), m4))
   .isSame(true)
   .describe(
     "3. return the correct result when the method's parameter is a number",
@@ -47,15 +46,21 @@ new validator(r1.xor(4).isEqualTo(m4))
   .test();
 
 new validator(runXor).throwsErrorWith(Matrix.random(2, 2))
-  .describe("4. throws error when the method's parameter is a Matrix with inappropriate dimension.")
+  .describe(
+    "4. throws error when the method's parameter is a Matrix with inappropriate dimension.",
+  )
   .test();
 
 new validator(runXor).throwsErrorWith([[12, 13]])
-  .describe("5. throws error when the method's parameter is a Matrix - like structure with inappropriate size.")
+  .describe(
+    "5. throws error when the method's parameter is a Matrix - like structure with inappropriate size.",
+  )
   .test();
 
 new validator(runXor).throwsErrorWith("incorrect parameter")
-  .describe("6. throws error when the method's parameter is not a number or Matrix or MatrixType or NumericMatrix.")
+  .describe(
+    "6. throws error when the method's parameter is not a number or Matrix or MatrixType or NumericMatrix.",
+  )
   .test();
 
 new validator(runXor).throwsErrorWith()
