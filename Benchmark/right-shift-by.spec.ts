34920305ff1f365acb843e27cd66e287b55d9c77
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-19 11:03:34 +0200

    Implemented test for the rightShiftBy method.

--- Benchmark/right-shift-by.spec.ts
+++ Benchmark/right-shift-by.spec.ts
@@ -1,33 +1,21 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 (async () => {
-  const r2 = Matrix.random(5000, 5000, 2, 5);
-  const r1 = Matrix.random(5000, 5000, 1, 2).or(0);
-  const m1 = r1.M;
-  const m2 = r2.M;
-  new validator(r2.rightShiftBy(r1).isEqualTo(numeric.rshift(m2, m1)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of the rightShiftBy method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r2).benchmark((m) => m.rightShiftBy(r1));
-      const t2 = new validator(m2).benchmark((m) => numeric.rshift(m, m1));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in xor method.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const m1 = Matrix.random(dimensions[0], dimensions[1], 10, 20);
+  const m2 = Matrix.random(dimensions[0], dimensions[1], 1, 2);
+  const condition = Matrix.isEqualTo(
+    Matrix.rightShiftBy(m1, m2),
+    numeric.rshift(m1, m2),
+  );
+  const euriklisTest = (m: any) => m.rightShiftBy(m1, m2);
+  const numericTest = (m: any) => m.rshift(m1, m2);
+  startPerformanceTest(
+    "rightShiftBy",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
