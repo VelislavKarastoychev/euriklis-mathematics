cad6e2a2700c1a1a3223bcc88e986fa14a8e1928
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-19 11:52:55 +0200

    Implemented benchmark test for the getBlock method.

--- Benchmark/get-block.spec.ts
+++ Benchmark/get-block.spec.ts
@@ -1,28 +1,29 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
-const dimensions: [number, number] = [6000, 6000],
-  from: [number, number] = [100, 101],
-  to: [number, number] = [5001, 5100];
-(async () =>
-  new validator(true).isBoolean
-    .describe(
-      `getBlock method time performance with parameters matrix -> ${
-        dimensions[0]
-      } x ${dimensions[1]}, from = ${from}, to = ${to}:`,
-    )
-    .on(true, () => {
-      const m = Matrix.random(...dimensions);
-      const t1 = new validator(m)
-        .benchmark((matrix) =>
-          matrix.getBlock({ from, to })
-        );
-      
-      const t2 = new validator(m.M).benchmark((matrix) => {
-        return numeric.getBlock(matrix, from, to);
-      });
-
-      console.table({ "@euriklis/mathematics": t1, "numericjs": t2 });
-    }))();
+(async () => {
+  const m1 = Matrix.uniqueRandom(...dimensions);
+  const from: [number, number] = [100, 101],
+    to: [number, number] = [4990, 4999];
+  const condition = Matrix.isEqualTo(
+    Matrix.getBlock(m1, { from, to }),
+    numeric.getBlock(m1, from, to),
+  );
+  const euriklisTest = (m: any) => m.getBlock(m1, { from, to });
+  const numericTest = (m: any) => m.getBlock(m1, from, to);
+  startPerformanceTest(
+    "getBlock",
+    [{
+      param: "matrix",
+      dimensions,
+      type: "float64",
+      from: [100, 101],
+      to: [4990, 4999],
+    }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
+})();
