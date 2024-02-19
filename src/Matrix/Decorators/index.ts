5e5e18cb6b2949d492791c4daaa5208fef6221c8
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-19 11:54:18 +0200

    Added decorator for the get and setBlock method

--- src/Matrix/Decorators/index.ts
+++ src/Matrix/Decorators/index.ts
@@ -9,6 +9,6 @@ export {
 } from "./IfIsNotArrayOfArraysWithEqualSizeThrow";
 export { ifTheParametersAreNotMatricesThrow } from "./IfTheParametersAreNotMatricesThrow";
 // export { resetMatrix } from "./ResetMatrix.ts";
-// export { ifFromOrToParametersAreIncorrectlyDefinedThrow } from "./IfFromOrToParametersAreIncorrectlyDefinedThrow.ts";
+export { ifFromOrToParametersAreIncorrectlyDefinedThrow } from "./IfFromOrToParametersAreIncorrectlyDefinedThrow.ts";
 // export { ifRowsAndColumnsAreInappropriatelyDefinedThrow } from "./IfRowsAndColumnsAreInappropriatelyDefinedThrow.ts";
 export { ifIsNotNumberOrMatrixThrow } from "./IfIsNotNumberOrMatrixThrow.ts";
