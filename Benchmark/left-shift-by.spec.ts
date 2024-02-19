7aae786833b4cf23976bff95225f1b84183a939b
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-19 11:03:22 +0200

    Implemented benchmark test for the leftShiftBy method.

--- Benchmark/left-shift-by.spec.ts
+++ Benchmark/left-shift-by.spec.ts
@@ -1,33 +1,21 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 (async () => {
-  const r2 = Matrix.random(5000, 5000, 20, 50);
-  const r1 = Matrix.random(5000, 5000, 1, 2).or(0);
-  const m1 = r1.M;
-  const m2 = r2.M;
-  new validator(r2.leftShiftBy(r1).isEqualTo(numeric.lshift(m2, m1)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of the leftShiftBy method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r2).benchmark((m) => m.leftShiftBy(r1));
-      const t2 = new validator(m2).benchmark((m) => numeric.lshift(m, m1));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in xor method.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const m1 = Matrix.random(dimensions[0], dimensions[1], 10, 20);
+  const m2 = Matrix.random(dimensions[0], dimensions[1], 1, 2);
+  const condition = Matrix.isEqualTo(
+    Matrix.leftShiftBy(m1, m2),
+    numeric.lshift(m1, m2),
+  );
+  const euriklisTest = (m: any) => m.leftShiftBy(m1, m2);
+  const numericTest = (m: any) => m.lshift(m1, m2);
+  startPerformanceTest(
+    "leftShiftBy",
+    [{ param: "matrices", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
