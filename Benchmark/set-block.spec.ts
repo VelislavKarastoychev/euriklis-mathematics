53f9c4276898b7d316a68ed4afd640b15602390a
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-20 12:38:44 +0200

    Fixed setBlock method benchmark test in order to ensure that the results will be equivalent with the numericjs library.

--- Benchmark/set-block.spec.ts
+++ Benchmark/set-block.spec.ts
@@ -4,18 +4,19 @@ import { Matrix } from "../src/index.ts";
 import {  startPerformanceTest, dimensions } from "./utils.ts";
 // const dimensions: [number, number] = [5, 6];
 (async () => {
-  const m1 = Matrix.uniqueRandom(...dimensions);
+  const m11 = Matrix.uniqueRandom(...dimensions);
+  const m12 = Matrix.copy(m11);
   const m2 = Matrix.random(dimensions[0] - 1, dimensions[1] - 1);
   const condition = Matrix.isEqualTo(
-    Matrix.setBlock(m1, {
+    Matrix.setBlock(m11, {
       from: [0, 0],
       to: [dimensions[0] - 2, dimensions[1] - 2],
     }, m2),
-    numeric.setBlock(m1, [0, 0], [dimensions[0] - 2, dimensions[1] - 2], m2),
+    numeric.setBlock(m12, [0, 0], [dimensions[0] - 2, dimensions[1] - 2], m2),
   );
   const euriklisTest = (m: any) =>
-    m.setBlock(m1, { from: [0, 0], to: [dimensions[0] - 2, dimensions[1] - 2] }, m2);
-  const numericTest = (m: any) => m.setBlock(m1, [0, 0], [dimensions[0] - 2, dimensions[1] - 2], m2);
+    m.setBlock(m11, { from: [0, 0], to: [dimensions[0] - 2, dimensions[1] - 2] }, m2);
+  const numericTest = (m: any) => m.setBlock(m12, [0, 0], [dimensions[0] - 2, dimensions[1] - 2], m2);
   startPerformanceTest(
     "setBlock",
     [{ param: "matrices", dimensions, type: "float64" }],
