88ef8781cad6c06703933cc7445b9dca41677d52
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-21 22:09:36 +0200

    Implemented decorator for the exchangeRows method.

---
+++ src/Matrix/Decorators/IfRowOrFromIndexOrToIndexIsIncorrectlyDefinedThrow.ts
@@ -0,0 +1,44 @@
+"use strict";
+
+import { Integer, MatrixType, NumericType } from "../types";
+
+export function ifRowOrFromIndexOrToIndexIsIncorrectlyDefinedThrow(
+  [
+    IncorrectRowIndexParametersInExchangeRows,
+    IncorrectFromColumnIndexParameterInExchangeRows,
+    IncorrectToColumnIndexParameterInExcangeRows,
+  ]: [
+    Function,
+    Function,
+    Function,
+  ],
+) {
+  return function (_: any, __: string, descriptor: PropertyDescriptor) {
+    const method: Function = descriptor.value;
+    descriptor.value = function (
+      matrix: MatrixType | NumericType,
+      row1: Integer,
+      row2: Integer,
+      fromColumn: Integer,
+      toColumn: Integer,
+    ) {
+      if (
+        row1 < 0 || row1 >= matrix.length || row2 < 0 || row2 >= matrix.length
+      ) {
+        IncorrectRowIndexParametersInExchangeRows();
+      }
+
+      if (fromColumn < 0 || fromColumn > toColumn) {
+        IncorrectFromColumnIndexParameterInExchangeRows();
+      }
+
+      if (
+        toColumn < fromColumn || toColumn > matrix[0].length || toColumn < 0
+      ) {
+        IncorrectToColumnIndexParameterInExcangeRows();
+      }
+
+      return method.call(this, matrix, row1, row2, fromColumn, toColumn);
+    };
+  };
+}
