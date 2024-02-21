0b2367c9bf90675df42ccb61654a5d6f9482d581
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-21 19:59:34 +0200

    Implemented test for the getRow method.

--- Tests/get-row.spec.ts
+++ Tests/get-row.spec.ts
@@ -1,6 +1,6 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
-import { Matrix } from "../src/index.ts";
+import { Matrix } from "../src";
 
 const mat45 = [
   [1, 2, 3, 4, 5],
@@ -8,36 +8,51 @@ const mat45 = [
   [1, 2, 3, 4, 5],
   [1, 2, 3, 4, 5],
 ];
-new validator(new Matrix(mat45).getRow(2).M)
+new validator(
+  Matrix.isEqualTo(
+    Matrix.getRow(mat45, 2),
+    [[1, 2, 3, 4, 5]],
+  ),
+)
   .describe("The getRow method:").test({
     title: true,
     success: "green",
     error: "red",
   })
-  .isSame([[1, 2, 3, 4, 5]])
+  .isSame(true)
   .describe(
     "1. Has to return all the elements of the row when starting and ending indices are not inserted",
   )
-  .test()
+  .test();
+
+new validator(
+  Matrix.isEqualTo(
+    Matrix.getRow(mat45, 2, 1),
+    [[2, 3, 4, 5]],
+  ),
+)
+  .isSame(true)
+  .and.bind(
+    new validator(
+      Matrix.isEqualTo(
+        Matrix.getRow(mat45, 3, 1, 3),
+        [[2, 3, 4]],
+      ),
+    )
+      .isSame(true),
+  )
   .describe(
     "2. Has to return the correct output for given starting and ending indices",
   )
-  .and.bind(
-    new validator(new Matrix(mat45).getRow(2, 1).M)
-      .isSame([[2, 3, 4, 5]])
-      .and.bind(
-        new validator(new Matrix(mat45).getRow(3, 1, 3).M)
-          .isSame([[2, 3, 4]]),
-      ),
-  ).test()
+  .test();
+
+new validator(() => Matrix.getRow(mat45, -1)).throwsErrorWith()
   .describe(
     "3. Has to throw error when the index, starting or ending indices are incorrectly defined.",
   )
   .and.bind(
-    new validator(() => new Matrix(mat45).getRow(-1)).throwsErrorWith(),
+    new validator(() => Matrix.getRow(mat45, 1, -1, 2)).throwsErrorWith(),
   ).and.bind(
-    new validator(() => new Matrix(mat45).getRow(1, -1, 2)).throwsErrorWith(),
-  ).and.bind(
-    new validator(() => new Matrix(mat45).getRow(0, 0, 1000)).throwsErrorWith(),
+    new validator(() => Matrix.getRow(mat45, 0, 0, 1000)).throwsErrorWith(),
   )
-  .test()
+  .test();
