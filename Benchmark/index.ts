4f9bf46aca652c641d8dc6115dd29929165ad521
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-21 09:26:12 +0200

    Specified setBlock to be tested.

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -25,8 +25,8 @@
 // import "./xor.spec.ts";
 // import "./right-shift-by.spec.ts";
 // import "./left-shift-by.spec.ts";
-import "./get-block.spec.ts";
-// import "./set-block.spec.ts";
+// import "./get-block.spec.ts";
+import "./set-block.spec.ts";
 // import "./append-block-bottom.spec.ts";
 // import "./append-block-right.spec.ts";
 // import "./exchange-columns.spec.ts";
