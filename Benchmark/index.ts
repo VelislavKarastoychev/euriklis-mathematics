544b89ca0978c0f2d1c5da380644a4f0069449bb
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-21 19:59:23 +0200

    Added benchmark test for the getRow method

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -26,14 +26,14 @@
 // import "./right-shift-by.spec.ts";
 // import "./left-shift-by.spec.ts";
 // import "./get-block.spec.ts";
-import "./set-block.spec.ts";
+// import "./set-block.spec.ts";
+import "./get-row.spec.ts";
 // import "./append-block-bottom.spec.ts";
 // import "./append-block-right.spec.ts";
 // import "./exchange-columns.spec.ts";
 // import "./exchange-rows.spec.ts";
 // import "./frobenius-norm.spec.ts";
 // import "./get-diagonal.spec.ts";
-// import "./get-row.spec.ts";
 // import "./reshape.spec.ts";
 // import "./max-norm.spec.ts";
 // import "./set-row.spec.ts";
