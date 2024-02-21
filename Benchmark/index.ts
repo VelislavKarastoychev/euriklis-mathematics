c77f85163d95aefb881c032e499dd29b1e3eedaa
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-21 22:07:01 +0200

    Added benchmark test for the exchangeRows method.

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -29,10 +29,10 @@
 // import "./set-block.spec.ts";
 import "./get-row.spec.ts";
 import "./set-row.spec.ts";
+import "./exchange-rows.spec.ts";
 // import "./append-block-bottom.spec.ts";
 // import "./append-block-right.spec.ts";
 // import "./exchange-columns.spec.ts";
-// import "./exchange-rows.spec.ts";
 // import "./frobenius-norm.spec.ts";
 // import "./get-diagonal.spec.ts";
 // import "./reshape.spec.ts";
