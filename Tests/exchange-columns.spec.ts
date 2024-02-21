673e35498d5402204866e9305887e3499f8a2cf3
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-21 23:01:08 +0200

    Implemented test for the exchangeColumns method.

--- Tests/exchange-columns.spec.ts
+++ Tests/exchange-columns.spec.ts
@@ -1,6 +1,7 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { Integer } from "../src/Matrix/types.ts";
 
 const a = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -32,27 +33,53 @@ const a1345 = [
   [2, 3, 4, 5, 6, 7, 8, 9, 10],
 ];
 
-new validator(new Matrix(a).exchangeColumns(1, 3).M)
-  .describe("exchangeColumns method:").test({
+new validator(
+  Matrix.isEqualTo(
+    Matrix.exchangeColumns(Matrix.copy(a), 1, 3),
+    a13,
+  ),
+)
+  .describe("exchangeColumns method has to:").test({
     title: true,
     success: "green",
     error: "red",
   })
-  .isSame(a13)
+  .isSame(true)
   .on(false, (v) => {
     console.table(v.value);
     console.table(a13);
   })
   .describe(
-    "1. Has to exchange the columns for all row indices when the from and to indices are not defined.",
+    "1. exchange the columns for all row indices when the from and to indices are not defined.",
   )
-  .test()
+  .test();
+
+new validator(
+  Matrix.isEqualTo(
+    Matrix.exchangeColumns(Matrix.copy(a), 1, 3, 4, 5),
+    a1345,
+  ),
+)
+  .isSame(true)
   .describe(
-    "2. Has to return the correct output when the from and to parameters are defined",
+    "2. return the correct output when the from and to parameters are defined",
+  ).test();
+
+new validator((col1: Integer, col2: Integer) =>
+  Matrix.exchangeColumns(Matrix.copy(a), col1, col2)
+)
+  .throwsErrorWith(-1, 0)
+  .and.throwsErrorWith(1, Infinity)
+  .describe(
+    "3. throws error when some of the column indices is incorectly defined.",
   )
-  .and.bind(
-    new validator(new Matrix(a).exchangeColumns(1, 3, 4, 5).M).isSame(a1345),
-  ).test()
+  .test();
+
+new validator((row1: Integer, row2: Integer) =>
+  Matrix.exchangeColumns(Matrix.copy(a), 0, 1, row1, row2)
+).throwsErrorWith(-1, 2)
+  .and.throwsErrorWith(1, Infinity)
   .describe(
-    "3. Time performance of exchangeColumns method for parameters matrix --> 6000 x 6000, col1 = 99, col2 = 5198, from = 99 to 5098",
-  ).test()
+    "4. throws error when some of the row indices is incorrectly defined.",
+  )
+  .test();
