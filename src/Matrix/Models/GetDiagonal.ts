ee36bdeea283e64b93d44f0f8a112c10228b8aaa
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-22 22:43:12 +0200

    Fixed typedArray length/size for the getDiagonal method utility functoin.

--- src/Matrix/Models/GetDiagonal.ts
+++ src/Matrix/Models/GetDiagonal.ts
@@ -13,18 +13,18 @@ import {
  * @param {MatrixType} matrix - The matrix from which to extract the diagonal or subdiagonal.
  * @param {Integer} row - The row index for subdiagonal (default is 0).
  * @param {TypedArrayConstructor} typedArray - The TypedArray constructor for the matrix type.
- * @returns {[TypedArray]} - The diagonal or subdiagonal as a new Matrix instance.
+ * @returns {MatrixType | NumericMatrix} - The diagonal or subdiagonal as a new Matrix instance.
  */
 export const GetDiagonal = (
   matrix: MatrixType | NumericMatrix,
   row: Integer,
-  typedArray: TypedArrayConstructor = Float64Array,
-): [TypedArray] => {
+  typedArray: TypedArrayConstructor | ArrayConstructor = Float64Array,
+): MatrixType | NumericMatrix => {
   const n = matrix.length - row;
-  const diag: TypedArray = new typedArray(n - row);
+  const diag: TypedArray | number[] = new typedArray(n);
   let i: Integer;
   for (i = n; i--;) {
     diag[i] = matrix[i + row][i];
   }
-  return [diag];
+  return [diag] as MatrixType | NumericMatrix;
 };
