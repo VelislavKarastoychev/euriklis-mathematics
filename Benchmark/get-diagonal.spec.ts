f0e200d4832ef8dc0161536c7a73990e46de3bd1
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-22 22:40:38 +0200

    Implemented benchmark test for the getDiagonal method.

--- Benchmark/get-diagonal.spec.ts
+++ Benchmark/get-diagonal.spec.ts
@@ -2,22 +2,21 @@
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
-new validator(true).isBoolean
-  .describe(
-    "Time performance of getDiagonal method for matrix with parameters 5000 x 5000",
-  )
-  .test({
-    title: true,
-    success: "green",
-    error: "red",
-  })
-  .on(true, () => {
-    const rand5kx5k = Matrix.random(5000, 5000);
-    const t1 = new validator(rand5kx5k).benchmark(
-      (matrix) => matrix.getDiagonal(),
-      100,
-    );
-    const t2 = new validator(rand5kx5k.M).benchmark((m) => [numeric.getDiag(m)]);
-    console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-  });
+(async () => {
+  const m = Matrix.random(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.getDiagonal(m),
+    [numeric.getDiag(m)],
+  );
+  const euriklisTest = (matrix: any) => matrix.getDiagonal(m);
+  const numericTest = (matrix: any) => matrix.getDiag(m);
+  startPerformanceTest(
+    "getDiagonal",
+    [{ param: "matrix", dimensions, type: "float64," }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
+})();
