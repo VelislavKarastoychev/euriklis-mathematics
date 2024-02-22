448aab535bc8a3ae75714aba5a76db1bb15fa182
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-22 22:42:08 +0200

    Implemented test for the getDiagonal method.

--- Tests/get-diagonal.spec.ts
+++ Tests/get-diagonal.spec.ts
@@ -1,31 +1,42 @@
 "use strict";
 
 import { Matrix } from "../src/index.ts";
-import {} from "../src/Matrix/types";
+import { Integer } from "../src/Matrix/types";
 import validator from "@euriklis/validator-ts";
 const rand55 = Matrix.random(5, 5);
-const rand55M = rand55.M;
-new validator(rand55.getDiagonal().M[0])
-  .describe("The getDiagonal method:").test({
+const rand55M = Matrix.copy(rand55, "generic");
+new validator(
+  Matrix.isEqualTo(
+    Matrix.getDiagonal(rand55),
+    [Array.from({ length: 5 }).map((_, i) => rand55M[i][i])],
+  ),
+)
+  .describe("The getDiagonal method has to:").test({
     title: true,
     success: "green",
     error: "red",
   })
   .describe(
-    "1. Has to get the diagonal of the matrix in an row vector vector when no argument is passed (Matrix with 1 x columns elements)",
+    "1. get the diagonal of the matrix in an row vector vector when no argument is passed (Matrix with 1 x columns elements)",
   )
-  .forEvery((di, i) => {
-    return di.isSame(rand55M[i as number][i as number]);
-  }).test()
+  .isSame(true).test();
+
+new validator(Matrix.getDiagonal(rand55, 1, "generic")[0]).forEvery((di, i) => {
+  return di.isSame(rand55M[i as number + 1][i as number]);
+}).on(false, (v) => {
+  const diag = v.value;
+  console.table(diag);
+  console.table(rand55M);
+})
   .describe(
-    "2. Has to return the subdiagonal of an matrix when an row index is defined",
+    "2. return the subdiagonal of an matrix when an row index is defined",
   )
-  .and.bind(
-    new validator(rand55.getDiagonal(1).M[0]).forEvery((di, i) => {
-      return di.isSame(rand55M[i as number + 1][i as number]);
-    }).on(false, (v) => {
-      const diag = v.value;
-      console.table(diag);
-      console.table(rand55M);
-    }),
-  ).test()
+  .test();
+
+new validator((row: Integer) => Matrix.getDiagonal(rand55, row))
+  .throwsErrorWith(-1)
+  .and.throwsErrorWith(100)
+  .describe(
+    "3. throws error when the row index parameter is incorrectly defined.",
+  )
+  .test();
