43fe6e4e37a0f10856d4ba2131b8661911639224
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-23 10:08:10 +0200

    Implemented benchmark test for the toDiagonalMatrix method.

--- Benchmark/to-diagonal-matrix.spec.ts
+++ Benchmark/to-diagonal-matrix.spec.ts
@@ -2,29 +2,21 @@
 import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
+import { startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(1, 5000);
-  const rm = r.toDiagonalMatrix();
-  new validator(rm.isEqualTo(numeric.diag(r.M[0])))
-    .isSame(true)
-    .on(true, (v) => {
-      const m = r.M[0];
-      v.describe("Time performance of toDiagonalMatrix method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.toDiagonalMatrix());
-      const t2 = new validator(m).benchmark((m) => numeric.diag(m));
-      console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-    }).on(false, (v) => {
-      v.describe("Internal error in toDiagonalMatrix benchmark method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.random(1, 5);
+  const condition = Matrix.isEqualTo(
+    Matrix.toDiagonalMatrix(r),
+    numeric.diag(Matrix.copy(r, "generic")[0]),
+  );
+  const euriklisTest = (m: any) => m.toDiagonalMatrix(r);
+  const numericTest = (m: any) => m.diag(r[0]);
+  startPerformanceTest(
+    "toDiagonalMatrix",
+    [{ param: "matrix", dimensions: [1, 5000], type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
