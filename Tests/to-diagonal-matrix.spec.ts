7120721b919ac7601f66df1095f156f646d6441c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-23 10:08:50 +0200

    Implemented test for the toDiagonalMatrix method.

--- Tests/to-diagonal-matrix.spec.ts
+++ Tests/to-diagonal-matrix.spec.ts
@@ -1,18 +1,24 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
-const mtx = Matrix.random(3, 1);
-const m: Matrix = Matrix.random(2, 50);
-new validator(m.toDiagonalMatrix().M).forEvery((row, i) => {
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
+const mtx = Matrix.random(3, 1, 2, 4, "generic");
+const m: MatrixType | NumericMatrix = Matrix.random(2, 50);
+new validator(Matrix.toDiagonalMatrix(m, "generic")).forEvery((row, i) => {
   return row.forEvery((el, j) =>
     (i as number % 50) === j ? el.isNotEqual(0) : el.isEqual(0)
   );
 })
-  .describe("toDiagonalMatrix method:").test({
+  .describe("toDiagonalMatrix method has to:").test({
     title: true,
     success: "green",
     error: "red",
   })
   .describe(
-    "1. Has to generate a row oriented collection of diagonal block matrices for each row of the matrix.",
+    "1. generate a row oriented collection of diagonal block matrices for each row of the matrix.",
   ).test();
+
+new validator(() => Matrix.toDiagonalMatrix(mtx))
+  .describe("2. throw error when the matrix parameter is a column vector.")
+  .throwsErrorWith()
+  .test();
