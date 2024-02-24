a718adab792c15c75c9b4912be4805a1be265a5f
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-24 20:15:44 +0200

    Implemented test for the appendBlockRight method.

--- Tests/append-block-right.spec.ts
+++ Tests/append-block-right.spec.ts
@@ -1,12 +1,13 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const m1 = Matrix.random(2, 3);
 const m2 = Matrix.random(2, 4);
 const m3 = Matrix.random(3, 4);
 
-new validator(m1.appendBlockRight(m2).M).forEvery((row) =>
+new validator(Matrix.appendBlockRight(m1, m2, "generic")).forEvery((row) =>
   row.isNumberArray.and.hasLength(7)
 )
   .describe("appendBlockRight method has to:").test({
@@ -14,24 +15,21 @@ new validator(m1.appendBlockRight(m2).M).forEvery((row) =>
     success: "green",
     error: "red",
   })
-  .describe("1. Append two matrices.").test()
-  .and.bind(
-    new validator(m1.appendBlockRight(m2.M).M).forEvery((row) =>
-      row.isNumberArray.and.hasLength(7)
-    ),
-  ).describe("2. Append matrix and numerix matrix.").test()
-  .and.bind(
-    new validator(m1.appendBlockRight(m2.data).M).forEvery((row) =>
-      row.isNumberArray.and.hasLength(7)
-    ),
-  )
-  .describe("3. Append matrix and typed matrix.").test()
-  .and.bind(
-    new validator((m: Matrix) => m1.appendBlockRight(m)).throwsErrorWith(m3),
-  ).describe("4. Throw error when the block is with inappropriate size.").test()
-  .and.bind(
-    new validator((m: Matrix) => m1.appendBlockRight(m)).not.throwsErrorWith([])
-      .and.not.throwsErrorWith(new Matrix()),
-  ).describe(
-    "5. Not throw when the argument is empty array or empty matrix but returns the same matrix."
-  ).test()
+  .describe("1. Append two matrices.").test();
+new validator(Matrix.appendBlockRight(m1, m2, "generic")).forEvery((row) =>
+  row.isNumberArray.and.hasLength(7)
+)
+  .describe("2. Append matrix and numerix matrix.").test();
+new validator(Matrix.appendBlockRight(m1, m2, "generic")).forEvery((row) =>
+  row.isNumberArray.and.hasLength(7)
+)
+  .describe("3. Append matrix and typed matrix.").test();
+new validator((m: MatrixType | NumericMatrix) => Matrix.appendBlockRight(m1, m))
+  .throwsErrorWith(m3)
+  .describe("4. Throw error when the block is with inappropriate size.").test();
+new validator((m: MatrixType | NumericMatrix) => Matrix.appendBlockRight(m1, m))
+  .not.throwsErrorWith([])
+  .and.not.throwsErrorWith(undefined)
+  .describe(
+    "5. Not throw when the argument is empty array (matrix) or undefined but returns the same matrix.",
+  ).test();
