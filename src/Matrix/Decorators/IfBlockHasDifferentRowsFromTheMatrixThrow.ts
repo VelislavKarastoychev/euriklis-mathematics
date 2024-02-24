c784439cada66b5af30aa65f38043606c37af909
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-24 20:17:54 +0200

    Implemented decorator function ifBlockHasDifferentRowsFromTheMatrixThrow.

---
+++ src/Matrix/Decorators/IfBlockHasDifferentRowsFromTheMatrixThrow.ts
@@ -0,0 +1,26 @@
+"use strict";
+
+import { IsEmpty } from "../Conditions";
+import { MatrixType, NumericMatrix, NumericType } from "../types";
+
+export function ifBlockHasDifferentRowsFromTheMatrixThrow(
+  error: Function,
+) {
+  return function (_: any, __: string, descriptor: PropertyDescriptor) {
+    const method: (
+      matrix: MatrixType | NumericMatrix,
+      block: MatrixType | NumericMatrix,
+      type: NumericType,
+    ) => MatrixType | NumericMatrix = descriptor.value;
+
+    descriptor.value = function (
+      matrix: MatrixType | NumericMatrix,
+      block: MatrixType | NumericMatrix,
+      type: MatrixType | NumericMatrix,
+    ): MatrixType | NumericMatrix {
+      if (matrix.length !== block.length) error();
+
+      return method.call(this, matrix, block, type);
+    };
+  };
+}
