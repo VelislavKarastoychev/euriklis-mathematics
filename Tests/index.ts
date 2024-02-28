7814ea514954d8aafe80918b155c5c1dd7d141b1
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 23:21:51 +0200

    Added test for the norm1 method.

--- Tests/index.ts
+++ Tests/index.ts
@@ -40,7 +40,7 @@ import "./transpose.spec.ts";
 import "./frobenius-norm.spec.ts";
 import "./infinity-norm.spec.ts";
 import "./max-norm.spec.ts";
-// import "./norm1.spec.ts";
+import "./norm1.spec.ts";
 // import "./superior.spec.ts";
 // import "./inferior.spec.ts";
 // import "./sum-of-all-elements.spec.ts"
