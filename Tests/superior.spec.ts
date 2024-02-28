64310d68d976d9290580ca3891b3379bc43e27c6
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 23:43:58 +0200

    Implemented test for the superior method

--- Tests/superior.spec.ts
+++ Tests/superior.spec.ts
@@ -1,13 +1,15 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const m = new Matrix([
+const m = [
   [-3, 5, 7],
   [2, 6, 4],
   [0, 2, 8],
-]);
-new validator(m.superior).describe("The superior method has to:")
+];
+const callSuperior = (m: MatrixType | NumericMatrix) => Matrix.superior(m);
+new validator(Matrix.superior(m)).describe("The superior method has to:")
   .test({
     title: true,
     success: "green",
@@ -15,4 +17,10 @@ new validator(m.superior).describe("The superior method has to:")
   }).isSame(8).describe(
     "1. returns the correct value (the element with maximum value in the matrix).",
   )
+  .test();
+
+new validator(callSuperior)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("2. thorws error when the matrix is incorrectly declared")
   .test();
