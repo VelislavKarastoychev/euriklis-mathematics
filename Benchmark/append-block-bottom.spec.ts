8dcbc8c691753e01048e726b848737242e3bfb23
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 10:36:59 +0200

    Implemented benchmark test for the appendBlockBottom method.

--- Benchmark/append-block-bottom.spec.ts
+++ Benchmark/append-block-bottom.spec.ts
@@ -1,27 +1,80 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
-import { Matrix } from "../src/index.ts";
-const rand1 = Matrix.random(5000, 5000);
-const rand2 = Matrix.random(5000, 5000);
-(async () =>
-  new validator(true).describe("appendBlockBottom performance:")
-    .test({
-      title: true,
-      success: "green",
-      error: "red",
-    }).isBoolean
-    .on(true, () => {
-      const r1 = rand1.M;
-      const r2 = rand2.M;
-      const benchmark1 = new validator(rand1).benchmark((m) =>
-        m.appendBlockBottom(rand2)
-      );
-      const benchmark2 = new validator([r1, r2]).benchmark((data) => {
-        return [...data[0], ...data[1]];
-      });
+import { Matrix } from "../src";
+import {
+  Integer,
+  MatrixType,
+  NumericMatrix,
+  TypedArray,
+} from "../src/Matrix/types";
+import { dimensions, startPerformanceTest } from "./utils";
 
-      console.table({
-        "@euriklis/mathematics": benchmark1,
-        "numericjs": benchmark2,
-      });
-    }))();
+(async () => {
+  const appendBloackBottomIterator = (m1, m2, k = 0) => {
+    const n1: Integer = m1.length;
+    const n2: Integer = m2.length;
+    const extendedMatrix: NumericMatrix | number[] = [];
+    let i: Integer, j: Integer;
+    if (k) {
+      for (i = 0; i < n1 >> 2; i++) {
+        j = i << 2;
+        (extendedMatrix as number[])[j] = m1[j++];
+        (extendedMatrix as number[])[j] = m1[j++];
+        (extendedMatrix as number[])[j] = m1[j++];
+        (extendedMatrix as number[])[j] = m1[j];
+      }
+
+      for (j = i << 2; j < n1; j++) {
+        (extendedMatrix as number[])[j] = m1[j];
+      }
+    } else {
+      for (i = n1; i--;) {
+        (extendedMatrix as NumericMatrix)[i] = appendBloackBottomIterator(
+          m1[i],
+          m2[i],
+          k + 1,
+        ) as number[];
+      }
+
+      for (i = n2; i--;) {
+        (extendedMatrix as NumericMatrix)[i + n1] = appendBloackBottomIterator(
+          m2[i],
+          m1[i],
+          k + 1,
+        ) as number[];
+      }
+    }
+
+    return extendedMatrix;
+  };
+
+  const appendBlockBottom = (
+    m1: MatrixType | NumericMatrix | number[] | TypedArray,
+    m2: MatrixType | NumericMatrix | number[] | TypedArray,
+  ): NumericMatrix | number[] => {
+    return appendBloackBottomIterator(m1, m2);
+  };
+  // Matrix.setType("generic")
+  const rand1 = Matrix.uniqueRandom(...dimensions);
+  const rand2 = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.appendBlockBottom(rand1, rand2),
+    appendBlockBottom(rand1, rand2) as NumericMatrix,
+  );
+  const euriklisTest = (m: any) => m.appendBlockBottom(rand1, rand2);
+  const numericTest = (_: any) => appendBlockBottom(rand1, rand2);
+  startPerformanceTest(
+    "appendBlockBottom",
+    [{
+      param: "matrix",
+      dimensions,
+      type: "float64",
+    }, {
+      param: "block",
+      dimensions,
+      type: "float64",
+    }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
+})();
