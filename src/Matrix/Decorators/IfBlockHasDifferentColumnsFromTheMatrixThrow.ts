ca528009e8200b7abadf272cba3af59c6dda190c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 10:41:15 +0200

    Implemented decorator for the appendBlockBottom method.

---
+++ src/Matrix/Decorators/IfBlockHasDifferentColumnsFromTheMatrixThrow.ts
@@ -0,0 +1,29 @@
+"use strict";
+
+import { MatrixType, NumericMatrix, NumericType } from "../types";
+
+export function ifBlockHasDifferentColumnsFromTheMatrixThrow(
+  error: Function,
+): (_: any, __: string, descriptor: PropertyDescriptor) => void {
+  return function (
+    _: any,
+    __: string,
+    descriptor: PropertyDescriptor,
+  ): void {
+    const method: (
+      matrix: MatrixType | NumericMatrix,
+      block: MatrixType | NumericMatrix,
+      type: NumericType,
+    ) => MatrixType | NumericMatrix = descriptor.value;
+
+    descriptor.value = function (
+      matrix: MatrixType | NumericMatrix,
+      block: MatrixType | NumericMatrix,
+      type: NumericType,
+    ) {
+      if (matrix[0].length !== block[0].length) error();
+
+      return method.call(this, matrix, block, type);
+    };
+  };
+}
