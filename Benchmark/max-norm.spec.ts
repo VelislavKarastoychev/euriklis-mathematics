82ac0f86834909f6cddd6555575129ce60890646
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 23:21:23 +0200

    Implemented benchmark test for the maxNorm method.

--- Benchmark/max-norm.spec.ts
+++ Benchmark/max-norm.spec.ts
@@ -1,28 +1,19 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r1 = Matrix.random(2, 5, -1, 1);
-  const m1 = r1.M;
-  new validator(r1.maxNorm).isSame(numeric.sup(m1.flat()))
-    .on(true, (v) => {
-      v.describe("Time performance of max norm method or superior method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r1).benchmark((m) => m.maxNorm);
-      const t2 = new validator(m1).benchmark((m) => numeric.sup(m.flat()));
-      console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-    }).on(false, (v) => {
-      v.describe("Internal error in maxNorm method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const cr = Matrix.copy(r, "generic");
+  const condition = Math.abs(Matrix.maxNorm(r) - numeric.sup(cr.flat())) < 1e-8;
+  const euriklisTest = (m: any) => m.maxNorm(r);
+  const numericTest = (m: any) => m.sup(cr.flat());
+  startPerformanceTest(
+    "maxNorm",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
