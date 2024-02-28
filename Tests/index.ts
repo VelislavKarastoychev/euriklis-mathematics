75e1a2275fe25cb17743b62424e066ea17d4d5d4
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 00:21:15 +0200

    Added test for the sumOfAllElements method.

--- Tests/index.ts
+++ Tests/index.ts
@@ -43,7 +43,7 @@ import "./max-norm.spec.ts";
 import "./norm1.spec.ts";
 import "./superior.spec.ts";
 import "./inferior.spec.ts";
-// import "./sum-of-all-elements.spec.ts"
+import "./sum-of-all-elements.spec.ts"
 // import "./product-of-all-elements.spec.ts";
 // import "./sum-of-squares-of-all-elements.spec.ts";
 // import "./sum-of-cubes-of-all-elements.spec.ts";
