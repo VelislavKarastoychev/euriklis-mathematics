6e32fbf6d3689706a41ba0788d8cff322a3ceb53
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 10:38:07 +0200

    Fixed reshape test methods.

--- Tests/reshape.spec.ts
+++ Tests/reshape.spec.ts
@@ -4,32 +4,48 @@ import { Matrix } from "../src/index.ts";
 import validator from "@euriklis/validator-ts";
 
 const m = Matrix.random(3, 4);
-const reshaped = m.reshape(2, 6);
+const reshaped = Matrix.reshape(m, 2, 6);
 const reshapeMatrix = (rows: Integer, columns: Integer) =>
-  Matrix.random(12, 5).reshape(rows, columns);
+  Matrix.reshape(Matrix.random(12, 5), rows, columns);
 const r1 = Matrix.random(3, 11);
 new validator(m)
-  .isInstanceof(Matrix)
+  .isInstanceof(Array)
   .describe("The reshape method has to:")
   .test({
     title: true,
     success: "green",
     error: "red",
   });
-new validator(reshaped.M).isArrayOfNumberArraysWithEqualSize
+new validator(reshaped).isArrayOfNumberArraysWithEqualSize
   .and.bind(
-    new validator(reshaped.rows).isSame(2),
+    new validator(reshaped.length).isSame(2),
   ).and.bind(
-    new validator(reshaped.columns).isSame(6),
+    new validator(reshaped[0].length).isSame(6),
   ).and.bind(
-    new validator(reshaped.reshape(3, 4).isEqualTo(m)).isSame(true),
+    new validator(Matrix.isEqualTo(Matrix.reshape(reshaped, 3, 4), m)).isSame(
+      true,
+    ),
   )
   .describe(
     "1. Regroups the elements of the matrix according to the rows and the columns of the method.",
   )
   .test();
-new validator(r1.reshape(1, 33).reshape(33, 1).reshape(3, 11).isEqualTo(r1)).isSame(true)
-  .describe("2. Returns the correct answer when the columns are even number.").test()
+new validator(
+  Matrix.isEqualTo(
+    Matrix.reshape(
+      Matrix.reshape(
+        Matrix.reshape(r1, 1, 33),
+        33,
+        1,
+      ),
+      3,
+      11,
+    ),
+    r1,
+  ),
+).isSame(true)
+  .describe("2. Returns the correct answer when the columns are even number.")
+  .test();
 new validator(reshapeMatrix).throwsErrorWith(2, 4)
   .and.throwsErrorWith(-1, 60)
   .and.throwsErrorWith(Math.PI, 60 / Math.PI)
