520c753a29f3a100162d770d735559f2c2da2f45
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 22:32:34 +0200

    Added test for the infinityNorm method.

--- Tests/index.ts
+++ Tests/index.ts
@@ -38,7 +38,7 @@ import "./append-block-bottom.spec.ts";
 import "./reshape.spec.ts";
 import "./transpose.spec.ts";
 import "./frobenius-norm.spec.ts";
-// import "./infinity-norm.spec.ts";
+import "./infinity-norm.spec.ts";
 // import "./max-norm.spec.ts";
 // import "./norm1.spec.ts";
 // import "./superior.spec.ts";
