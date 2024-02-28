e43583369b274b1821e3ef89e53fa514425fbe10
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 12:28:54 +0200

    Implemented test for the transpose method.

--- Tests/transpose.spec.ts
+++ Tests/transpose.spec.ts
@@ -2,8 +2,9 @@
 import { Matrix } from "../src/index.ts";
 import validator from "@euriklis/validator-ts";
 const a = Matrix.random(27, 23);
-new validator(a.transpose().M).forEvery((row, i) =>
-  row.forEvery((el, j) => el.isSame(a.M[j as number][i as number]))
+const runTranspose = (m) => Matrix.transpose(m);
+new validator(Matrix.transpose(a, "generic")).forEvery((row, i) =>
+  row.forEvery((el, j) => el.isSame(a[j as number][i as number]))
 ).describe("transpose method has to:").test({
   title: true,
   success: "green",
@@ -11,6 +12,14 @@ new validator(a.transpose().M).forEvery((row, i) =>
 })
   .describe("1. Has to produce the correct result").test();
 
-new validator(a.transpose().transpose().M).isSame(a.M).describe(
-  "2. return the initial matrix, when is called again",
-).test();
+new validator(Matrix.transpose(Matrix.transpose(a), "generic")).isSame(
+  Matrix.copy(a, "generic"),
+)
+  .describe(
+    "2. return the initial matrix, when is called again",
+  ).test();
+
+new validator(runTranspose)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .describe("3. throws error when the input matrix is not correcly defined.")
+  .test();
