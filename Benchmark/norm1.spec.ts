cbbac8ae500e41168955d1c44f34c7747b407d8c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 23:21:41 +0200

    Implemented benchmark test for the norm1 method.

--- Benchmark/norm1.spec.ts
+++ Benchmark/norm1.spec.ts
@@ -1,39 +1,25 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
-const m = Matrix.random(5000, 5000);
-const md = m.M;
-console.log(m.norm1);
-console.log(
-  numeric.sup(numeric.transpose(md).map(numeric.norm1)),
-);
 (async () => {
-  new validator(
-    m.norm1 -
-        numeric.sup(numeric.transpose(md).map(numeric.norm1)) <
-      1e-8,
-  ).isSame(true)
-    .on(true, (v) => {
-      v.describe("  Time performance of norm1 method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(m).benchmark((m) => m.norm1, 20);
-      const t2 = new validator(md).benchmark(
-        (m) => numeric.sup(numeric.transpose(m).map(numeric.norm1)),
-        20,
-      );
-      console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-    }).on(false, (v) => {
-      v.describe("Internal error in norm1 benchmark method test.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const cr = Matrix.copy(r, "generic");
+  const condition =
+    Math.abs(
+      Matrix.norm1(r) - numeric.sup(
+        numeric.transpose(cr).map((m) => numeric.norm1(m)),
+      ),
+    ) < 1e-8;
+  const euriklisTest = (m: any) => m.norm1(r);
+  const numericTest = (m: any) =>
+    m.sup(numeric.transpose(cr).map((row: number[]) => numeric.norm1(row)));
+  startPerformanceTest(
+    "norm1",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
