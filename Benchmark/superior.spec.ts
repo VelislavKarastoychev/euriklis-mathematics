d276eff9a1031345f3e6304240ce355e10545824
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 23:43:21 +0200

    Implemented superior method benchmark test.

--- Benchmark/superior.spec.ts
+++ Benchmark/superior.spec.ts
@@ -3,26 +3,20 @@ import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
 import { validateHeaderValue } from "http";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const m = Matrix.random(5000, 5000, -1, 1);
-  new validator(m.superior - numeric.sup(m.M.flat())).isInRange(-1e-8, 1e-8)
-    .on(true, (v) => {
-      v.describe("Time performance of the superior method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(m).benchmark((m) => m.superior);
-      const t2 = new validator(m.M).benchmark((m) => numeric.sup(m.flat()));
-      console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-    }).on(false, (v) => {
-      v.describe("Internal error in superior benchmark test.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const cr = Matrix.copy(r, "generic");
+  const condition =
+    Math.abs(Matrix.superior(r) - numeric.sup(cr.flat())) < 1e-8;
+  const euriklisTest = (m: any) => m.superior(r);
+  const numericTest = (m: any) => m.sup(cr.flat());
+  startPerformanceTest(
+    "superior",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
