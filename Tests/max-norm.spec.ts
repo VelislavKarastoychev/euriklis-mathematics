851c314231c2cf2a26fd3338dc8178f2555feff5
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 23:04:25 +0200

    Implemented test for the maxNorm method.

--- Tests/max-norm.spec.ts
+++ Tests/max-norm.spec.ts
@@ -1,13 +1,15 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const matrix = new Matrix([[1, 2, 3], [-4, 5, 6], [7, 8, -9]]);
-const m = Matrix.random(4, 4);
-const M = m.M;
+const matrix = [[1, 2, 3], [-4, 5, 6], [7, 8, -9]];
+let m = Matrix.random(4, 4);
+const M = Matrix.copy(m, "generic");
 M[2][2] = Math.PI;
-m.M = M;
-new validator(matrix.maxNorm)
+m = Matrix.copy(M);
+const callMaxNorm = (m: MatrixType | NumericMatrix) => Matrix.maxNorm(m);
+new validator(Matrix.maxNorm(matrix))
   .describe("The maxNorm getter method has to:")
   .test({
     title: true,
@@ -16,9 +18,15 @@ new validator(matrix.maxNorm)
   })
   .isSame(9)
   .and.bind(
-    new validator(m.maxNorm).isSame(Math.PI),
+    new validator(Matrix.maxNorm(m)).isSame(Math.PI),
   )
   .describe(
-    "1. Returns the element with the greates absolute value of the matrix",
+    "1. returns the element with the greates absolute value of the matrix",
   )
+  .test();
+
+new validator(callMaxNorm)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("This throws!")
+  .describe("2. throw error when the matrix is incorrectly defined.")
   .test();
