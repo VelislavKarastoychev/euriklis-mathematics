0745566e56a851a8ebf9f4038eeac7d6e8d440ac
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 00:09:53 +0200

    Implemented benchmark test for the inferior method.

--- Benchmark/inferior.spec.ts
+++ Benchmark/inferior.spec.ts
@@ -1,25 +1,20 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
-const m = Matrix.random(5000, 5000, -1, 1);
-new validator(m.inferior - numeric.inf(m.M.flat())).isInRange(-1e-8, 1e-8)
-  .on(true, (v) => {
-    v.describe("Time performance of inferior getter method:")
-      .test({
-        title: true,
-        success: "green",
-        error: "red",
-      });
-    const t1 = new validator(m).benchmark((m) => m.inferior);
-    const t2 = new validator(m.M).benchmark((m) => numeric.inf(m.flat()));
-    console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-  }).on(false, (v) => {
-    v.describe("Internal error in inferior benchmark test.")
-      .test({
-        title: true,
-        success: "red",
-        "error": "green",
-      });
-  });
+(async () => {
+  const r = Matrix.uniqueRandom(...dimensions);
+  const cr = Matrix.copy(r, "generic");
+  const condition =
+    Math.abs(Matrix.inferior(r) - numeric.inf(cr.flat())) < 1e-7;
+  const euriklisTest = (m: any) => m.inferior(r);
+  const numericTest = (m: any) => m.inf(cr.flat());
+  startPerformanceTest(
+    "inferior",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
+})();
