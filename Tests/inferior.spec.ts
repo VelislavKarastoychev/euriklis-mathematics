77d76df7a18407339ad52d1c8bfe5371c43883cc
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 00:10:14 +0200

    Implemented test for the inferior method.

--- Tests/inferior.spec.ts
+++ Tests/inferior.spec.ts
@@ -1,14 +1,16 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const m = new Matrix([
+const m = [
   [-3, 5, 7],
   [2, 6, 4],
   [0, 2, 8],
-]);
+];
 
-new validator(m.inferior)
+const callInferior = (m: MatrixType | NumericMatrix) => Matrix.inferior(m);
+new validator(Matrix.inferior(m))
   .describe("The inferior method has to:")
   .test({
     title: true,
@@ -17,4 +19,10 @@ new validator(m.inferior)
   }).isSame(-3).describe(
     "1. return the correct value (the element with the minimal value in the matrix).",
   )
+  .test();
+
+new validator(callInferior)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws!")
+  .describe("2. throw error when the matrix is incorrectly defined.")
   .test();
