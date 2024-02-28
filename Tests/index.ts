8587641f2389cb2da64be642aaf76a955f44f824
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 10:37:47 +0200

    Added test for the appendBlockBottom method.

--- Tests/index.ts
+++ Tests/index.ts
@@ -34,7 +34,7 @@ import "./exchange-columns.spec.ts";
 import "./get-diagonal.spec.ts";
 import "./to-diagonal-matrix.spec.ts";
 import "./append-block-right.spec.ts";
-// import "./append-block-bottom.spec.ts";
+import "./append-block-bottom.spec.ts";
 // import "./transpose.spec.ts";
 // import "./frobenius-norm.spec.ts";
 // import "./infinity-norm.spec.ts";
