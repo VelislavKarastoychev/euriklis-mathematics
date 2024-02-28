0dbfc355fd15bfa8a858e04179bf65204f1b11f1
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 12:28:28 +0200

    Implemented benchmark test for the transpose method.

--- Benchmark/transpose.spec.ts
+++ Benchmark/transpose.spec.ts
@@ -1,28 +1,21 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
-import { Matrix } from "../src/index.js";
+import { Matrix } from "../src";
+import { dimensions, startPerformanceTest } from "./utils";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.transpose().isEqualTo(numeric.transpose(m))).isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of transpose method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.transpose());
-      const t2 = new validator(m).benchmark((m) => numeric.transpose(m));
-      console.table({ "@euriklis/mathematics": t1, numericjs: t2 });
-    }).on(false, (v) => {
-      v.describe("Internal error in transpose method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const m = Matrix.random(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.transpose(m),
+    numeric.transpose(m),
+  );
+  const test = (lib: any) => lib.transpose(m);
+
+  startPerformanceTest(
+    "transpose",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
