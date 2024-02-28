6ba62c0c927f5bfb43bf3efc42c9ec91212f2cae
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-28 23:22:07 +0200

    Implemented test for the norm1 method.

--- Tests/norm1.spec.ts
+++ Tests/norm1.spec.ts
@@ -1,22 +1,28 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const m = new Matrix(
-  [
+const m = [
     [-3, 5, 7],
     [2, 6, 4],
     [0, 2, 8],
-  ],
-);
+  ];
 
-new validator(m.norm1).isSame(19)
+const callNorm1 = (m: MatrixType | NumericMatrix) => Matrix.norm1(m);
+new validator(Matrix.norm1(m)).isSame(19)
   .describe("The norm1 method has to:")
   .test({
     title: true,
     success: "green",
     error: "red",
   }).describe(
-    "1. Compoute the correct result of the maximum absolute column sum for a matrix.",
+    "1. compoute the correct result of the maximum absolute column sum for a matrix.",
   )
+  .test();
+
+new validator(callNorm1)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("This throws")
+  .describe("2. throws error when the matrix is incorrectly defined.")
   .test();
