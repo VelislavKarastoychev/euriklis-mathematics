3aa778cf7044c6a56df3579c1a3ff912247188e7
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 11:08:25 +0200

    Implemented sumOfSquaresOfAllElements.

--- Benchmark/sum-of-squares-of-all-elements.spec.ts
+++ Benchmark/sum-of-squares-of-all-elements.spec.ts
@@ -1,34 +1,23 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const m = Matrix.random(5000, 5000);
-  const md = m.M;
-  new validator(m.sumOfSquaresOfAllElements - numeric.mapreduce("accum += xi * xi", "0")(md))
-    .isInRange(-1e-6, 1e-6)
-    .on(true, (v) => {
-      v.describe("Time performance of the sumOfSquaresOfAllElements method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(m).benchmark((m) => m.sumOfSquaresOfAllElements);
-      const t2 = new validator(md).benchmark((m) =>
-        numeric.mapreduce("accum += xi * xi", "0")(m)
-      );
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in sumOfSquaresOfAllElements.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const rc = Matrix.copy(r, "generic");
+  const condition =
+    Math.abs(
+      Matrix.sumOfSquaresOfAllElements(r) -
+        numeric.mapreduce("accum += xi*xi", "0")(rc),
+    ) < 1e-6;
+  const euriklisTest = (m: any) => m.sumOfSquaresOfAllElements(r);
+  const numericTest = (m: any) => m.mapreduce("accum += xi * xi", "0")(rc);
+  startPerformanceTest(
+    "sumOfSquaresOfAllElements",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
