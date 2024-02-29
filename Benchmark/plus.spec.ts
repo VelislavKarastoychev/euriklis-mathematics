e9e455759fed52011c9188a0e059f9370e4a9d0c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 12:04:52 +0200

    Implemented benchmark test for the plus method.

--- Benchmark/plus.spec.ts
+++ Benchmark/plus.spec.ts
@@ -3,35 +3,22 @@ import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
 import { NumericMatrix } from "../src/Matrix/types.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const rm = r.M;
-  const  halfs = Matrix.replicate(0.5, 5000, 5000);
-  const halfsm = halfs.M;
-  new validator(r.plus(halfs).isEqualTo(
-    ((numeric.add(rm, halfsm)) as boolean[][]).map(row => row.map((el: boolean) => +el)) as NumericMatrix)
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of plus method:")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-      const t1 = new validator(r).benchmark((m) => m.plus(halfs));
-      const t2 = new validator(rm).benchmark((m) => numeric.add(m, halfsm));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in plus benchmark test.")
-       .test({
-          title: true,
-          success: "green",
-          error: "red"
-        });
-    })
+  const r = Matrix.uniqueRandom(...dimensions);
+  // const rt2 = Matrix.copy(r, undefined, 2, 0);
+  const condition = Matrix.isEqualTo(
+    Matrix.plus(r, r),
+    numeric.add(r, r),
+  );
+  const euriklisTest = (m: any) => m.plus(r, r);
+  const numericTest = (m: any) => m.add(r, r);
+  startPerformanceTest(
+    "plus",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
