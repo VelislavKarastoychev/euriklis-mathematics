ea08b49d32821a1a85772e9260df4149eb83489b
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 11:08:56 +0200

    Implemented test for the sumOfSquaresOfAllElemnts method.

--- Tests/sum-of-squares-of-all-elements.spec.ts
+++ Tests/sum-of-squares-of-all-elements.spec.ts
@@ -1,17 +1,26 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const m = new Matrix([
+const m = [
   [-3, 5, 7],
   [2, 6, 4],
   [0, 2, 8],
-]);
-new validator(m.sumOfSquaresOfAllElements).isSame(207)
+];
+
+const callSumOfSquares = (m: MatrixType | NumericMatrix) => Matrix.sumOfSquaresOfAllElements(m);
+new validator(Matrix.sumOfSquaresOfAllElements(m)).isSame(207)
   .describe("The sumOfSquaresOfAllElements getter method has to:")
   .test({
     title: true,
     success: "green",
     error: "red",
-  }).describe("1. Compute the correct result (never negative or NaN result).")
+  }).describe("1. compute the correct result (never negative or NaN result).")
+  .test();
+
+new validator(callSumOfSquares)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("2. throw error when the matrix is incorrectly declared.")
   .test();
