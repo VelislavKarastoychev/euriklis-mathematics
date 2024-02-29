763664951471aa4ba80f368556fe76a372949b8c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 12:05:15 +0200

    Implemented benchmark test for the sumOfCubes method.

--- Benchmark/sum-of-cubes-of-all-elements.spec.ts
+++ Benchmark/sum-of-cubes-of-all-elements.spec.ts
@@ -1,29 +1,23 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const m = Matrix.random(5000, 5000);
-  const md = m.M;
-  new validator(
-    m.sumOfCubesOfAllElements -
-      numeric.mapreduce("accum += xi * xi * xi", "0")(md),
-  ).isInRange(-1e-6, 1e-6)
-    .on(true, (v) => {
-      v.describe("Time performance of sumOfCubesOfAllElements getter method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(m).benchmark((m) => m.sumOfCubesOfAllElements);
-      const t2 = new validator(md).benchmark((m) =>
-        numeric.mapreduce("accum += xi * xi * xi", "0")(m)
-      );
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const rc = Matrix.copy(r, "generic");
+  const condition =
+    Math.abs(
+      Matrix.sumOfCubesOfAllElements(r) -
+        numeric.mapreduce("accum += xi * xi * xi", "0")(rc),
+    ) < 1e-5;
+  const euriklisTest = (m: any) => m.sumOfCubesOfAllElements(r);
+  const numericTest = (m: any) => m.mapreduce("accum += xi * xi * xi", "0")(rc);
+  startPerformanceTest(
+    "sumOfCubesOfAllElements",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
