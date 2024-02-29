98862a69261281fc3270c10a72af4dd8b794f40c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 10:51:27 +0200

    Implemented benchmark test for the productOfAllElements method.

--- Benchmark/product-of-all-elements.spec.ts
+++ Benchmark/product-of-all-elements.spec.ts
@@ -1,34 +1,20 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const m = Matrix.random(5, 5, 1, 2);
-  const md = m.M;
-  new validator(m.productOfAllElements - numeric.prod(md)).isInRange(
-    -1e-8,
-    1e-8,
-  )
-    .on(true, (v) => {
-      v.describe("Time performance of productOfAllElements getter method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(m).benchmark((m) => m.productOfAllElements);
-      const t2 = new validator(md).benchmark((m) => numeric.prod(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in productOfAllElements benchmark test.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const rc = Matrix.copy(r, "generic");
+  const condition =
+    Math.abs(Matrix.productOfAllElements(r) - numeric.prod(rc)) < 1e-8;
+  const euriklisTest = (m: any) => m.productOfAllElements(r);
+  const numericTest = (m: any) => m.prod(rc);
+  startPerformanceTest(
+    "productOfAllElements",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
