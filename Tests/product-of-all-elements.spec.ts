a89bc3fd221a33924a458bbefea422fdebe2ab40
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-29 10:51:54 +0200

    Implemented test for the productOfAllElements method.

--- Tests/product-of-all-elements.spec.ts
+++ Tests/product-of-all-elements.spec.ts
@@ -1,14 +1,16 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
-const m = new Matrix([
+const m = [
   [-3, 5, 7],
   [2, 6, 4],
   [0, 2, 8],
-]);
+];
 
-new validator(m.productOfAllElements).isSame(0)
+const callProductsOfAllElements = (m: MatrixType | NumericMatrix) => Matrix.productOfAllElements(m); 
+new validator(Matrix.productOfAllElements(m)).isSame(0)
   .describe("The productOfAllElements getter method has to:")
   .test({
     title: true,
@@ -17,4 +19,10 @@ new validator(m.productOfAllElements).isSame(0)
   }).describe(
     "1. return the correct result (the product of all matrix elements).",
   )
+  .test();
+
+new validator(callProductsOfAllElements)
+  .throwsErrorWith([[1, 2, 3], [1,23], [123]])
+  .throwsErrorWith("this throws!")
+  .describe("2. throws error when the matrix is incorrectly defined.")
   .test();
