d891e12e809b3a312c3c86d635f867f3996ba270
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 11:30:35 +0200

    Implemented benchmark test for the tan method.

--- Benchmark/tan.spec.ts
+++ Benchmark/tan.spec.ts
@@ -2,34 +2,20 @@
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.tan().isEqualTo(numeric.tan(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the tan method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.tan());
-      const t2 = new validator(m).benchmark((m) => numeric.tan(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in tan benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.tan(r),
+    numeric.tan(r),
+  );
+  const test = (m: any) => m.tan(r);
+  startPerformanceTest(
+    "tan",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
