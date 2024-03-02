5135ea7830437e58245b80595a49573ed724843e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 10:54:29 +0200

    Implemented test for the sin method.

--- Tests/sin.spec.ts
+++ Tests/sin.spec.ts
@@ -1,14 +1,20 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const sin = r.M.map((r: number[]) => r.map((c: number) => Math.sin(c)));
-const sinWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.sin(2 * c + 3))
+const sin = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.sin(c))
 );
+const sinWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.sin(2 * c + 3)));
 
-new validator(r.sin().isEqualTo(sin))
+const callSin = (
+  matrix: MatrixType | NumericMatrix,
+): MatrixType | NumericMatrix => Matrix.sin(matrix);
+new validator(Matrix.isEqualTo(Matrix.sin(r), sin))
   .describe("The sin method has to:")
   .test({
     title: true,
@@ -18,7 +24,13 @@ new validator(r.sin().isEqualTo(sin))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.sin(2, 3).isEqualTo(sinWeighted))
+new validator(Matrix.isEqualTo(Matrix.sin(r, 2, 3), sinWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callSin)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this thorws")
+  .describe("3. throws error when the matrix is incorrectly defined.")
   .test();
