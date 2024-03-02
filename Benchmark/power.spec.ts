c61a054106040572331c7919da7df4332910e921
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 08:39:21 +0200

    Implemented benchmark test for the power method.

--- Benchmark/power.spec.ts
+++ Benchmark/power.spec.ts
@@ -1,33 +1,21 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
-import { Matrix } from "../src/index.ts";
+import { Matrix } from "../src";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils";
 (async () => {
-  const r1 = Matrix.random(5000, 5000);
-  const r2 = Matrix.random(5000, 5000, 1, 2);
-  const m1 = r1.M;
-  const m2 = r2.M;
-  new validator(r2.power(r1).isEqualTo(numeric.pow(m2, m1)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of the power method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r2).benchmark((m) => m.power(r1));
-      const t2 = new validator(m2).benchmark((m) => numeric.pow(m, m1));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in power method.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const r1 = Matrix.uniqueRandom(...dimensions);
+  const r2 = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.power(r1, r2),
+    numeric.pow(r1, r2),
+  );
+  const euriklisTest = (m: any) => m.power(r1, r2);
+  const numericTest = (m: any) => m.pow(r1, r2);
+  startPerformanceTest(
+    "power",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
