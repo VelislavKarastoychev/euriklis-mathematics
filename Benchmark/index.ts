14362e7a83ce2ad5bcb788a1ac97623f568b850e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 10:07:32 +0200

    Added benchmark test for the negate method.

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -52,8 +52,8 @@
 // import "./power.spec.ts";
 // import "./Hadamard.spec.ts";
 // import "./divide.spec.ts";
-import "./modulus.spec.ts";
-// import "./negate.spec.ts";
+// import "./modulus.spec.ts";
+import "./negate.spec.ts";
 // import "./bitwise-negate.spec.ts";
 // import "./sin.spec.ts";
 // import "./cos.spec.ts";
