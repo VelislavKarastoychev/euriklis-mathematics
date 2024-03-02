8b58344631ee75569a47cf2e683d896aec0afe0d
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 11:12:08 +0200

    Implemented test for the cos method.

--- Tests/cos.spec.ts
+++ Tests/cos.spec.ts
@@ -1,14 +1,18 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const cos = r.M.map((r: number[]) => r.map((c: number) => Math.cos(c)));
-const cosWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.cos(2 * c + 3))
+const cos = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.cos(c))
 );
+const cosWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.cos(2 * c + 3)));
 
-new validator(r.cos().isEqualTo(cos))
+const callCos = (matrix: MatrixType | NumericMatrix) => Matrix.cos(matrix);
+new validator(Matrix.isEqualTo(Matrix.cos(r), cos))
   .describe("The cos method has to:")
   .test({
     title: true,
@@ -18,7 +22,13 @@ new validator(r.cos().isEqualTo(cos))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.cos(2, 3).isEqualTo(cosWeighted))
+new validator(Matrix.isEqualTo(Matrix.cos(r, 2, 3), cosWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callCos)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix is incorrectly defined.")
   .test();
