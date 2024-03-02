454350d06800d113cc8cf0ba6d3d62033ffc3897
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 09:27:35 +0200

    Added benchmark test for the divide method.

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -50,8 +50,8 @@
 // import "./plus.spec.ts";
 // import "./minus.spec.ts";
 // import "./power.spec.ts";
-import "./Hadamard.spec.ts";
-// import "./divide.spec.ts";
+// import "./Hadamard.spec.ts";
+import "./divide.spec.ts";
 // import "./modulus.spec.ts";
 // import "./negate.spec.ts";
 // import "./bitwise-negate.spec.ts";
