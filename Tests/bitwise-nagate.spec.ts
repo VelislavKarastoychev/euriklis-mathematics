5ebf1b7b52a2097b085eac84d62514f409d44710
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 10:41:30 +0200

    Implemented test for the bitwiseNegate method.

--- Tests/bitwise-nagate.spec.ts
+++ Tests/bitwise-nagate.spec.ts
@@ -1,11 +1,19 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const bneg = r.M.map((r: number[]) => r.map((c: number) => ~c));
-const bneg2 = r.M.map((r: number[]) => r.map((c: number)=> ~(2 * c)));
-new validator(r.bitwiseNegate().isEqualTo(bneg))
+const bneg = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => ~c)
+);
+const bneg2 = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => ~(2 * c))
+);
+const callBitwiseNegate = (matrix: MatrixType | NumericMatrix):
+  | MatrixType
+  | NumericMatrix => Matrix.bitwiseNegate(matrix);
+new validator(Matrix.isEqualTo(Matrix.bitwiseNegate(r), bneg))
   .describe("The bitwiseNegate method has to:")
   .test({
     title: true,
@@ -17,7 +25,13 @@ new validator(r.bitwiseNegate().isEqualTo(bneg))
   )
   .test();
 
-new validator(r.bitwiseNegate(2).isEqualTo(bneg2))
+new validator(Matrix.isEqualTo(Matrix.bitwiseNegate(r, 2), bneg2))
   .describe("2. return the correct result when a weight and bias are defined.")
   .isSame(true)
+  .test();
+
+new validator(callBitwiseNegate)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix is incorrectly defined.")
   .test();
