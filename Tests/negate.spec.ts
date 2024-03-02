91ede4abb9f2f351d858afc59845fbeaf4418bc6
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 10:08:04 +0200

    Implemented test for the negate method.

--- Tests/negate.spec.ts
+++ Tests/negate.spec.ts
@@ -1,10 +1,15 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r1 = Matrix.random(3, 4);
 const zeros = Matrix.zeros(3, 4);
-new validator(r1.plus(r1.negate()).isEqualTo(zeros))
+const callNegate = (matrix: MatrixType | NumericMatrix) =>
+  Matrix.negate(matrix);
+new validator(
+  Matrix.isEqualTo(Matrix.plus(r1, Matrix.negate(r1)), zeros),
+)
   .describe("The negate getter method has to:")
   .test({
     title: true,
@@ -16,7 +21,21 @@ new validator(r1.plus(r1.negate()).isEqualTo(zeros))
   )
   .test();
 
-new validator(r1.Hadamard(2).plus(r1.negate(2)).isEqualTo(zeros))
+new validator(
+  Matrix.isEqualTo(
+    Matrix.plus(
+      Matrix.Hadamard(r1, 2),
+      Matrix.negate(r1, 2),
+    ),
+    zeros,
+  ),
+)
   .isSame(true)
   .describe("2. return the correct result when weight and bias are defined.")
+  .test();
+
+new validator(callNegate)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
