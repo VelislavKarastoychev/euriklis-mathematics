c56e2bb379988e78939d347e230d8f3c5a16a064
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-02 08:39:06 +0200

    Added benchmark test for the power method.

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -48,8 +48,8 @@
 // import "./sum-of-squares-of-all-elements.spec.ts";
 // import "./sum-of-cubes-of-all-elements.spec.ts";
 // import "./plus.spec.ts";
-import "./minus.spec.ts";
-// import "./power.spec.ts";
+// import "./minus.spec.ts";
+import "./power.spec.ts";
 // import "./Hadamard.spec.ts";
 // import "./divide.spec.ts";
 // import "./modulus.spec.ts";
