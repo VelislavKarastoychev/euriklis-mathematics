666c986b8a0a987e686c37dd08c6d7aebfd7608f
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 18:48:44 +0200

    Implemented sqrt method.

--- src/Matrix/index.ts
+++ src/Matrix/index.ts
@@ -2226,34 +2226,38 @@ export class Matrix {
       bias,
     );
   }
-  //
-  // /**
-  //  * Applies the point-wise square root function to the elements of the Matrix.
-  //  *
-  //  * Optionally, a weight and bias can be applied to each element before computing the square root.
-  //  * The resulting value is computed as `Math.sqrt(weight * element + bias)`.
-  //  *
-  //  * @param {MatrixType | NumericMatrix} matrix - The matrix of the elements of which will
-  //  * be performaed a new matrix with elements equal to its square root values.
-  //  * @param {number} weight - A number to multiply each element before applying the square root function.
-  //  * @param {number} bias - A number to be added to each element before applying the square root function.
-  //  * @param {NumericType} type - The type of the output matrix elements.
-  //  * @returns {MatrixType | NumericMatrix} A new matrix with the square root function applied to its elements.
-  //  */
-  // static sqrt(
-  //   matrix: MatrixType | NumericMatrix,
-  //   weight: number = 1,
-  //   bias: number = 0,
-  //   type: NumericType = Matrix._type,
-  // ): MatrixType | NumericMatrix {
-  //   return models.UnaryPointwise(
-  //     matrix,
-  //     "sqrt",
-  //     type,
-  //     weight,
-  //     bias,
-  //   );
-  // }
+
+  /**
+   * Applies the point-wise square root function to the elements of the Matrix.
+   *
+   * Optionally, a weight and bias can be applied to each element before computing the square root.
+   * The resulting value is computed as `Math.sqrt(weight * element + bias)`.
+   * Be careful for the elements of the matrix. Ensure that all elements are positive before run the
+   * method.
+   *
+   * @param {MatrixType | NumericMatrix} matrix - The matrix of the elements of which will
+   * be performaed a new matrix with elements equal to its square root values.
+   * @param {number} weight - A number to multiply each element before applying the square root function.
+   * @param {number} bias - A number to be added to each element before applying the square root function.
+   * @param {NumericType} type - The type of the output matrix elements.
+   * @returns {MatrixType | NumericMatrix} A new matrix with the square root function applied to its elements.
+   * @throws {Error} If the matrix parameter is incorrectly defined.
+   */
+  @ifIsNotArrayOfArraysWithEqualSizeThrow(errors.IncorrectMatrixInput)
+  static sqrt(
+    matrix: MatrixType | NumericMatrix,
+    weight: number = 1,
+    bias: number = 0,
+    type: NumericType = Matrix._type,
+  ): MatrixType | NumericMatrix {
+    return models.UnaryPointwise(
+      matrix,
+      "sqrt",
+      type,
+      weight,
+      bias,
+    );
+  }
   //
   // /**
   //  * Applies the point-wise natural logarithm function to the elements of the Matrix.
