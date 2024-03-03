3022e3812ac82d09b1f44240fce961d0e9a988b2
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 19:17:49 +0200

    Implemented benchmark test for the floor method.

--- Benchmark/floor.spec.ts
+++ Benchmark/floor.spec.ts
@@ -1,35 +1,20 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.floor().isEqualTo(numeric.floor(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the floor method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.floor());
-      const t2 = new validator(m).benchmark((m) => numeric.floor(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in floor benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.floor(r),
+    numeric.floor(r),
+  );
+  const test = (lib: any) => lib.floor(r);
+  startPerformanceTest(
+    "floor",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
