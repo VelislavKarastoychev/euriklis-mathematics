a674b6eb55729e4f37c82ccc99a61b0b3387bc1d
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 11:20:52 +0200

    Implemented test for the arccotan method.

--- Tests/arccotan.spec.ts
+++ Tests/arccotan.spec.ts
@@ -1,14 +1,20 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const arccotan = r.M.map((r: number[]) => r.map((c: number) => Math.PI/2 - Math.atan(c)));
-const arccotanWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.PI/2 - Math.atan((2 * c + 3)))
-);
+const arccotan = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.PI / 2 - Math.atan(c)));
+const arccotanWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.PI / 2 - Math.atan(2 * c + 3)));
+const callArcCotan = (
+  m: MatrixType | NumericMatrix,
+): MatrixType | NumericMatrix => Matrix.arccotan(m);
 
-new validator(r.arccotan().isEqualTo(arccotan))
+new validator(Matrix.isEqualTo(Matrix.arccotan(r), arccotan))
   .describe("The arccotan method has to:")
   .test({
     title: true,
@@ -18,7 +24,13 @@ new validator(r.arccotan().isEqualTo(arccotan))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.arccotan(2, 3).isEqualTo(arccotanWeighted))
+new validator(Matrix.isEqualTo(Matrix.arccotan(r, 2, 3), arccotanWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callArcCotan)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
