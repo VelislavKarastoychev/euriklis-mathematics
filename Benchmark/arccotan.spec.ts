2109fda6962f0dcc8a7b69e43227a5d7d907bab9
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 11:20:26 +0200

    Implemented benchmark test for the arccotan method.

--- Benchmark/arccotan.spec.ts
+++ Benchmark/arccotan.spec.ts
@@ -1,35 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.arccotan().isEqualTo(numeric.pointwise(['x[i]'], 'ret[i] = Math.PI/2 - Math.atan(x[i])')(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the arccotan method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.arccotan());
-      const t2 = new validator(m).benchmark((m) => numeric.pointwise(['x[i]'], 'ret[i] = Math.PI/2 - Math.atan(x[i])')(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in arccotan benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.arccotan(r),
+    numeric.pointwise(["x[i]"], "ret[i] = Math.PI/2 - Math.atan(x[i])")(r),
+  );
+  const euriklisTest = (m: any) => Matrix.arccotan(r);
+  const numericTest = (m: any) =>
+    numeric.pointwise(["x[i]"], "ret[i] = Math.PI/2 - Math.atan(x[i])")(r);
+  startPerformanceTest(
+    "arccotan",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
