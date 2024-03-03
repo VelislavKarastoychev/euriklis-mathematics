9e7f3c38db34402db3b0400894368ba19b026255
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 19:51:41 +0200

    Implemented test for the ReLU method.

--- Tests/ReLU.spec.ts
+++ Tests/ReLU.spec.ts
@@ -1,14 +1,19 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const ReLU = r.M.map((r: number[]) => r.map((c: number) => c <= 0 ? -1 : c));
-const ReLUWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => (2 * c + 3) <= 0 ? -1 : 2 * c + 3)
+const ReLU = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => c <= 0 ? -1 : c)
 );
+const ReLUWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => (2 * c + 3) <= 0 ? -1 : 2 * c + 3));
+const callReLU = (m: MatrixType | NumericMatrix): NumericMatrix | MatrixType =>
+  Matrix.ReLU(m);
 
-new validator(r.ReLU().isEqualTo(ReLU))
+new validator(Matrix.isEqualTo(Matrix.ReLU(r), ReLU))
   .describe("The ReLU method has to:")
   .test({
     title: true,
@@ -18,7 +23,13 @@ new validator(r.ReLU().isEqualTo(ReLU))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.ReLU(2, 3).isEqualTo(ReLUWeighted))
+new validator(Matrix.isEqualTo(Matrix.ReLU(r, 2, 3), ReLUWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callReLU)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
