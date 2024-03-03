12596d6fb3fc8cd6e61aa8bdf8ee73a1982aeddc
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 10:20:47 +0200

    Implemented benchmark test for the cotanh method.

--- Benchmark/cotanh.spec.ts
+++ Benchmark/cotanh.spec.ts
@@ -1,35 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.cotanh().isEqualTo(numeric.pointwise(['x[i]'], 'ret[i] = 1 / Math.tanh(x[i])')(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the cotanh method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.cotanh());
-      const t2 = new validator(m).benchmark((m) => numeric.pointwise(['x[i]'], 'ret[i] = 1 / Math.tanh(x[i])')(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in cotanh benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.cotanh(r),
+    numeric.pointwise(["x[i]"], "ret[i] = 1 / Math.tanh(x[i])")(r),
+  );
+  const euriklisTest = (m: any) => m.cotanh(r);
+  const numericTest = (m: any) =>
+    m.pointwise(["x[i]"], "ret[i] = 1 / Math.tanh(x[i])")(r);
+  startPerformanceTest(
+    "cotanh",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
