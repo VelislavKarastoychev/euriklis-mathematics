62aa29467025913c3c802d62a485dd35face1f01
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 08:50:53 +0200

    Implemented benchmark test for the cotan method.

--- Benchmark/cotan.spec.ts
+++ Benchmark/cotan.spec.ts
@@ -1,35 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.cotan().isEqualTo(numeric.pow(numeric.tan(m), -1)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the cotan method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.cotan());
-      const t2 = new validator(m).benchmark((m) => numeric.pow(numeric.tan(m), -1));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in cotan benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.cotan(r),
+    numeric.pointwise(["x[i]"], "ret[i] = 1/Math.tan(x[i])")(r),
+  );
+  const euriklisTest = (m: any) => m.cotan(r);
+  const numericTest = (m: any) =>
+    m.pointwise(["x[i]"], "ret[i] = 1 / Math.tan(x[i])")(r);
+  startPerformanceTest(
+    "cotan",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
