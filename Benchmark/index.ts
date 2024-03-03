3545aa04c20eb414d6d010b9e29ca387b72e4c4f
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 10:50:20 +0200

    Added benchmark test for the arccos method.

--- Benchmark/index.ts
+++ Benchmark/index.ts
@@ -64,8 +64,8 @@
 // import "./cosh.spec.ts";
 // import "./tanh.spec.ts";
 // import "./cotanh.spec.ts";
-import "./arcsin.spec.ts";
-// import "./arccos.spec.ts";
+// import "./arcsin.spec.ts";
+import "./arccos.spec.ts";
 // import "./arctan.spec.ts";
 // import "./arccotan.spec.ts";
 // import "./abs.spec.ts";
