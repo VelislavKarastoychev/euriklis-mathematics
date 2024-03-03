7ebfec57eea00363be768a14d5dd82d8cc443800
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 10:50:04 +0200

    Implemented benchmark test for the arccos method.

--- Benchmark/arccos.spec.ts
+++ Benchmark/arccos.spec.ts
@@ -1,35 +1,21 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.arccos().isEqualTo(numeric.acos(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the arccos method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.arccos());
-      const t2 = new validator(m).benchmark((m) => numeric.acos(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in arccos benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.arccos(r),
+    numeric.acos(r),
+  );
+  const euriklisTest = (m: any) => m.arccos(r);
+  const numericTest = (m: any) => m.acos(r);
+  startPerformanceTest(
+    "arccos",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
