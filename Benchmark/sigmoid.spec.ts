08e32a5252ad64a01daf39c2708910b1f72a14ef
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 17:36:39 +0200

    Implemented benchmark test for the sigmoid method.

--- Benchmark/sigmoid.spec.ts
+++ Benchmark/sigmoid.spec.ts
@@ -1,41 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(
-    r.sigmoid().isEqualTo(
-      numeric.pointwise(["x[i]"], "ret[i] = 1 / (1 + Math.exp(-x[i]))")(m),
-    ),
-  )
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the sigmoid method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.sigmoid());
-      const t2 = new validator(m).benchmark((m) =>
-        numeric.pointwise(["x[i]"], "ret[i] = 1 / (1 + Math.exp(-x[i]))")(m)
-      );
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in sigmoid benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.sigmoid(r),
+    numeric.pointwise(["x[i]"], "ret[i] = 1 / (1 + Math.exp(-x[i]))")(r),
+  );
+  const euriklisTest = (m: any) => m.sigmoid(r);
+  const numericTest = (m: any) =>
+    m.pointwise(["x[i]"], "ret[i] = 1 / (1 + Math.exp(-x[i]))")(r);
+  startPerformanceTest(
+    "sigmoid",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
