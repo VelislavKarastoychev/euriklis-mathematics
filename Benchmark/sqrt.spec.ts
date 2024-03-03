686303f2112a31111b25a1ba33de9726ddcc80d7
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 18:47:39 +0200

    Implemented benchmark test for the sqrt method.

--- Benchmark/sqrt.spec.ts
+++ Benchmark/sqrt.spec.ts
@@ -1,35 +1,20 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.sqrt().isEqualTo(numeric.sqrt(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the sqrt method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.sqrt());
-      const t2 = new validator(m).benchmark((m) => numeric.sqrt(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in sqrt benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.sqrt(r),
+    numeric.sqrt(r),
+  );
+  const test = (lib: any) => lib.sqrt(r);
+  startPerformanceTest(
+    "sqrt",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
