7089abc437dcab3046192b3a85a93b4341ffe87c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 19:06:09 +0200

    Implemented test for the log method.

--- Tests/log.spec.ts
+++ Tests/log.spec.ts
@@ -1,14 +1,19 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const log = r.M.map((r: number[]) => r.map((c: number) => Math.log(c)));
-const logWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.log(2 * c + 3))
+const log = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.log(c))
 );
+const logWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.log(2 * c + 3)));
+const callLog = (m: MatrixType | NumericMatrix): MatrixType | NumericMatrix =>
+  Matrix.log(m);
 
-new validator(r.log().isEqualTo(log))
+new validator(Matrix.isEqualTo(Matrix.log(r), log))
   .describe("The log method has to:")
   .test({
     title: true,
@@ -18,7 +23,13 @@ new validator(r.log().isEqualTo(log))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.log(2, 3).isEqualTo(logWeighted))
+new validator(Matrix.isEqualTo(Matrix.log(r, 2, 3), logWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callLog)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
