8bbec37cb3893119d5bc583cd4b26184725bf601
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 09:26:10 +0200

    Implemented test for the sinh method.

--- Tests/sinh.spec.ts
+++ Tests/sinh.spec.ts
@@ -1,14 +1,18 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
-
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 const r = Matrix.random(3, 4);
-const sinh = r.M.map((r: number[]) => r.map((c: number) => Math.sinh(c)));
-const sinhWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.sinh(2 * c + 3))
+const sinh = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.sinh(c))
 );
+const sinhWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.sinh(2 * c + 3)));
 
-new validator(r.sinh().minus(sinh).FrobeniusNorm)
+const callSinh = (m: MatrixType | NumericMatrix): MatrixType | NumericMatrix =>
+  Matrix.sinh(m);
+new validator(Matrix.FrobeniusNorm(Matrix.minus(Matrix.sinh(r), sinh)))
   .describe("The sinh method has to:")
   .test({
     title: true,
@@ -18,7 +22,15 @@ new validator(r.sinh().minus(sinh).FrobeniusNorm)
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.sinh(2, 3).minus(sinhWeighted).FrobeniusNorm)
+new validator(
+  Matrix.FrobeniusNorm(Matrix.minus(Matrix.sinh(r, 2, 3), sinhWeighted)),
+)
   .isInRange(-1e-8, 1e8)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callSinh)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
