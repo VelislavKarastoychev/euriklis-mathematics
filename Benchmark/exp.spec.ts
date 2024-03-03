1c0cc97c07f58e2b6818cb684b02664f5e3e7f79
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 09:06:43 +0200

    Implemented benchmark test for the exp method.

--- Benchmark/exp.spec.ts
+++ Benchmark/exp.spec.ts
@@ -1,35 +1,20 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.exp().isEqualTo(numeric.exp(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the exp method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.exp());
-      const t2 = new validator(m).benchmark((m) => numeric.exp(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in exp benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.exp(r),
+    numeric.exp(r),
+  );
+  const test = (lib: any) => lib.exp(r);
+  startPerformanceTest(
+    "exp",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
