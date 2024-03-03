f83f4c2823a26f7795f767b6972dfb6a505ab098
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 19:52:04 +0200

    Implemented test for the step method.

--- Tests/step.spec.ts
+++ Tests/step.spec.ts
@@ -1,14 +1,19 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const step = r.M.map((r: number[]) => r.map((c: number) => c <= 0 ? -1 : 1));
-const stepWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => (2 * c + 3) <= 0 ? -1 : 1)
+const step = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => c <= 0 ? -1 : 1)
 );
+const stepWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => (2 * c + 3) <= 0 ? -1 : 1));
+const callStep = (m: MatrixType | NumericMatrix): MatrixType | NumericMatrix =>
+  Matrix.step(m);
 
-new validator(r.step().isEqualTo(step))
+new validator(Matrix.isEqualTo(Matrix.step(r), step))
   .describe("The step method has to:")
   .test({
     title: true,
@@ -18,7 +23,13 @@ new validator(r.step().isEqualTo(step))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.step(2, 3).isEqualTo(stepWeighted))
+new validator(Matrix.isEqualTo(Matrix.step(r, 2, 3), stepWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callStep)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
