9a33505557e8b9b5581b78841e7293a6ce9d4ded
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 17:13:25 +0200

    Implemented test for the abs method.

--- Tests/abs.spec.ts
+++ Tests/abs.spec.ts
@@ -1,13 +1,18 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4, -1, 1);
-const abs = r.M.map((r: number[]) => r.map((c: number) => Math.abs(c)));
-const absWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.abs(2 * c + 3))
+const abs = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.abs(c))
 );
-new validator(r.abs().isEqualTo(abs))
+const absWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.abs(2 * c + 3)));
+const callAbs = (m: MatrixType | NumericMatrix): MatrixType | NumericMatrix =>
+  Matrix.abs(m);
+new validator(Matrix.isEqualTo(Matrix.abs(r), abs))
   .describe("The abs method has to:")
   .test({
     title: true,
@@ -17,7 +22,13 @@ new validator(r.abs().isEqualTo(abs))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.abs(2, 3).isEqualTo(absWeighted))
+new validator(Matrix.isEqualTo(Matrix.abs(r, 2, 3), absWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callAbs)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
