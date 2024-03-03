9f57dd0fbcbf1d01c2096d2313231dd389a91eff
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 09:44:55 +0200

    Implemented benchmark test for the cosh method.

--- Benchmark/cosh.spec.ts
+++ Benchmark/cosh.spec.ts
@@ -1,35 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.cosh().isEqualTo(numeric.pointwise(['x[i]'], 'ret[i] = Math.cosh(x[i])')(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the cosh method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.cosh());
-      const t2 = new validator(m).benchmark((m) => numeric.pointwise(['x[i]'], 'ret[i] = Math.cosh(x[i])')(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in cosh benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.cosh(r),
+    numeric.pointwise(["x[i]"], "ret[i] = Math.cosh(x[i])")(r),
+  );
+  const euriklisTest = (m: any) => m.cosh(r);
+  const numericTest = (m: any) =>
+    m.pointwise(["x[i]"], "ret[i] = Math.cosh(x[i])")(r);
+  startPerformanceTest(
+    "cosh",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
