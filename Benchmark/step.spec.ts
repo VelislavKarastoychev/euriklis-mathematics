921a2be30564bc85a0cfd09c427153b74c01a511
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 19:51:29 +0200

    Implemented benchmark test for the step method.

--- Benchmark/step.spec.ts
+++ Benchmark/step.spec.ts
@@ -1,35 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.step().isEqualTo(numeric.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : 1")(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the step method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.step());
-      const t2 = new validator(m).benchmark((m) => numeric.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : 1")(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in step benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(dimensions[0], dimensions[1], -19, 10);
+  const condition = Matrix.isEqualTo(
+    Matrix.step(r),
+    numeric.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : 1")(r),
+  );
+  const euriklisTest = (m: any) => m.step(r);
+  const numericTest = (m: any) =>
+    m.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : 1")(r);
+  startPerformanceTest(
+    "step",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
