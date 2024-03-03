6d05e36c9f9e18de9266ad6a665878584c8d3140
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 18:48:33 +0200

    Implemented test for the method sqrt.

--- Tests/sqrt.spec.ts
+++ Tests/sqrt.spec.ts
@@ -1,14 +1,19 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const sqrt = r.M.map((r: number[]) => r.map((c: number) => Math.sqrt(c)));
-const sqrtWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.sqrt(2 * c + 3))
+const sqrt = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.sqrt(c))
 );
+const sqrtWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.sqrt(2 * c + 3)));
+const callSqrt = (m: MatrixType | NumericMatrix): MatrixType | NumericMatrix =>
+  Matrix.sqrt(m);
 
-new validator(r.sqrt().isEqualTo(sqrt))
+new validator(Matrix.isEqualTo(Matrix.sqrt(r), sqrt))
   .describe("The sqrt method has to:")
   .test({
     title: true,
@@ -18,7 +23,14 @@ new validator(r.sqrt().isEqualTo(sqrt))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.sqrt(2, 3).isEqualTo(sqrtWeighted))
+new validator(Matrix.isEqualTo(Matrix.sqrt(r, 2, 3), sqrtWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callSqrt)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .and.throwsErrorWith([[-1, -2, -3], [-3, -4, -5]])
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
