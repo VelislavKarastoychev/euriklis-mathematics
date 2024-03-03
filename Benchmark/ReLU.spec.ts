c0f7ced16bca7a179122db14e7d84c26e907b448
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 19:50:45 +0200

    Implemented benchmark test for the ReLU method.

--- Benchmark/ReLU.spec.ts
+++ Benchmark/ReLU.spec.ts
@@ -1,35 +1,22 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.ReLU().isEqualTo(numeric.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : x[i]")(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the ReLU method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.ReLU());
-      const t2 = new validator(m).benchmark((m) => numeric.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : x[i]")(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in ReLU benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(...dimensions);
+  const condition = Matrix.isEqualTo(
+    Matrix.ReLU(r),
+    numeric.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : x[i]")(r),
+  );
+  const euriklisTests = (m: any) => m.ReLU(r);
+  const numericTest = (m: any) =>
+    m.pointwise(["x[i]"], "ret[i] = x[i] <= 0 ? -1 : x[i]")(r);
+  startPerformanceTest(
+    "ReLU",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTests,
+    numericTest,
+  );
 })();
