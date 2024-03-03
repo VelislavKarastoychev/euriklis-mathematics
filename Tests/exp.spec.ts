5333d3e03d1e455a2cd5986ab8c7d51fb6d9ef0b
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 09:07:07 +0200

    Implemented test for the exp method.

--- Tests/exp.spec.ts
+++ Tests/exp.spec.ts
@@ -1,14 +1,17 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
-
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 const r = Matrix.random(3, 4);
-const exp = r.M.map((r: number[]) => r.map((c: number) => Math.exp(c)));
-const expWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.exp(2 * c + 3))
+const exp = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.exp(c))
 );
+const expWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.exp(2 * c + 3)));
 
-new validator(r.exp().isEqualTo(exp))
+const callExp = (m: MatrixType | NumericMatrix): MatrixType | NumericMatrix => Matrix.exp(m);
+new validator(Matrix.isEqualTo(Matrix.exp(r), exp))
   .describe("The exp method has to:")
   .test({
     title: true,
@@ -18,7 +21,13 @@ new validator(r.exp().isEqualTo(exp))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.exp(2, 3).isEqualTo(expWeighted))
+new validator(Matrix.isEqualTo(Matrix.exp(r, 2, 3), expWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callExp)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix is incorrectly defined.")
   .test();
