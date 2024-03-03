88bfd70cebfed03cfdf7aac88c3fc891ac5fb409
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 10:01:36 +0200

    Implemented test for the tanh method.

--- Tests/tanh.spec.ts
+++ Tests/tanh.spec.ts
@@ -1,14 +1,19 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const tanh = r.M.map((r: number[]) => r.map((c: number) => Math.tanh(c)));
-const tanhWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.tanh(2 * c + 3))
+const tanh = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.tanh(c))
 );
+const tanhWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.tanh(2 * c + 3)));
 
-new validator(r.tanh().isEqualTo(tanh))
+const callTanh = (m: MatrixType | NumericMatrix): MatrixType | NumericMatrix =>
+  Matrix.tanh(m);
+new validator(Matrix.isEqualTo(Matrix.tanh(r), tanh))
   .describe("The tanh method has to:")
   .test({
     title: true,
@@ -18,7 +23,13 @@ new validator(r.tanh().isEqualTo(tanh))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.tanh(2, 3).isEqualTo(tanhWeighted))
+new validator(Matrix.isEqualTo(Matrix.tanh(r, 2, 3), tanhWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callTanh)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
