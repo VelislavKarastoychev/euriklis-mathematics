eed694adb4b14e83fd872a3edae935ef7f6cc6c9
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 11:05:22 +0200

    Implemented test for the arctan method.

--- Tests/arctan.spec.ts
+++ Tests/arctan.spec.ts
@@ -1,14 +1,20 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const arctan = r.M.map((r: number[]) => r.map((c: number) => Math.atan(c)));
-const arctanWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.atan(2 * c + 3))
+const arctan = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.atan(c))
 );
+const arctanWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.atan(2 * c + 3)));
+const callArcTan = (
+  m: MatrixType | NumericMatrix,
+): MatrixType | NumericMatrix => Matrix.arctan(m);
 
-new validator(r.arctan().isEqualTo(arctan))
+new validator(Matrix.isEqualTo(Matrix.arctan(r), arctan))
   .describe("The arctan method has to:")
   .test({
     title: true,
@@ -18,7 +24,13 @@ new validator(r.arctan().isEqualTo(arctan))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.arctan(2, 3).isEqualTo(arctanWeighted))
+new validator(Matrix.isEqualTo(Matrix.arctan(r, 2, 3), arctanWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callArcTan)
+  .throwsErrorWith([[1, 2, 3], [[1, 23]], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
