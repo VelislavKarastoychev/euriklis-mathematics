3d726805215b7c3e103a5c486bdf5d9c7643fc75
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 09:25:54 +0200

    Added test for the sinh method.

--- Tests/index.ts
+++ Tests/index.ts
@@ -60,7 +60,7 @@ import "./cos.spec.ts";
 import "./tan.spec.ts";
 import "./cotan.spec.ts";
 import "./exp.spec.ts";
-// import "./sinh.spec.ts";
+import "./sinh.spec.ts";
 // import "./cosh.spec.ts";
 // import "./tanh.spec.ts";
 // import "./cotanh.spec.ts";
