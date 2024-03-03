5e31c9e3a0b8de30529fb8963a5f166dd2a0a45c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 10:50:39 +0200

    Implemented test for the arccos method.

--- Tests/arccos.spec.ts
+++ Tests/arccos.spec.ts
@@ -1,14 +1,20 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types.ts";
 
 const r = Matrix.random(3, 4);
-const arccos = r.M.map((r: number[]) => r.map((c: number) => Math.acos(c)));
-const arccosWeighted = r.M.map((r: number[]) =>
-  r.map((c: number) => Math.acos(0.5 * c + 0.1))
+const arccos = (Matrix.copy(r, "generic") as NumericMatrix).map((r: number[]) =>
+  r.map((c: number) => Math.acos(c))
 );
+const arccosWeighted = (Matrix.copy(r, "generic") as NumericMatrix).map((
+  r: number[],
+) => r.map((c: number) => Math.acos(0.5 * c + 0.1)));
 
-new validator(r.arccos().isEqualTo(arccos))
+const callArcCos = (
+  m: MatrixType | NumericMatrix,
+): MatrixType | NumericMatrix => Matrix.arccos(m);
+new validator(Matrix.isEqualTo(Matrix.arccos(r), arccos))
   .describe("The arccos method has to:")
   .test({
     title: true,
@@ -18,7 +24,13 @@ new validator(r.arccos().isEqualTo(arccos))
   .describe("1. return the correct result without method parameters")
   .test();
 
-new validator(r.arccos(0.5, 0.1).isEqualTo(arccosWeighted))
+new validator(Matrix.isEqualTo(Matrix.arccos(r, 0.5, 0.1), arccosWeighted))
   .isSame(true)
   .describe("2. return the correct result when the method has parameters.")
+  .test();
+
+new validator(callArcCos)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("3. throw error when the matrix parameter is incorrectly defined.")
   .test();
