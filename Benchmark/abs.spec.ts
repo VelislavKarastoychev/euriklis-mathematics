87658d11f03ca80444e462201e3afa9b59d88047
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 17:12:46 +0200

    Implemented benchmark test for the abs method.

--- Benchmark/abs.spec.ts
+++ Benchmark/abs.spec.ts
@@ -1,35 +1,20 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000);
-  const m = r.M;
-  new validator(r.abs().isEqualTo(numeric.abs(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe(
-        "Time performance of the abs method with matrix with dimension 5000 x 5000.",
-      )
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.abs());
-      const t2 = new validator(m).benchmark((m) => numeric.abs(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    })
-    .on(false, (v) => {
-      v.describe("Internal error in abs benchmark test method.")
-        .test({
-          title: true,
-          success: "red",
-          error: "green",
-        });
-    });
+  const r = Matrix.uniqueRandom(dimensions[0], dimensions[1], -1, 1);
+  const condition = Matrix.isEqualTo(
+    Matrix.abs(r),
+    numeric.abs(r),
+  );
+  const test = (lib: any) => lib.abs(r);
+  startPerformanceTest(
+    "abs",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
