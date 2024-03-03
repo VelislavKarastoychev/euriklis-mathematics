ff1a9e5d7fba0215f0016bf7d242e36d4e11e874
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-03 17:59:05 +0200

    Implemented benchmark test for the ceil method.

--- Benchmark/ceil.spec.ts
+++ Benchmark/ceil.spec.ts
@@ -1,32 +1,20 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src/index.ts";
 import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const r = Matrix.random(5000, 5000, -10, 10);
-  const m = r.M;
-  new validator(r.ceil().isEqualTo(numeric.ceil(m)))
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of the ceil method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.ceil());
-      const t2 = new validator(m).benchmark((m) => numeric.ceil(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in the ceil benchmark test method.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const r = Matrix.uniqueRandom(dimensions[0], dimensions[1], -10, 10);
+  const condition = Matrix.isEqualTo(
+    Matrix.ceil(r),
+    numeric.ceil(r),
+  );
+  const test = (lib: any) => lib.ceil(r);
+  startPerformanceTest(
+    "ceil",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    test,
+    test,
+  );
 })();
