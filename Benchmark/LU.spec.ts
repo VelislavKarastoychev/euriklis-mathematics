471b462e9413cd8c501bc78c71a6bc219cba832c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-04 12:45:45 +0200

    Implemented benchmark test for the LU factorization method.

--- Benchmark/LU.spec.ts
+++ Benchmark/LU.spec.ts
@@ -1,33 +1,45 @@
 "use strict";
-import validator from "@euriklis/validator-ts";
 import numeric from "numericjs";
 import { Matrix } from "../src/index.ts";
-// import { CompactLUFactorizationWithPermutations } from "../src/Matrix/Models/index.ts";
+import { dimensions, startPerformanceTest } from "./utils.ts";
 
 (async () => {
-  const m = numeric.random([1000, 1000]);
-  const r = new Matrix(m);
-  new validator(true)
-    .isSame(true)
-    .on(true, (v) => {
-      v.describe("Time performance of the LUPC method:")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-      const t1 = new validator(r).benchmark((m) => m.LUPC());
-      const t2 = new validator(m).benchmark((m) => numeric.LU(m));
-      console.table({
-        "@euriklis/mathematics": t1,
-        numericjs: t2,
-      });
-    }).on(false, (v) => {
-      v.describe("Internal error in the LUPC method.")
-        .test({
-          title: true,
-          success: "green",
-          error: "red",
-        });
-    });
+  const r1 = Matrix.uniqueRandom(
+    dimensions[0] * 0.2,
+    dimensions[1] * 0.2,
+    10,
+    100,
+  );
+  const r2 = Matrix.copy(r1);
+  const a1 = [
+    [0, 5, 22/3],
+    [4, 2, 1],
+    [2, 7, 9]
+  ];
+  const a2 = Matrix.copy(a1, "generic");
+  const lu = [
+    [4, 2, 1],
+    [0.5, 6, 8.5],
+    [0, (5/6), 0.25]
+  ];
+  const condition = Matrix.isEqualTo(
+    Matrix.LUPC(a1).LU,
+    numeric.LU(a2).LU
+  ) && Matrix.isEqualTo(
+      Matrix.LUPC(r1).LU,
+      numeric.LU(r2).LU
+    );
+  const euriklisTest = (m: any) => m.LUPC(r1);
+  const numericTest = (m: any) => m.LU(r2);
+  startPerformanceTest(
+    "LUPC",
+    [{
+      param: "matrix",
+      dimensions: dimensions.map((el) => el / 1) as [number, number],
+      type: "float64",
+    }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
 })();
