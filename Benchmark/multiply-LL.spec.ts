abb1087414978135350adc651034094a500ddda1
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-09 23:37:25 +0200

    Implemented benchmark test for the LL multiplicaton method.

---
+++ Benchmark/multiply-LL.spec.ts
@@ -0,0 +1,27 @@
+"use strict";
+import { Matrix } from "../src";
+import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils";
+
+(async () => {
+  const l1 = Matrix.lowerTriangularRandom(dimensions[0] / 10, dimensions[1] / 10);
+  const l2 = Matrix.lowerTriangularUniqueRandom(dimensions[0] / 10, dimensions[1] / 10);
+  const tiny = 1e-8;
+
+  const condition = Matrix.FrobeniusNorm(
+    Matrix.minus(
+      Matrix.multiplyLL(l1, l2),
+      numeric.dot(l1, l2)
+    )
+  ) <= tiny;
+  const euriklisTest = (m: any) => m.multiplyLL(l1, l2);
+  const numericTest = (m: any) => m.dot(l1, l2);
+
+  startPerformanceTest(
+    "multiplyLL",
+    [{param: "lower triangular matrices", dimensions, type: "float64"}],
+    condition,
+    euriklisTest,
+    numericTest
+  )
+})();
