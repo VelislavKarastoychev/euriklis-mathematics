fc7897fc89e68d4b1a7f7afa5657463add6c5cad
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-09 23:38:24 +0200

    Implemented test for the multiplyLL method.

---
+++ Tests/multiply-lower-lower-triangular.spec.ts
@@ -0,0 +1,33 @@
+"use strict";
+
+import { Matrix } from "../src";
+import validator from "@euriklis/validator-ts";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+
+const a: MatrixType | NumericMatrix = [
+  [Math.random(), 0, 0, 0, 0],
+  [Math.random(), Math.random(), 0, 0, 0],
+  [Math.random(), Math.random(), Math.random(), 0, 0],
+  [Math.random(), Math.random(), Math.random(), Math.random(), 0],
+  [Math.random(), Math.random(), Math.random(), Math.random(), Math.random()],
+];
+
+const b: MatrixType | NumericMatrix = [
+  [Math.random(), 0, 0, 0, 0],
+  [Math.random(), Math.random(), 0, 0, 0],
+  [Math.random(), Math.random(), Math.random(), 0, 0],
+  [Math.random(), Math.random(), Math.random(), Math.random(), 0],
+  [Math.random(), Math.random(), Math.random(), Math.random(), Math.random()],
+];
+
+new validator(Matrix.FrobeniusNorm(
+  Matrix.minus(
+    Matrix.times(a, b),
+    Matrix.multiplyLL(a, b),
+  ),
+)).isInRange(-1e-8, 1e-8)
+  .describe("The multiplyLL method has to:")
+  .test({ title: true, success: "green", error: "red" })
+  .describe("1. multiply correctly lower triangular matrices.")
+  .test();
+
