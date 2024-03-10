f307f03c9b3d89ecd838b3a930c0cd10658f758d
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-10 11:45:38 +0200

    Implemented benchmark test for the multiplyLU method.

---
+++ Benchmark/multiplyUU.spec.ts
@@ -0,0 +1,35 @@
+"use strict";
+import { Matrix } from "../src";
+import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils";
+
+(async () => {
+  const u1 = Matrix.randomUpperTriangular(
+    dimensions[0] / 10 | 0,
+    dimensions[1] / 10 | 0,
+  );
+  const u2 = Matrix.uniqueRandomUpperTriangular(
+    dimensions[0] / 10 | 0,
+    dimensions[1] / 10 | 0,
+  );
+  const eps = 1e-8;
+  const condition = Matrix.FrobeniusNorm(
+    Matrix.minus(
+      Matrix.multiplyUU(u1, u2),
+      numeric.dot(u1, u2),
+    ),
+  ) < eps;
+  const euriklisTest = (m: any) => m.multiplyUU(u1, u2);
+  const numericTest = (m: any) => m.dot(u1, u2);
+  startPerformanceTest(
+    "multiplyUU",
+    [{
+      param: "matrices",
+      dimensions: [dimensions[0] / 10 | 0, dimensions[1] / 10 | 0],
+      type: "float64",
+    }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
+})();
