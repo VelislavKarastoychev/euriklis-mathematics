09840f39dd0c51f64bf850b4260dcb05121c2f06
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-23 00:56:35 +0200

    Fully implemented test for the absoluteSumOfRowElements method.

--- Tests/absolute-sum-of-row-elements.spec.ts
+++ Tests/absolute-sum-of-row-elements.spec.ts
@@ -11,10 +11,22 @@ const r1: NumericMatrix = Matrix.uniqueRandom(
   "generic",
 ) as NumericMatrix;
 
+const callAbsoluteSumOfRowElements = (m: MatrixType | NumericMatrix) =>
+  Matrix.absoluteSumOfRowElements(m);
 const rowsNorm1 = [r1.reduce((norm1: number[], row: number[], i: Integer) => {
   norm1[i] = row.reduce((sum: number, el: number) => sum += Math.abs(el), 0);
   return norm1;
 }, [])];
+const rowsNorm1AsColumn = r1.reduce(
+  (norm1: number[][], row: number[], i: Integer) => {
+    norm1[i] = [
+      row.reduce((sum: number, el: number) => sum += Math.abs(el), 0),
+    ];
+    return norm1;
+  },
+  [],
+);
+
 new validator(
   Matrix.FrobeniusNorm(
     Matrix.minus(
@@ -28,4 +40,22 @@ new validator(
   .describe(
     "1. return the correct result when the parameters are correctly defined.",
   )
+  .test();
+
+new validator(
+  Matrix.FrobeniusNorm(
+    Matrix.minus(
+      Matrix.absoluteSumOfRowElements(r1, undefined, "column"),
+      rowsNorm1AsColumn,
+    ),
+  ) < 1e-8,
+).describe(
+  "2. return the correct result in column vector when the mode parameter is set to 'column'",
+)
+  .isSame(true)
+  .test();
+new validator(callAbsoluteSumOfRowElements)
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("it throws")
+  .describe("3. thorw error when the matrix parameter is incorrectly defined.")
   .test();
