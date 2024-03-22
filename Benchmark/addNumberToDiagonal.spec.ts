ba061a0b905c165297fce57b3f697035c3e1293d
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-22 23:16:38 +0200

    Implemented benchmark test for the addNumberToDiagonal method - fixed.

--- Benchmark/addNumberToDiagonal.spec.ts
+++ Benchmark/addNumberToDiagonal.spec.ts
@@ -4,26 +4,27 @@ import numeric from "numericjs";
 import { Matrix } from "../src";
 import { dimensions, startPerformanceTest } from "./utils";
 import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+(async () => {
+  const r1 = Matrix.uniqueRandom(...dimensions);
+  const r2 = Matrix.copy(r1);
+  const addNumberToDiagonal = (m: MatrixType | NumericMatrix, n: number) =>
+    numeric.mapreduce2(`accum[i][i] += ${n}`, "accum = x")(m);
 
-const r1 = Matrix.uniqueRandom(...dimensions);
-const r2 = Matrix.copy(r1);
-const addNumberToDiagonal = (m: MatrixType | NumericMatrix, n: number) =>
-  numeric.mapreduce2(`accum[i][i] += ${n}`, "accum = x")(m);
+  const condition = Matrix.FrobeniusNorm(
+    Matrix.minus(
+      Matrix.addNumberToDiagonal(r1, Math.PI),
+      addNumberToDiagonal(r2, Math.PI),
+    ),
+  ) < 1e-8;
 
-const condition = Matrix.FrobeniusNorm(
-  Matrix.minus(
-    Matrix.addNumberToDiagonal(r1, Math.PI),
-    addNumberToDiagonal(r2, Math.PI),
-  ),
-) < 1e-8;
+  const euriklisTest = (m: any) => m.addNumberToDiagonal(r1, Math.E);
+  const numericTest = (m: any) => addNumberToDiagonal(r2, Math.E);
 
-const euriklisTest = (m: any) => m.addNumberToDiagonal(r1, Math.E);
-const numericTest = (m: any) => addNumberToDiagonal(r2, Math.E);
-
-startPerformanceTest(
-  "addNumberToDiagonal",
-  [{ param: "matrix", dimensions, type: "float64" }],
-  condition,
-  euriklisTest,
-  numericTest,
-);
+  startPerformanceTest(
+    "addNumberToDiagonal",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTest,
+    numericTest,
+  );
+})();
