eb559299023c0eebae60db5786c5c4c5d7c1859f
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-22 06:56:05 +0200

    Implemented utility function for the GershgorinCircles method.

---
+++ src/Matrix/Models/GershgorinCircles.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+import {
+  Integer,
+  MatrixType,
+  NumericMatrix,
+  TypedArrayConstructor,
+} from "../types";
+
+export const GershgorinCircles = (
+  matrix: MatrixType,
+  typedArray: TypedArrayConstructor | ArrayConstructor,
+): MatrixType | NumericMatrix => {
+  return [];
+};
