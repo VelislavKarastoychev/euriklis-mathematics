af6e27618a383240c219dddb70604bc8bd9d0aff
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-22 23:18:02 +0200

    Implemented test for the sumOfRowElements method.

--- Benchmark/sum-of-row-elements.spec.ts
+++ Benchmark/sum-of-row-elements.spec.ts
@@ -3,50 +3,51 @@ import { Matrix } from "../src";
 import numeric from "numericjs";
 import { dimensions, startPerformanceTest } from "./utils";
 import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+(async () => {
+  const r1 = Matrix.uniqueRandom(...dimensions);
+  const r2 = Matrix.copy(r1);
+  const sumOfRowElementsAsRow = (m: MatrixType | NumericMatrix) =>
+    numeric.mapreduce2(
+      "sum = 0;for (j = xi.length;j--;) { sum += xi[j];}\n accum[0][i] = sum;",
+      "let j, sum, accum = [[]];",
+    )(m);
 
-const r1 = Matrix.uniqueRandom(...dimensions);
-const r2 = Matrix.copy(r1);
-const sumOfRowElementsAsRow = (m: MatrixType | NumericMatrix) =>
-  numeric.mapreduce2(
-    "sum = 0;for (j = xi.length;j--;) { sum += xi[j];}\n accum[0][i] = sum;",
-    "let j, sum, accum = [[]];",
-  )(m);
+  const sumOfRowElementsAsColumn = (m: MatrixType | NumericMatrix) =>
+    numeric.mapreduce2(
+      "sum = 0;for (j = xi.length;j--;) { sum += xi[j];}\n accum[i] = [sum];",
+      "let j, sum, xij, accum = [];",
+    )(m);
+  const condition = Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.sumOfRowElements(r1, undefined, "row"),
+          sumOfRowElementsAsRow(r2),
+        ),
+      ) < 1e-8 && Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.sumOfRowElements(r1, undefined, "column"),
+          sumOfRowElementsAsColumn(r2),
+        ),
+      ) < 1e-8;
 
-const sumOfRowElementsAsColumn = (m: MatrixType | NumericMatrix) =>
-  numeric.mapreduce2(
-    "sum = 0;for (j = xi.length;j--;) { sum += xi[j];}\n accum[i] = [sum];",
-    "let j, sum, xij, accum = [];",
-  )(m);
-const condition = Matrix.FrobeniusNorm(
-      Matrix.minus(
-        Matrix.sumOfRowElements(r1, undefined, "row"),
-        sumOfRowElementsAsRow(r2),
-      ),
-    ) < 1e-8 && Matrix.FrobeniusNorm(
-      Matrix.minus(
-        Matrix.sumOfRowElements(r1, undefined, "column"),
-        sumOfRowElementsAsColumn(r2),
-      ),
-    ) < 1e-8;
+  const euriklisTestRow = (m: any) => m.sumOfRowElements(r1);
+  const numericTestAsRow = (_: any) => sumOfRowElementsAsRow(r2);
+  const euriklisTestColumn = (m: any) =>
+    m.sumOfRowElements(r1, undefined, "column");
+  const numericTestAsColumn = (_: any) => sumOfRowElementsAsColumn(r2);
 
-const euriklisTestRow = (m: any) => m.sumOfRowElements(r1);
-const numericTestAsRow = (_: any) => sumOfRowElementsAsRow(r2);
-const euriklisTestColumn = (m: any) =>
-  m.sumOfRowElements(r1, undefined, "column");
-const numericTestAsColumn = (_: any) => sumOfRowElementsAsColumn(r2);
+  startPerformanceTest(
+    "sumOfRowElements method in row mode",
+    [{ param: "matrix" }],
+    condition,
+    euriklisTestRow,
+    numericTestAsRow,
+  );
 
-startPerformanceTest(
-  "sumOfRowElements method in row mode",
-  [{ param: "matrix" }],
-  condition,
-  euriklisTestRow,
-  numericTestAsRow,
-);
-
-startPerformanceTest(
-  "sumOfRowElements in column mode",
-  [{ param: "matrix", dimensions, type: "float64" }],
-  condition,
-  euriklisTestColumn,
-  numericTestAsColumn,
-);
+  startPerformanceTest(
+    "sumOfRowElements in column mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestColumn,
+    numericTestAsColumn,
+  );
+})();
