9a54a911c18067cbf200be2c32c0559b2a226000
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-22 23:17:41 +0200

    Implemented benchmark test for the sumOfRowElements method.

--- Benchmark/sum-of-row-elements-except-diagonal.spec.ts
+++ Benchmark/sum-of-row-elements-except-diagonal.spec.ts
@@ -5,54 +5,56 @@ import numeric from "numericjs";
 
 import { MatrixType, NumericMatrix } from "../src/Matrix/types";
 import { dimensions, startPerformanceTest } from "./utils";
+(async () => {
+  const r1 = Matrix.uniqueRandom(...dimensions);
+  const r2 = Matrix.copy(r1);
 
-const r1 = Matrix.uniqueRandom(...dimensions);
-const r2 = Matrix.copy(r1);
+  const sumOfRowElementsExceptDiagonalAsRow = (m: MatrixType | NumericMatrix) =>
+    numeric.mapreduce2(
+      "sum = 0;for (j = xi.length;j--;) {sum += i !== j ? xi[j] : 0;}; accum[0][i] = sum;",
+      "let j, xij, sum, accum = [[]];",
+    )(m);
 
-const sumOfRowElementsExceptDiagonalAsRow = (m: MatrixType | NumericMatrix) =>
-  numeric.mapreduce2(
-    "sum = 0;for (j = xi.length;j--;) {sum += i !== j ? xi[j] : 0;}; accum[0][i] = sum;",
-    "let j, xij, sum, accum = [[]];",
-  )(m);
+  const sumOfRowElementsExceptDiagonalAsColumn = (
+    m: MatrixType | NumericMatrix,
+  ) =>
+    numeric.mapreduce2(
+      "sum = 0;for (j = xi.length;j--;) {sum += i !== j ? xi[j] : 0;}; accum[i] = [sum];",
+      "let j, xij, sum, accum = [];",
+    )(m);
+  const condition = Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.sumOfRowElementsExceptDiagonal(r1),
+          sumOfRowElementsExceptDiagonalAsRow(r2),
+        ),
+      ) < 1e-8 && Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.sumOfRowElementsExceptDiagonal(r1, undefined, "column"),
+          sumOfRowElementsExceptDiagonalAsColumn(r2),
+        ),
+      ) < 1e-8;
 
-const sumOfRowElementsExceptDiagonalAsColumn = (
-  m: MatrixType | NumericMatrix,
-) =>
-  numeric.mapreduce2(
-    "sum = 0;for (j = xi.length;j--;) {sum += i !== j ? xi[j] : 0;}; accum[i] = [sum];",
-    "let j, xij, sum, accum = [];",
-  )(m);
-const condition = Matrix.FrobeniusNorm(
-      Matrix.minus(
-        Matrix.sumOfRowElementsExceptDiagonal(r1),
-        sumOfRowElementsExceptDiagonalAsRow(r2),
-      ),
-    ) < 1e-8 && Matrix.FrobeniusNorm(
-      Matrix.minus(
-        Matrix.sumOfRowElementsExceptDiagonal(r1, undefined, "column"),
-        sumOfRowElementsExceptDiagonalAsColumn(r2),
-      ),
-    ) < 1e-8;
+  const euriklisTestAsRow = (m: any) =>
+    Matrix.sumOfRowElementsExceptDiagonal(r2);
+  const numericTestAsRow = (_: any) => sumOfRowElementsExceptDiagonalAsRow(r2);
+  const euriklisTestAsColumn = (m: any) =>
+    Matrix.sumOfRowElementsExceptDiagonal(r1, undefined, "column");
+  const numericTestAsColumn = (_: any) =>
+    sumOfRowElementsExceptDiagonalAsColumn(r2);
 
-const euriklisTestAsRow = (m: any) => Matrix.sumOfRowElementsExceptDiagonal(r2);
-const numericTestAsRow = (_: any) => sumOfRowElementsExceptDiagonalAsRow(r2);
-const euriklisTestAsColumn = (m: any) =>
-  Matrix.sumOfRowElementsExceptDiagonal(r1, undefined, "column");
-const numericTestAsColumn = (_: any) =>
-  sumOfRowElementsExceptDiagonalAsColumn(r2);
+  startPerformanceTest(
+    "sumOfRowElementsExceptDiagonal in row mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestAsRow,
+    numericTestAsRow,
+  );
 
-startPerformanceTest(
-  "sumOfRowElementsExceptDiagonal in row mode",
-  [{ param: "matrix", dimensions, type: "float64" }],
-  condition,
-  euriklisTestAsRow,
-  numericTestAsRow,
-);
-
-startPerformanceTest(
-  "sumOfRowElementsExceptDiagonal in column mode",
-  [{ param: "matrix", dimensions, type: "float64" }],
-  condition,
-  euriklisTestAsColumn,
-  numericTestAsColumn,
-);
+  startPerformanceTest(
+    "sumOfRowElementsExceptDiagonal in column mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestAsColumn,
+    numericTestAsColumn,
+  );
+})();
