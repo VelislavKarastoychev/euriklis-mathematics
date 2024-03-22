2706a7753a88b36e109632693c7e99e360a0f257
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-22 06:53:29 +0200

    Implemented test for the sumOfColumnElementes method.

---
+++ Tests/sum-of-column-elements.spec.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+import validator from "@euriklis/validator-ts";
+import { Matrix } from "../src";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+
+const r = Matrix.uniqueRandom(4, 7);
+const sum = Matrix.sumOfColumnElements(r);
+const sum1 = Matrix.sumOfColumnElements(r, undefined, "column");
+
+new validator(
+  Matrix.isEqualTo(
+    Matrix.transpose(sum1),
+    sum,
+  ),
+).describe("The sumOfColumnElements method has to:")
+  .test({ title: true, success: "green", error: "red" })
+  .isSame(true)
+  .describe(
+    "1. produce correct result and to return row vector by default and column vector with mode 'column'.",
+  )
+  .test();
+
+new validator((m: MatrixType | NumericMatrix) => Matrix.sumOfColumnElements(m))
+  .throwsErrorWith([[1, 2, 3], [1, 23], [123]])
+  .and.throwsErrorWith("this throws")
+  .describe("2. throw error when the matrix parameter is incorrectly defined.")
+  .test();
