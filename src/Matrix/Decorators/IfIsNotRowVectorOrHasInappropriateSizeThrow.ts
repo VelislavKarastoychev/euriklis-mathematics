4b916def1cf256ed3ff3be67201ffe30b3bd2f0d
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-22 06:55:16 +0200

    Implemented decorator.

---
+++ src/Matrix/Decorators/IfIsNotRowVectorOrHasInappropriateSizeThrow.ts
@@ -0,0 +1,24 @@
+"use strict";
+
+import { Integer, MatrixType, NumericMatrix } from "../types";
+
+export function ifIsNotRowVectorOrHasInappropriateSizeThrow(
+  error: Function,
+): (_: any, __: string, descriptor: PropertyDescriptor) => void {
+  return function (_: any, __: string, descriptor: PropertyDescriptor) {
+    const method: (
+      matrix: MatrixType | NumericMatrix,
+      v: MatrixType | NumericMatrix,
+    ) => MatrixType | NumericMatrix = descriptor.value;
+    descriptor.value = function (
+      matrix: MatrixType | NumericMatrix,
+      v: MatrixType | NumericMatrix,
+    ) {
+      const r: Integer = v.length;
+      const c: Integer = v.length;
+      const n = Math.min(matrix.length, matrix[0].length);
+      if (r !== 1 && c !== n) error();
+      return method.call(this, matrix, v);
+    };
+  };
+}
