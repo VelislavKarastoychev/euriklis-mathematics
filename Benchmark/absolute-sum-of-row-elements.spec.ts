6d4a97a91990f6bfb332b6806d8481cb4c442a50
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-23 01:10:38 +0200

    Implemented benchmark test for the absoluteSumOfRowElements method.

---
+++ Benchmark/absolute-sum-of-row-elements.spec.ts
@@ -0,0 +1,53 @@
+"use strict";
+
+import { Matrix } from "../src";
+import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+
+(async () => {
+  const r1 = Matrix.uniqueRandom(...dimensions);
+  const r2 = Matrix.copy(r1);
+  const absoluteSumOfRowElementsAsRow = (m: MatrixType | NumericMatrix) =>
+    numeric.mapreduce2(
+      "sum = 0;for (j = xi.length;j--;) { sum += abs(xi[j]);}\n accum[0][i] = sum;",
+      "const abs = Math.abs;let j, sum, accum = [[]];",
+    )(m);
+
+  const absoluteSumOfRowElementsAsColumn = (m: MatrixType | NumericMatrix) =>
+    numeric.mapreduce2(
+      "sum = 0;for (j = xi.length;j--;) { sum += abs(xi[j]);}\n accum[i] = [sum];",
+      "const abs = Math.abs;let j, sum, xij, accum = [];",
+    )(m);
+  const condition = Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.absoluteSumOfRowElements(r1),
+          absoluteSumOfRowElementsAsRow(r2),
+        ),
+      ) < 1e-8 && Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.absoluteSumOfRowElements(r1, undefined, "column"),
+          absoluteSumOfRowElementsAsColumn(r2),
+        ),
+      ) < 1e-8;
+
+  const euriklisTestAsRow = (m: any) => m.absoluteSumOfRowElements(r1);
+  const euriklisTestAsColumn = (m: any) =>
+    m.absoluteSumOfRowElements(r1, undefined, "column");
+  const numericTestAsRow = (_: any) => absoluteSumOfRowElementsAsRow(r2);
+  const numericTestAsColumn = (_: any) => absoluteSumOfRowElementsAsColumn(r2);
+  startPerformanceTest(
+    "absoluteSumOfRowElements in row mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestAsRow,
+    numericTestAsRow,
+  );
+  startPerformanceTest(
+    "absoluteSumOfRowsElements in column mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestAsColumn,
+    numericTestAsColumn,
+  );
+})();
