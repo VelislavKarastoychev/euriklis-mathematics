82ad319020e4b7f2a63a18193787edfa2aaa06b0
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-24 14:21:06 +0200

    Added test for the absoluteSumOfColumnElements.

--- Tests/index.ts
+++ Tests/index.ts
@@ -96,3 +96,4 @@ import "./add-row-vector-to-diagonal.spec";
 import "./add-column-vector-to-diagonal.spec";
 import "./absolute-sum-of-row-elements.spec";
 import "./absolute-sum-of-row-elements-except-diagonal.spec";
+import "./absolute-sum-of-column-elements.spec";
