c5cd86e4e919f7af347dc651c2cfa1d6b5aea264
VelislavKarastoychev <exel_mmm@abv.bg>
2024-03-31 16:58:20 +0300

    Extended benchmark test for the method addVectorToMatrixByRowAxis.

--- Benchmark/add-vector-to-matrix-by-row-axis.spec.ts
+++ Benchmark/add-vector-to-matrix-by-row-axis.spec.ts
@@ -1,12 +1,12 @@
 "use strict";
 import { Matrix } from "../src";
 import numeric from "numericjs";
-import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+import { Integer, MatrixType, NumericMatrix } from "../src/Matrix/types";
 import { dimensions, startPerformanceTest } from "./utils";
 (async () => {
   const r1 = Matrix.uniqueRandom(...dimensions);
-  const r2 = Matrix.copy(r1);
   const v1 = Matrix.uniqueRandom(1, dimensions[0]);
+  const v2 = Matrix.uniqueRandom(dimensions[0], 1);
   const addVectorToMatrixByRowAxis = (
     v: MatrixType | NumericMatrix,
     m: MatrixType | NumericMatrix,
@@ -17,22 +17,53 @@ import { dimensions, startPerformanceTest } from "./utils";
         "ret[i] = Array(cols);\n" +
         "reti = ret[i];\n" +
         "for(j = cols;j-- > 1;) {reti[j] = xi[j--] + v[i];reti[j] = xi[j] + v[i];};\n" +
-        "if(j === 0) reti[0] = xi[0] + v[0];",
+        "if(j === 0) reti[0] = xi[0] + v[i];",
       "v = v[0];let j, reti;const [_, cols] = numeric.dim(x);",
     )(v, m);
+  const addColVectorToMatrixByRowAxis = (
+    v: MatrixType | NumericMatrix,
+    m: MatrixType | NumericMatrix,
+  ) =>
+    numeric.pointwise2(
+      ["v[i]", "x[i]"],
+      "xi = x[i];\n" +
+        "ret[i] = Array(cols);\n" +
+        "reti = ret[i];\n" +
+        "for(j = cols;j-- > 1;) {reti[j] = xi[j--] + v[i][0];reti[j] = xi[j] + v[i][0];};\n" +
+        "if(j === 0) reti[0] = xi[0] + v[i][0];",
+      "let j, reti;const [_, cols] = numeric.dim(x);",
+    )(v, m);
   const condition = Matrix.FrobeniusNorm(
-    Matrix.minus(
-      Matrix.addVectorToMatrixByRowAxis(r1, v1),
-      addVectorToMatrixByRowAxis(v1, r2),
-    ),
-  ) <= 1e-8;
-  const euriklisTest = (m: any) => m.addVectorToMatrixByRowAxis(r1, v1);
-  const numericTest = (_: any) => addVectorToMatrixByRowAxis(v1, r2);
+        Matrix.minus(
+          Matrix.addVectorToMatrixByRowAxis(r1, v1),
+          addVectorToMatrixByRowAxis(v1, r1),
+        ),
+      ) <= 1e-8 && Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.addVectorToMatrixByRowAxis(r1, v2, undefined, "column"),
+          addColVectorToMatrixByRowAxis(v2, r1),
+        ),
+      ) <= 1e-8;
+  const euriklisTestForRowVector = (m: any) =>
+    m.addVectorToMatrixByRowAxis(r1, v1);
+  const numericTestForRowVector = (_: any) =>
+    addVectorToMatrixByRowAxis(v1, r1);
+  const euriklisTestForColVector = (m: any) =>
+    m.addVectorToMatrixByRowAxis(r1, v2, undefined, "column");
+  const numericTestForColVector = (_: any) =>
+    addColVectorToMatrixByRowAxis(v2, r1);
   startPerformanceTest(
-    "addVectorToMatrixByRowAxis",
+    "addVectorToMatrixByRowAxis in row mode",
     [{ param: "matrix", dimensions, type: "float64" }],
     condition,
-    euriklisTest,
-    numericTest,
+    euriklisTestForRowVector,
+    numericTestForRowVector,
+  );
+  startPerformanceTest(
+    "addVectorToMatrixByRowAxis in column mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestForColVector,
+    numericTestForColVector,
   );
 })();
