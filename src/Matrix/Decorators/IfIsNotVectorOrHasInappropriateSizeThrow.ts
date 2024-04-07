b8441d15ecf806680a17e48a6eedb3e3a06904ab
VelislavKarastoychev <exel_mmm@abv.bg>
2024-04-07 11:32:26 +0300

    Fixed for column axis methods.

--- src/Matrix/Decorators/IfIsNotVectorOrHasInappropriateSizeThrow.ts
+++ src/Matrix/Decorators/IfIsNotVectorOrHasInappropriateSizeThrow.ts
@@ -3,7 +3,10 @@
 import { IsArrayOfArraysWithEqualSize } from "../Conditions";
 import { MatrixType, NumericMatrix, NumericType } from "../types";
 
-export function ifIsNotVectorOrHasInappropriateSizeThrow(error: Function) {
+export function ifIsNotVectorOrHasInappropriateSizeThrow(
+  error: Function,
+  axis: "row" | "column" = "row",
+) {
   return function (_: any, __: string, descriptor: PropertyDescriptor) {
     const method: (
       matrix: MatrixType | NumericMatrix,
@@ -21,12 +24,30 @@ export function ifIsNotVectorOrHasInappropriateSizeThrow(error: Function) {
       // check if the vector is correctly defined:
       if (!IsArrayOfArraysWithEqualSize(vector)) error();
       // check if the dimensions of the vector are correct.
-      if (mode === "row") {
-        if (vector.length !== 1 || vector[0].length !== matrix.length) error();
+      if (axis === "row") {
+        if (mode === "row") {
+          if (vector.length !== 1 || vector[0].length !== matrix.length) {
+            error();
+          }
+        }
+        if (mode === "column") {
+          if (vector.length !== matrix.length || vector[0].length !== 1) {
+            error();
+          }
+        }
+      } else {
+        if (mode === "row") {
+          if (vector.length !== 1 || vector[0].length !== matrix[0].length) {
+            error();
+          }
+        }
+        if (mode === "column") {
+          if (vector.length !== matrix[0].length || vector[0].length !== 1) {
+            error();
+          }
+        }
       }
-      if (mode === "column") {
-        if (vector.length !== matrix.length || vector[0].length !== 1) error();
-      }
+
       return method.call(this, matrix, vector, type, mode);
     };
   };
