cb0cb651c7c5894236db742aba10e87d804287fa
VelislavKarastoychev <exel_mmm@abv.bg>
2024-04-07 13:55:43 +0300

    Implemented benchmark test for the pointwiseMultiplyMatrixWithVectorByColumnAxis method.

---
+++ Benchmark/pointwise-multiply-matrix-with-vector-by-column-axis.spec.ts
@@ -0,0 +1,78 @@
+"use strict";
+import { Matrix } from "../src";
+import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils";
+
+(async () => {
+  const r1 = Matrix.uniqueRandom(...dimensions);
+  const r2 = Matrix.copy(r1);
+  const v1 = Matrix.uniqueRandom(1, dimensions[1]);
+  const v2 = Matrix.transpose(v1);
+  const pointwiseMultiplyMatrixWithVectorByColumnAxisInRowMode = (m, v) =>
+    numeric.pointwise2(
+      ["v", "x[i]"],
+      "xi = x[i];\n" +
+        "ret[i] = Array(cols);\n" +
+        "reti = ret[i];\n" +
+        "for(j = cols;j-- > 1;) {reti[j] = xi[j] * v[j--];reti[j] = xi[j] * v[j];};\n" +
+        "if(j === 0) reti[0] = xi[0] * v[0];",
+      "v = v[0];let j, reti;const [_, cols] = numeric.dim(x);",
+    )(v, m);
+
+  const pointwiseMultiplyMatrixWithVectorByColumnAxisInColumnMode = (m, v) =>
+    numeric.pointwise2(
+      ["v[i]", "x[i]"],
+      "xi = x[i];\n" +
+        "ret[i] = Array(cols);\n" +
+        "reti = ret[i];\n" +
+        "for(j = cols;j-- > 1;) {reti[j] = xi[j] * v[j--][0];reti[j] = xi[j] * v[j][0];};\n" +
+        "if(j === 0) reti[0] = xi[0] * v[0][0];",
+      "let j, reti;const [_, cols] = numeric.dim(x);",
+    )(v, m);
+
+  const condition = Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.pointwiseMultiplyMatrixWithVectorByColumnAxis(r1, v1),
+          pointwiseMultiplyMatrixWithVectorByColumnAxisInRowMode(r2, v1),
+        ),
+      ) <= 1e-8 && Matrix.FrobeniusNorm(
+        Matrix.minus(
+          Matrix.pointwiseMultiplyMatrixWithVectorByColumnAxis(
+            r1,
+            v2,
+            undefined,
+            "column",
+          ),
+          pointwiseMultiplyMatrixWithVectorByColumnAxisInColumnMode(r2, v2),
+        ),
+      ) <= 1e-8;
+
+  const euriklisTestInRowMode = (m: any) =>
+    m.pointwiseMultiplyMatrixWithVectorByColumnAxis(r1, v1);
+  const euriklisTestInColumnMode = (m: any) =>
+    m.pointwiseMultiplyMatrixWithVectorByColumnAxis(
+      r1,
+      v2,
+      undefined,
+      "column",
+    );
+  const numericTestInRowMode = (_: any) =>
+    pointwiseMultiplyMatrixWithVectorByColumnAxisInRowMode(r2, v1);
+  const numericTestInColumnMode = (_: any) =>
+    pointwiseMultiplyMatrixWithVectorByColumnAxisInColumnMode(r2, v2);
+  startPerformanceTest(
+    "pointwiseMultiplyMatrixWithVectorByColumnAxis in row mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestInRowMode,
+    numericTestInRowMode,
+  );
+
+  startPerformanceTest(
+    "pointwiseMultiplyMatrixWithVectorByColumnAxis in column mode",
+    [{ param: "matrix", dimensions, type: "float64" }],
+    condition,
+    euriklisTestInColumnMode,
+    numericTestInColumnMode,
+  );
+})();
