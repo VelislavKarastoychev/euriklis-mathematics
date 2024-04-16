d11494a2bf0a18528535e4bed1946ed5a0a4a4f7
VelislavKarastoychev <exel_mmm@abv.bg>
2024-04-16 16:44:01 +0300

    Implemented method for the setDiagonalToNumber method.

---
+++ Benchmark/set-diagonal-to-number.spec.ts
@@ -0,0 +1,31 @@
+"use strict";
+import { Matrix } from "../src";
+import numeric from "numericjs";
+import { dimensions, startPerformanceTest } from "./utils";
+import { MatrixType, NumericMatrix } from "../src/Matrix/types";
+(async () => {
+  const r1 = Matrix.uniqueRandom(...dimensions);
+  const r2 = Matrix.copy(r1);
+  const setDiagonalToNumber = (m: MatrixType | NumericMatrix, k: number) =>
+    numeric.pointwise2(
+      ["x[i]", "k"],
+      "for (j = x[i].length;j--;) i === j ? x[i][j] = k : x[i][j];\nret[i] = x[i];",
+      "let j;",
+    )(m, k);
+  const condition = Matrix.FrobeniusNorm(
+    Matrix.minus(
+      Matrix.setDiagonalToNumber(r1, Math.PI),
+      setDiagonalToNumber(r2, Math.PI),
+    ),
+  ) <= 1e-8;
+  const rand = Math.random();
+  const euriklisTest = (m: any) => m.setDiagonalToNumber(r1, rand);
+  const numericTest = (_: any) => setDiagonalToNumber(r2, rand);
+  startPerformanceTest(
+    "setDiagonalToNumber",
+    [{param: "matrix", dimensions, type: "float64"}],
+    condition,
+    euriklisTest,
+    numericTest
+  );
+})();
