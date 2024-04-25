ded7ff4c199a4cd615dbc6310ac004f505a0f110
VelislavKarastoychev <exel_mmm@abv.bg>
2024-04-25 19:13:23 +0300

    Fixed missing type declaration.

--- src/DataStructures/DataNode/index.ts
+++ src/DataStructures/DataNode/index.ts
@@ -1,33 +1,33 @@
 "use strict";
 export class DataNode {
-  private _data = null;
-  private _prev = null;
-  private _next = null;
+  private _data: any = null;
+  private _prev: DataNode | null = null;
+  private _next: DataNode | null = null;
   constructor(data: any) {
     this.data = data;
   }
   
   get data(): any {
-    return this._data;
+    return (this as DataNode)._data;
   }
 
   set data(d: any) {
-    this._data = d;
+    (this as DataNode)._data = d;
   }
 
-  get prev (): DataNode {
-    return this._prev;
+  get prev (): DataNode | null {
+    return (this as DataNode)._prev;
   }
 
-  set prev(node: DataNode) {
-    this._prev = node;
+  set prev(node: DataNode | null) {
+    (this as DataNode)._prev = node;
   }
 
-  get next(): DataNode {
-    return this._next;
+  get next(): DataNode | null{
+    return (this as DataNode)._next;
   }
 
-  set next(node: DataNode) {
-    this._next = node;
+  set next(node: DataNode | null) {
+    (this as DataNode)._next = node;
   }
 }
