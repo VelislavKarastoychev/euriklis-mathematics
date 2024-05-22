cc8e47ca9aed1fb78529b4737f244881c5e8a291
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-22 23:04:36 +0300

    Decorator for the Power method.

---
+++ src/Matrix/Decorators/IfTheVectorIsDefinedAndHasInappropriateSizeThrow.ts
@@ -0,0 +1,20 @@
+"use strict";
+import { Matrix } from "..";
+import type { Integer, MatrixType, NumericMatrix } from "../types";
+
+export function ifTheVectorIsDefinedAndHasInappropriateSizeThrow(
+  error: Function,
+  paramIndex: Integer = 1,
+) {
+  return function (_: any, __: any, descriptor: PropertyDescriptor) {
+    const method = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      const matrix: MatrixType | NumericMatrix = args[0];
+      const vector: MatrixType | NumericMatrix | undefined = args[paramIndex];
+      if (vector) {
+        if (Matrix.dimensions(matrix)[1] !== Matrix.dimensions(vector)[0]) error();
+      }
+      return method.apply(this, args)
+    };
+  };
+}
