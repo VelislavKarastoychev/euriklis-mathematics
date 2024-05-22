e84fea80cfc2acce87e2f6317e0bbfb10ed8e01b
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-22 23:00:01 +0300

    Changed implementation of the zeros utility function. Nor more ArrayBuffers in the implementation.

--- src/Matrix/Models/GenerateZeroMatrix.ts
+++ src/Matrix/Models/GenerateZeroMatrix.ts
@@ -1,15 +1,63 @@
 "use strict";
-import {
+import type {
   Integer,
   MatrixType,
   NumericMatrix,
-  NumericType,
+  // NumericType,
   TypedArray,
   TypedArrayConstructor,
 } from "../types";
-import { CreateTypedArrayConstructor } from "./CreateTypedArrayConstructor.ts";
-import { ComputeBytesLength } from "./ComputeBytesLength.ts";
-import { Replicate } from "./Replicate.ts";
+// import { CreateTypedArrayConstructor } from "./CreateTypedArrayConstructor.ts";
+// import { ComputeBytesLength } from "./ComputeBytesLength.ts";
+// import { Replicate } from "./Replicate.ts";
+
+const CreateZerosRecursively = (
+  dim: Integer[],
+  typedArray: TypedArrayConstructor | ArrayConstructor,
+  it: Integer = 0,
+): TypedArray | number[] | MatrixType | NumericMatrix => {
+  const n = dim[it];
+  let z: TypedArray | number[] | MatrixType | NumericMatrix;
+  let i: Integer;
+  if (it === dim.length - 1) {
+    z = new typedArray(n);
+    if (Array.isArray(z)) {
+      for (i = n; i-- > 3;) {
+        z[i--] = 0;
+        z[i--] = 0;
+        z[i--] = 0;
+        z[i] = 0;
+      }
+      for (++i;i--;) z[0] = 0;
+    }
+
+    return z as TypedArray | number[];
+  }
+
+  z = [];
+  for (i = n; i-- > 3;) {
+    z[i--] = CreateZerosRecursively(dim, typedArray, it + 1) as
+      | TypedArray
+      | number[];
+    z[i--] = CreateZerosRecursively(dim, typedArray, it + 1) as
+      | TypedArray
+      | number[];
+    z[i--] = CreateZerosRecursively(dim, typedArray, it + 1) as
+      | TypedArray
+      | number[];
+    z[i] = CreateZerosRecursively(dim, typedArray, it + 1) as
+      | TypedArray
+      | number[];
+  }
+
+  for (++i;i--;) {
+    z[i] = CreateZerosRecursively(dim, typedArray, it + 1) as
+      | TypedArray
+      | number[];
+  }
+
+  return z as MatrixType | NumericMatrix;
+};
 
 /**
  * Utility function for the zeros method.
@@ -26,21 +74,23 @@ import { Replicate } from "./Replicate.ts";
 export const GenerateZeroMatrix = (
   rows: Integer,
   columns: Integer,
-  type: NumericType,
+  typedArray: TypedArrayConstructor | ArrayConstructor,
 ): MatrixType | NumericMatrix => {
-  const matrix: MatrixType | NumericMatrix = [];
-  const bytesLength: number = (ComputeBytesLength(type) as Integer) * columns;
-  const buffer = new ArrayBuffer(bytesLength * rows);
-  const typedArray = CreateTypedArrayConstructor(type);
-  let i: Integer;
-  if (type !== "generic") {
-    for (i = rows; i--;) {
-      matrix[i] = new (typedArray as TypedArrayConstructor)(
-        buffer,
-        i * bytesLength,
-        columns,
-      );
-    }
-  } else return Replicate(0, rows, columns, type);
-  return matrix;
+  // const matrix: MatrixType | NumericMatrix = [];
+  // const bytesLength: number = (ComputeBytesLength(type) as Integer) * columns;
+  // const buffer = new ArrayBuffer(bytesLength * rows);
+  // const typedArray = CreateTypedArrayConstructor(type);
+  // let i: Integer;
+  // if (type !== "generic") {
+  //   for (i = rows; i--;) {
+  //     matrix[i] = new (typedArray as TypedArrayConstructor)(
+  //       buffer,
+  //       i * bytesLength,
+  //       columns,
+  //     );
+  //   }
+  // } else return Replicate(0, rows, columns, type);
+  return CreateZerosRecursively([rows, columns], typedArray) as
+    | MatrixType
+    | NumericMatrix;
 };
