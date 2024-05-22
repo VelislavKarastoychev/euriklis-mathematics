2513010feb03bd55e3a9e275ebd8e23f9b2f03b9
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-22 22:56:55 +0300

    Writen tests for the eigenvalues with HQR and Jacobi utility functions. Tests for the eigenproblem method.

--- Tests/eigenproblem.spec.ts
+++ Tests/eigenproblem.spec.ts
@@ -1,15 +1,32 @@
 "use strict";
-import numeric from "numericjs";
 import validator from "@euriklis/validator-ts";
 import { Matrix } from "../src";
+import type { MatrixType, NumericMatrix } from "../src/Matrix/types";
 
-// const matrix = [
-//   [1, 2, 3, 4],
-//   [5, 6, 7, 8],
-//   [9, 10, 11, 12],
-//   [13, 14, 5, 16]
-// ];
-const matrix = numeric.random([20, 20]);
-const eig = Matrix.eigenproblem(Matrix.copy(matrix));
-console.log(numeric.eig(Matrix.copy(matrix)).lambda)
-console.log(eig.eigenvalues);
+const matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 5, 16],
+];
+
+const eigenvalues = [32.4642, -2.46425, 4, 0],
+  eigenvectors = [
+    [.25531, -.978357, 0, 2],
+    [.600834, -.443225, -1, -3],
+    [.946137, .0919069, -2, 0],
+    [1, 1, 2, 1],
+  ];
+new validator(
+  Matrix.isNearlyEqualTo(
+    [
+      Matrix.eigenproblem(matrix, { method: "HQR2" }).eigenvalues.real,
+    ] as MatrixType | NumericMatrix,
+    [eigenvalues],
+    1e-4
+  ),
+).isSame(true)
+  .describe("The eigenproblem method has to:")
+  .test({title: true, success: "green", error: "red"})
+  .describe("1. compute the correct eigenpairs with the method HQR.")
+  .test();
