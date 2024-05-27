baa1eed935aca7a79112a669b0f2d6c8745a3e57
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-27 20:59:59 +0300

    Implemented test for the eigenproblem

---
+++ Benchmark/eigenproblem.spec.ts
@@ -0,0 +1,25 @@
+"use strict";
+import { Matrix } from "../src";
+import numeric from "numericjs";
+import { startPerformanceTest } from "./utils";
+
+(async () => {
+  const matrix = Matrix.random(100, 100); const condition = true;
+  const euriklisTest = (m: any) => m.eigenproblem(Matrix.copy(matrix));
+  const numericTest = (m: any) => m.eig(matrix);
+  startPerformanceTest(
+    "eigenvalues",
+    [{ param: "matrix", dimensions: [100, 100], type: "float64" }],
+    condition,
+    {
+      "@euriklis/mathematics": {
+        instance: Matrix,
+        test: euriklisTest,
+      },
+      numericjs: {
+        instance: numeric,
+        test: numericTest,
+      },
+    },
+  );
+})();
