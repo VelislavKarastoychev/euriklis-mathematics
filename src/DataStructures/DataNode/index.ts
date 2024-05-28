7fb1ae81700cb189da1419751586db981fcfd90e
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-28 21:33:39 +0300

    Added LinkedDataNode and splitted DataNode. Types set to be consistent with the DLL and the Stacks.

--- src/DataStructures/DataNode/index.ts
+++ src/DataStructures/DataNode/index.ts
@@ -1,11 +1,28 @@
 "use strict";
+
+import { v4 as uuidv4 } from "uuid";
+
+import type { Integer } from "../../Matrix/types";
+
 export class DataNode<T extends DataNode<any>> {
+  protected _id: string = "";
   protected _data: any = null;
   protected _prev: T | null = null;
   constructor(data?: any) {
     this.data = data;
+    if (data?.id && !this.id) this.id = data.id;
+    else if (typeof data === "string" || typeof data === "number") {
+      this.id = data + "";
+    } else this.id = uuidv4();
   }
 
+  get id(): string {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
   get data(): any {
     return this._data;
   }
@@ -35,5 +52,5 @@ export class LinkedDataNode extends DataNode<LinkedDataNode> {
   set next(node: LinkedDataNode | null) {
     (this as LinkedDataNode)._next = node;
   }
-
 }
+
