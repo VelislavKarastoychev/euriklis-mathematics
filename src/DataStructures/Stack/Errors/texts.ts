ea5b6af9364c586feb569b8e488bdc75e9bdf1b4
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-28 21:33:53 +0300

    Deleted

--- src/DataStructures/Stack/Errors/texts.ts
+++
@@ -1,3 +0,0 @@
-"use strict";
-export const IncorrectParameterInPushManyTxt =
-  "Incorrect parameter in the pushMany method of the current Stack instance. Note that this parameter has to be an array of arbitrary elements.";
