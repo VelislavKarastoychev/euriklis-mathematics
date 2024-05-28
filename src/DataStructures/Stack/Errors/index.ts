00c804e7a2329289b85bc550c4cad7bb437d8030
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-28 21:33:45 +0300

    Deleted

--- src/DataStructures/Stack/Errors/index.ts
+++
@@ -1,8 +0,0 @@
-"use strict";
-import { IncorrectParameterInPushManyTxt } from "./texts";
-const runError: (text: string) => never = (text) => {
-  throw new Error(text);
-};
-
-export const IncorrectParameterInPushMany: () => never = () =>
-  runError(IncorrectParameterInPushManyTxt);
