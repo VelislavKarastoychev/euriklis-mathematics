c101451660bcb42a9c38fb02868371d2260f9a03
VelislavKarastoychev <exel_mmm@abv.bg>
2024-05-28 21:34:50 +0300

    Function runError put in utils.

--- src/Matrix/Errors/index.ts
+++ src/Matrix/Errors/index.ts
@@ -1,16 +1,12 @@
 "use strict";
-import * as texts from "./texts.ts";
+import * as texts from "./texts";
+import { errorGenerator } from "../../utils";
 
 /**
  * Generates the suitable error message for the
  * Matrix library.
  */
-const runError = (text: string): never => {
-  const error: Error = new Error();
-  error.name = "@euriklis/mathematics matrix error message";
-  error.message = text;
-  throw error;
-};
+const runError = errorGenerator("@euriklis/mathematics matrix error message");
 
 export const IncorrectMatrixInput = (): never =>
   runError(texts.IncorrectMatrixInputTxt);
