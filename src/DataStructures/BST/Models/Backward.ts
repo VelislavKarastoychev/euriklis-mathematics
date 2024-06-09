4a14368d2d6604361f201350fcca55f803113cfd
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-09 09:25:52 +0300

    Implemented BackwardRight and BackgroundLeft.

--- src/DataStructures/BST/Models/Backward.ts
+++ src/DataStructures/BST/Models/Backward.ts
@@ -1,9 +1,17 @@
 "use strict";
 import type { BSTDataNode } from "../../DataNode";
-export const Backward = (x: BSTDataNode | null): BSTDataNode | null => {
+export const RightBackward = (x: BSTDataNode | null): BSTDataNode | null => {
   if (x?.prev && (x === x?.prev?.right)) {
-    return Backward(x.prev);
+    return RightBackward(x.prev);
   } else if (!x?.prev && (x?.right)) return null;
+
+  return x;
+};
+
+export const LeftBackward = (x: BSTDataNode | null): BSTDataNode | null => {
+  if (x?.prev && (x === x?.prev?.left)) {
+    return LeftBackward(x?.prev);
+  } else if (!x?.prev && (x?.left)) return null;
 
   return x;
 };
