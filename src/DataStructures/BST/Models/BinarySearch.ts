c7c475b47dace1053e23440cb397094f74a36196
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-12 20:30:11 +0300

    Added generic type.

--- src/DataStructures/BST/Models/BinarySearch.ts
+++ src/DataStructures/BST/Models/BinarySearch.ts
@@ -3,31 +3,33 @@ import type { BST } from "..";
 import type { BSTNodeValueComparisonCallbackType } from "../../../Types";
 import type { BSTDataNode } from "../../DataNode";
 
-export const BinarySearchNode = (
-  tree: BST,
-  node: BSTDataNode | null,
-  callback: (node: BSTDataNode, tree?: BST) => -1 | 0 | 1,
-): BSTDataNode | null => {
+export const BinarySearchNode = <T extends BSTDataNode>(
+  tree: BST<T>,
+  node: T | null,
+  callback: (node: T, tree?: BST<T>) => -1 | 0 | 1,
+): T | null => {
   if (!node) return null;
   const comparison = callback(node, this);
   if (comparison < 0) {
-    return BinarySearchNode(tree, node.left, callback);
+    return BinarySearchNode(tree, node.left as T | null, callback);
   }
   if (comparison > 0) {
-    return BinarySearchNode(tree, node.right, callback);
+    return BinarySearchNode(tree, node.right as T | null, callback);
   }
   return node;
 };
 
-export const BinarySearch = (
-  node: BSTDataNode | null,
+export const BinarySearch = <T extends BSTDataNode>(
+  node: T | null,
   value: any,
   callback: BSTNodeValueComparisonCallbackType,
-): BSTDataNode | null => {
+): T | null => {
   if (node) {
     const comparison = callback(node, value);
-    if (comparison < 0) return BinarySearch(node.left, value, callback);
-    else if (comparison > 0) return BinarySearch(node.right, value, callback);
-    else return node;
+    if (comparison < 0) {
+      return BinarySearch(node.left, value, callback) as T | null;
+    } else if (comparison > 0) {
+      return BinarySearch(node.right, value, callback) as T | null;
+    } else return node;
   } else return null;
 };
