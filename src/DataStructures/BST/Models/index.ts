884962f7f295a0a7316f5a5f6fb1b203c2bb54b3
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-12 19:36:51 +0300

    Added some models

--- src/DataStructures/BST/Models/index.ts
+++ src/DataStructures/BST/Models/index.ts
@@ -2,6 +2,7 @@
 export { BinarySearch, BinarySearchNode } from "./BinarySearch";
 export { CallDFS } from "./CallDFS";
 export { CallBFS } from "./CallBFS";
+export { InsertNodeInBST    } from "./InsertNode";
 export { IsNodeSame } from "./IsNodeSame";
 export { LeftBackward, RightBackward } from "./Backward";
 export { ShiftNodes } from "./ShiftNodes";
