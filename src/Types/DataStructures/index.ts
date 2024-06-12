eca9a3ca5a7502b03619c427f686d3487097b209
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-12 20:31:30 +0300

    Implemented datastructures.

--- src/Types/DataStructures/index.ts
+++ src/Types/DataStructures/index.ts
@@ -1,10 +1,10 @@
 "use strict";
 import type { BSTDataNode } from "../../DataStructures/DataNode";
-export type BSTNodeComparisonCallbackType = (
-  x: BSTDataNode,
-  y: BSTDataNode,
+export type BSTNodeComparisonCallbackType = <T extends BSTDataNode>(
+  x: T,
+  y: T,
 ) => -1 | 0 | 1;
-export type BSTNodeValueComparisonCallbackType = (
-  x: BSTDataNode,
+export type BSTNodeValueComparisonCallbackType = <T extends BSTDataNode>(
+  x: T,
   value: any,
 ) => -1 | 0 | 1;
