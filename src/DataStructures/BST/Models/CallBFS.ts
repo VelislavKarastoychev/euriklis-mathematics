8cd690d4f1f6bf13d8d8f132e76bdcd7d7310fe2
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-12 20:30:21 +0300

    Added generic type.

--- src/DataStructures/BST/Models/CallBFS.ts
+++ src/DataStructures/BST/Models/CallBFS.ts
@@ -4,16 +4,16 @@ import type { BST } from "..";
 import type { BSTDataNode } from "../../DataNode";
 import type { Queue } from "../../Queue";
 
-export const CallBFS = (
-  tree: BST,
+export const CallBFS = <T extends BSTDataNode>(
+  tree: BST<T>,
   Q: Queue,
-  callback: (node: BSTDataNode, tree: BST) => void,
+  callback: (node: T, tree: BST<T>) => void,
 ): void => {
   if (Q.isEmpty) return;
   const node: BSTDataNode = Q.dequeue();
   if (node.right) Q.enqueue(node.right);
   if (node.left) Q.enqueue(node.left);
-  callback(node, tree);
+  callback(node as T, tree);
 
   return CallBFS(tree, Q, callback);
 };
