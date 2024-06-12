d95e57e68bb53d2d169e90c353dde454c49f61bf
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-12 20:30:36 +0300

    Added generic type.

--- src/DataStructures/BST/Models/InsertNode.ts
+++ src/DataStructures/BST/Models/InsertNode.ts
@@ -4,33 +4,33 @@ import type { BST } from "..";
 import type { BSTNodeComparisonCallbackType } from "../../../Types";
 import { BSTDataNode } from "../../DataNode";
 
-const searchForLeaf = (
-  n1: BSTDataNode | null,
-  n2: BSTDataNode,
+const searchForLeaf = <T extends BSTDataNode>(
+  n1: T | null,
+  n2: T,
   orderCallback: BSTNodeComparisonCallbackType,
-  prev: BSTDataNode | null = null,
-): BSTDataNode | null => {
+  prev: T | null = null,
+): T | null => {
   if (n1) {
     prev = n1;
-    if (orderCallback(n2, n1) < 0) n1 = n1.left;
-    else n1 = n1.right;
+    if (orderCallback(n2, n1) < 0) n1 = n1.left as T | null;
+    else n1 = n1.right as T | null;
     return searchForLeaf(n1, n2, orderCallback, prev);
   } else return prev;
 };
 
-export const InsertNodeInBST = (
-  tree: BST,
+export const InsertNodeInBST = <T extends BSTDataNode>(
+  tree: BST<T>,
   data: any,
-  id: string | undefined,
+  id?: string,
 ) => {
   const node = new BSTDataNode(data);
   const orderCallback = tree.order;
-  let root: BSTDataNode | null = tree.rootNode, y: BSTDataNode | null;
+  let root: T | null = tree.rootNode, y: T | null;
   if (id) node.id = id;
-  y = searchForLeaf(root, node, orderCallback);
+  y = searchForLeaf(root, node, orderCallback) as T | null;
   node.prev = y;
 
-  if (!y) tree.rootNode = node;
+  if (!y) tree.rootNode = node as T;
   else {
     if (orderCallback(node, y) < 0) y.left = node;
     else y.right = node;
