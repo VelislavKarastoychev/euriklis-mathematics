e62046196722c6b5e0a4d97cb51c0d21c38fbcd4
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-13 11:31:42 +0300

    Tested the unique property of the BST.

--- Tests/avl.spec.ts
+++ Tests/avl.spec.ts
@@ -3,7 +3,9 @@ import validator from "@euriklis/validator-ts";
 import { AVLTree } from "../src/DataStructures/AVL";
 
 const tree = new AVLTree(4);
+tree.unique = true;
 tree.insert(3);
 tree.insert(4);
+tree.insert(22 / 7);
+tree.insert(Math.sqrt(2));
 tree.print();
-
