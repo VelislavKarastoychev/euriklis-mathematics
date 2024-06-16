29de948d2dc4b61197a651806e4b9708176bcc0a
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-16 18:43:02 +0300

    Implemented Height and delete utility functions.

--- src/DataStructures/BST/Models/index.ts
+++ src/DataStructures/BST/Models/index.ts
@@ -2,8 +2,9 @@
 export { BinarySearch, BinarySearchNode } from "./BinarySearch";
 export { CallDFS } from "./CallDFS";
 export { CallBFS } from "./CallBFS";
-export { InsertNodeInBST    } from "./InsertNode";
+export { InsertNodeInBST } from "./InsertNode";
 export { IsNodeSame } from "./IsNodeSame";
 export { LeftBackward, RightBackward } from "./Backward";
 export { ShiftNodes } from "./ShiftNodes";
 export { CompareNodes, CompareNodeWithValue } from "./OrderCallback";
+export { DeleteNodeInBST } from "./DeleteNodeInBST";
