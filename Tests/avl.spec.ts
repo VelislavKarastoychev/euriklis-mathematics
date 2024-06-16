5501110258a4e3a0c940f686083afec368e8e570
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-16 18:41:19 +0300

    Implemented tests for the avl data structures.

--- Tests/avl.spec.ts
+++ Tests/avl.spec.ts
@@ -1,12 +1,18 @@
 "use strict";
 import validator from "@euriklis/validator-ts";
 import { AVLTree } from "../src/DataStructures/AVL";
+import { BST } from "../src";
 
-const tree = new AVLTree(4);
+const tree = new AVLTree();
+tree.search = (node, value) =>
+  node.data === value ? 0 : node.data > value ? -1 : 1;
 tree.unique = true;
+tree.insert(4);
 tree.insert(3);
-// tree.insert(5);
-// tree.insert(22 / 7);
+tree.insert(5);
+tree.insert(22 / 7);
 tree.insert(Math.sqrt(2));
-// tree.insert(Math.sqrt(3))
+tree.insert(Math.sqrt(3));
+tree.insert(1.41);
+const n = tree.delete(Math.sqrt(2));
 tree.print();
