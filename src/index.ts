37b57683e39efd79eecdff13a41bce05a1c0c6c1
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-18 09:07:35 +0300

    Exported AVLTree and BST data structures.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export const epsilon = 2.220446049250313e-16;
 export { Matrix } from "./Matrix";
 export { Complex } from "./Complex";
 export {
+  AVLTree,
   BST,
   DoublyLinkedList,
   DynamicStack,
