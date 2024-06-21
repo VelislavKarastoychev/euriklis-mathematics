46f18ef410cf03613a217daa32679c5b89e05d63
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-21 12:12:29 +0300

    IMplemented test.

---
+++ Tests/min-row-elements-except-diagonal.spec.ts
@@ -0,0 +1,27 @@
+"use strict";
+import validator from "@euriklis/validator-ts";
+import { Matrix } from "../src";
+
+const a = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const mina = [[2, 4, 7]];
+
+new validator(Matrix.isEqualTo(Matrix.minRowElementsExceptDiagonal(a), mina))
+  .isSame(true)
+  .and.bind(
+    new validator(
+      Matrix.isEqualTo(
+        Matrix.minRowElementsExceptDiagonal(a, undefined, "column"),
+        Matrix.transpose(mina),
+      ),
+    ).isSame(true),
+  ).describe("The minRowElementsExceptDiagonal has to:")
+  .test({ title: true, success: "green", error: "red" })
+  .describe(
+    "1. compute the min elements of each row, excluding the diagonal one and to return a row vector by default.",
+  )
+  .test();
