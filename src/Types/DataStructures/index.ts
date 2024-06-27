75e9923d458ddc2734f05edd8cf45383c6b2d621
VelislavKarastoychev <exel_mmm@abv.bg>
2024-06-27 21:20:20 +0300

    Added types for Graph instance.

--- src/Types/DataStructures/index.ts
+++ src/Types/DataStructures/index.ts
@@ -1,5 +1,7 @@
 "use strict";
+import type { AVLTree } from "../../DataStructures";
 import type { BSTDataNode } from "../../DataStructures/DataNode";
+import type { MatrixType, NumericMatrix, TypedArray } from "../Matrix";
 export type BSTNodeComparisonCallbackType = <T extends BSTDataNode>(
   x: T,
   y: T,
@@ -8,3 +10,32 @@ export type BSTNodeValueComparisonCallbackType = <T extends BSTDataNode>(
   x: T,
   value: any,
 ) => -1 | 0 | 1;
+
+export type GraphNodeType = {
+  id: string;
+  attributes: {
+    value: number | TypedArray | number[] | MatrixType | NumericMatrix;
+    [prop: string]: any;
+  };
+};
+
+export type GraphEdgeType = {
+  source: string;
+  target: string;
+  attributes: {
+    width: number | TypedArray | number[] | MatrixType | NumericMatrix;
+    [prop: string]: any;
+  };
+};
+
+export type GraphEdgePartType = {
+  id: string;
+  attributes: {
+    width: number | TypedArray | number[] | MatrixType | NumericMatrix;
+    [prop: string]: any;
+  };
+};
+export type GraphOptionsType = {
+  nodes: GraphNodeType[];
+  edges?: GraphEdgeType[];
+};
